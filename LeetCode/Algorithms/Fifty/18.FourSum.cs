93c3f5b052807551a361c19c7d06cfc723a8fa7b
XuhuiChu <736265181@qq.com>
2018-01-13 18:19:34 +0800

    18.FourSum

---
+++ LeetCode/Algorithms/Fifty/18.FourSum.cs
@@ -0,0 +1,12 @@
+using System.Collections.Generic;
+
+namespace ConsoleApplication.Algorithms.Fifty
+{
+    class FourSum
+    {
+        public IList<IList<int>> Calc(int[] nums,int target){
+            IList<IList<int>> result = new List<IList<int>>();
+            return result;
+        }
+    }
+}
