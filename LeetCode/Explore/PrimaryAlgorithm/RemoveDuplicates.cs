e4aa5174abe4e8b170a60d47d8a5fa4c1c729b01
LoveLan1314 <736265181@qq.com>
2018-04-25 23:42:37 +0800

    RemoveDuplicates

---
+++ LeetCode/Explore/PrimaryAlgorithm/RemoveDuplicates.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode.Explore.PrimaryAlgorithm
+{
+    class RemoveDuplicatesSolution
+    {
+        public int RemoveDuplicates(int[] nums)
+        {
+            if (nums.Length == 0)
+            {
+                return 0;
+            }
+            if (nums.Length == 1)
+            {
+                return 1;
+            }
+            int result = 0;
+            for (int i = 1; i < nums.Length; i++)
+            {
+                if (nums[result] != nums[i])
+                {
+                    result++;
+                    nums[result] = nums[i];
+                }
+            }
+            return result + 1;
+        }
+    }
+}
