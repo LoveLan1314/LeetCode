f4c9f708c71e95fe5ae12d399771796d506c8080
LoveLan1314 <736265181@qq.com>
2018-05-11 23:05:22 +0800

    买卖股票的最佳时机 II

---
+++ LeetCode/Explore/PrimaryAlgorithm/MaxProfitSolution.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode.Explore.PrimaryAlgorithm
+{
+    class MaxProfitSolution
+    {
+        public int MaxProfit(int[] prices)
+        {
+            int maxPro = 0;
+            int temp = 0;
+            for (int i = 1; i < prices.Length; i++)
+            {
+                temp = prices[i] - prices[i - 1];
+                if(temp > 0)
+                {
+                    maxPro += temp;
+                }
+            }
+            return maxPro;
+        }
+    }
+}
