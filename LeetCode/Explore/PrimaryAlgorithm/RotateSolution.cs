ec4f7a0624350c416489d7ef0efc657d4adcd51f
XuhuiChu <736265181@qq.com>
2018-05-11 13:32:22 +0800

    旋转数组

---
+++ LeetCode/Explore/PrimaryAlgorithm/RotateSolution.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode.Explore.PrimaryAlgorithm
+{
+    class RotateSolution
+    {
+        /// <summary>
+        /// 该方法超时
+        /// </summary>
+        /// <param name="nums"></param>
+        /// <param name="k"></param>
+        public void Rotate(int[] nums, int k)
+        {
+            int length = nums.Length;
+            while (k > 0)
+            {
+                int t = nums[length - 1];
+                for (int i = length - 1; i > 0; i--)
+                {
+                    nums[i] = nums[i - 1];
+                }
+                nums[0] = t;
+                k--;
+            }
+        }
+
+        public void Rotate2(int[] nums, int k)
+        {
+            if (nums == null || nums.Length == 0 || k % nums.Length == 0)
+            {
+                return;
+            }
+
+            int turns = k % nums.Length;
+            int middle = nums.Length - turns;
+
+            Reverse(nums, 0, middle - 1);
+            Reverse(nums, middle, nums.Length - 1);
+            Reverse(nums, 0, nums.Length - 1);
+        }
+
+        private void Reverse(int[] arr, int s, int e)
+        {
+            while (s < e)
+            {
+                int temp = arr[s];
+                arr[s] = arr[e];
+                arr[e] = temp;
+
+                s++;
+                e--;
+            }
+        }
+    }
+}
