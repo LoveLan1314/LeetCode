83de173e21bb45eb79c1d626428a1d2ef25829f1
XuhuiChu <736265181@qq.com>
2018-05-12 13:30:16 +0800

    存在重复元素

---
+++ LeetCode/Explore/PrimaryAlgorithm/ContainsDuplicateSolution.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode.Explore.PrimaryAlgorithm
+{
+    class ContainsDuplicateSolution
+    {
+        public bool ContainsDuplicate(int[] nums)
+        {
+            if (nums.Length == 0 || nums.Length == 1)
+                return false;
+            Array.Sort(nums);
+            for (int i = 1; i < nums.Length; i++)
+            {
+                if (nums[i] == nums[i - 1])
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public bool ContainsDuplicate1(int[] nums)
+        {
+            HashSet<int> set = new HashSet<int>();
+            foreach (var item in nums)
+            {
+                if (set.Contains(item))
+                {
+                    return true;
+                }
+                set.Add(item);
+            }
+            return false;
+        }
+    }
+}
