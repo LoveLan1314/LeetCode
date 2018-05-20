45999403561c3ed662e4b722b90230a35263ba61
LoveLan1314 <736265181@qq.com>
2018-05-20 23:53:49 +0800

    两数之和

---
+++ LeetCode/Explore/PrimaryAlgorithm/TwoSumSolution.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode.Explore.PrimaryAlgorithm
+{
+    class TwoSumSolution
+    {
+        public int[] TwoSum(int[] nums, int target)
+        {
+            for (int i = 0; i < nums.Length; i++)
+            {
+                for (int j = i + 1; j < nums.Length; j++)
+                {
+                    if(nums[i] + nums[j] == target)
+                    {
+                        return new int[] { i, j };
+                    }
+                }
+            }
+            return null;
+        }
+    }
+}
