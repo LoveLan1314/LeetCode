ed5b8232c64032ec4d250d2913094be131114a5a
XuhuiChu <736265181@qq.com>
2018-05-23 13:01:31 +0800

    反转字符串

---
+++ LeetCode/Explore/PrimaryAlgorithm/String/ReverseStringSolution.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode.Explore.PrimaryAlgorithm.String
+{
+    class ReverseStringSolution
+    {
+        public string ReverseString(string s)
+        {
+            var list = s.ToCharArray();
+            for (int i = 0; i < list.Length / 2; i++)
+            {
+                char swap = list[i];
+                list[i] = list[list.Length - 1 - i];
+                list[list.Length - 1 - i] = swap;
+            }
+            return new string(list);
+        }
+
+        public string ReverseString1(string s)
+        {
+            char[] chars = new char[s.Length];
+            int start = 0;
+            int end = s.Length - 1;
+            while (start <= end)
+            {
+                chars[start] = s[end];
+                chars[end] = s[start];
+                start++;
+                end--;
+            }
+            return new string(chars);
+        }
+    }
+}
