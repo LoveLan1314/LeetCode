096ddfcb8968a7003b1f537ff003ead76d859e8f
XuhuiChu <736265181@qq.com>
2018-05-24 13:18:53 +0800

    反转整数

---
+++ LeetCode/Explore/PrimaryAlgorithm/String/ReverseSolution.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode.Explore.PrimaryAlgorithm.String
+{
+    class ReverseSolution
+    {
+        public int Reverse(int x)
+        {
+            char[] vs = x.ToString().ToCharArray();
+            int index = 0;
+            int length = vs.Length;
+            if (vs[0] == '-')
+            {
+                index++;
+                length--;
+            }
+            for (int i = index; i < length / 2 + index; i++)
+            {
+                char swap = vs[i];
+                vs[i] = vs[vs.Length - 1 + index - i];
+                vs[vs.Length - 1 + index - i] = swap;
+            }
+            int.TryParse(new string(vs), out int result);
+            return result;
+        }
+
+        public int Reverse1(int x)
+        {
+            char[] vs = x.ToString().ToCharArray();
+            int index = 0;
+            if (vs[0] == '-')
+            {
+                index++;
+            }
+            for (int i = index; i < vs.Length / 2 + index; i++)
+            {
+                char swap = vs[i];
+                vs[i] = vs[vs.Length - 1 + index - i];
+                vs[vs.Length - 1 + index - i] = swap;
+            }
+            int.TryParse(new string(vs), out int result);
+            return result;
+        }
+    }
+}
