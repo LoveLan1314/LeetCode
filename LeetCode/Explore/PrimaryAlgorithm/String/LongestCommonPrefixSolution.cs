a9ef1390095b59ffc6908cd9892efb822a557ade
XuhuiChu <736265181@qq.com>
2018-05-31 12:32:14 +0800

    最长公共前缀

---
+++ LeetCode/Explore/PrimaryAlgorithm/String/LongestCommonPrefixSolution.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode.Explore.PrimaryAlgorithm.String
+{
+    class LongestCommonPrefixSolution
+    {
+        public string LongestCommonPrefix(string[] strs)
+        {
+            if (strs.Length == 0)
+            {
+                return "";
+            }
+            string prefix = strs[0];
+            for (int i = 1; i < strs.Length; i++)
+            {
+                if (prefix.Length == 0 || strs[i].Length == 0)
+                {
+                    return "";
+                }
+
+                int len = Math.Min(prefix.Length, strs[i].Length);
+                int j = 0;
+                for (; j < len; j++)
+                {
+                    if (prefix[j] != strs[i][j])
+                    {
+                        break;
+                    }
+                }
+                prefix = prefix.Substring(0, j);
+            }
+            return prefix;
+        }
+    }
+}
