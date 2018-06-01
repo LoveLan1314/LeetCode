3b25638d4df0e8d2cd45e323d5e5c82ab5231dc1
LoveLan1314 <736265181@qq.com>
2018-06-01 22:03:46 +0800

    删除链表中的节点

---
+++ LeetCode/Explore/PrimaryAlgorithm/LinkedList/DeleteNodeSolution.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode.Explore.PrimaryAlgorithm.LinkedList
+{
+    class DeleteNodeSolution
+    {
+        public void DeleteNode(ListNode node)
+        {
+            node.val = node.next.val;
+            node.next = node.next.next;
+        }
+    }
+}
---
+++ LeetCode/Explore/PrimaryAlgorithm/LinkedList/ListNode.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode.Explore.PrimaryAlgorithm.LinkedList
+{
+    class ListNode
+    {
+        public int val;
+        public ListNode next;
+        public ListNode(int x) { val = x; }
+    }
+}
