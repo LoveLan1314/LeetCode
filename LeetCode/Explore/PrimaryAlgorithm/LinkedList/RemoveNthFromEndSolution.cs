ee233e566873213fb8074cf534f3d49196c2562a
XuhuiChu <736265181@qq.com>
2018-06-04 13:12:32 +0800

    删除链表的倒数第N个节点

---
+++ LeetCode/Explore/PrimaryAlgorithm/LinkedList/RemoveNthFromEndSolution.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode.Explore.PrimaryAlgorithm.LinkedList
+{
+    class RemoveNthFromEndSolution
+    {
+        public ListNode RemoveNthFromEnd(ListNode head,int n)
+        {
+            ListNode faster = head;
+            ListNode slower = head;
+            for (int i = 0; i < n; i++)
+            {
+                faster = faster.next;
+            }
+            if(faster == null)
+            {
+                head = head.next;
+                return head;
+            }
+            while(faster.next != null)
+            {
+                slower = slower.next;
+                faster = faster.next;
+            }
+            slower.next = slower.next.next;
+            return head;
+        }
+    }
+}
