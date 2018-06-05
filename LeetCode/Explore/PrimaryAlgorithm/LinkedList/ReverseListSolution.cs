170295bc568666bf9be2f892b6d7abec7c9ea830
XuhuiChu <736265181@qq.com>
2018-06-05 13:08:52 +0800

    反转链表

---
+++ LeetCode/Explore/PrimaryAlgorithm/LinkedList/ReverseListSolution.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode.Explore.PrimaryAlgorithm.LinkedList
+{
+    class ReverseListSolution
+    {
+        public ListNode ReverseList(ListNode head)
+        {
+            if(head == null || head.next == null)
+            {
+                return head;
+            }
+            ListNode cur = head;
+            ListNode newH = null;
+            while (cur != null)
+            {
+                ListNode tmp = cur.next;
+                cur.next = newH;
+                newH = cur;
+                cur = tmp;
+            }
+            return newH;
+        }
+    }
+}
