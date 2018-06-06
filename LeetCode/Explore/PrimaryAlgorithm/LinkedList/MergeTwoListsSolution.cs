2fe1aba68af93dc9bc640fb8582dc33d04fa4ea4
XuhuiChu <736265181@qq.com>
2018-06-06 12:58:01 +0800

    合并两个有序链表

---
+++ LeetCode/Explore/PrimaryAlgorithm/LinkedList/MergeTwoListsSolution.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode.Explore.PrimaryAlgorithm.LinkedList
+{
+    class MergeTwoListsSolution
+    {
+        public ListNode MergeTwoLists(ListNode l1, ListNode l2)
+        {
+            if (l1 == null) return l2;
+            if (l2 == null) return l1;
+            ListNode tmp = null;
+            if(l1.val > l2.val)
+            {
+                tmp = l2;
+                tmp.next = MergeTwoLists(l1, l2.next);
+            }
+            else
+            {
+                tmp = l1;
+                tmp.next = MergeTwoLists(l1.next, l2);
+            }
+            return tmp;
+        }
+    }
+}
