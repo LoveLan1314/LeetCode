62e1f46e4570c3e4be38cca96d1a8f1a9fc0fd63
XuhuiChu <736265181@qq.com>
2018-06-07 12:56:22 +0800

    回文链表

---
+++ LeetCode/Explore/PrimaryAlgorithm/LinkedList/IsPalindromeSolution.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode.Explore.PrimaryAlgorithm.LinkedList
+{
+    class IsPalindromeSolution
+    {
+        public bool IsPalindrome(ListNode head)
+        {
+            if(head == null)
+            {
+                return true;
+            }
+
+            ListNode slow = head;
+            ListNode fast = head;
+            while(fast.next != null && fast.next.next != null)
+            {
+                slow = slow.next;
+                fast = fast.next.next;
+            }
+
+            slow.next = Reverse(slow.next);
+            ListNode pre = head;
+            while(slow.next != null)
+            {
+                slow = slow.next;
+                if(pre.val != slow.val)
+                {
+                    return false;
+                }
+                pre = pre.next;
+            }
+            return true;
+        }
+
+        private ListNode Reverse(ListNode head)
+        {
+            if (head == null || head.next == null)
+            {
+                return head;
+            }
+            ListNode cur = head;
+            ListNode newH = null;
+            while (cur != null)
+            {
+                ListNode tmp = cur.next;
+                cur.next = newH;
+                newH = cur;
+                cur = tmp;
+            }
+            return newH;
+        }
+    }
+}
