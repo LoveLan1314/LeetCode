b317244fd67fed3ac1a8fc821703890da2ef152f
XuhuiChu <736265181@qq.com>
2018-06-09 12:35:35 +0800

    二叉树的最大深度

---
+++ LeetCode/Explore/PrimaryAlgorithm/Tree/MaxDepthSolution.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode.Explore.PrimaryAlgorithm.Tree
+{
+    class MaxDepthSolution
+    {
+        public int MaxDepth(TreeNode root)
+        {
+            return GetTreeDepth(root, 0);
+        }
+        public int GetTreeDepth(TreeNode node,int deep)
+        {
+            if(node == null)
+            {
+                return deep;
+            }
+            deep++;
+            return Math.Max(GetTreeDepth(node.left, deep), GetTreeDepth(node.right, deep));
+        }
+    }
+}
