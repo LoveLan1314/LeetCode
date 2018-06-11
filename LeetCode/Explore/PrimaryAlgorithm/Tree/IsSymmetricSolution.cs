9555eda477eefac71971d1e76066aee38edff729
XuhuiChu <736265181@qq.com>
2018-06-11 13:05:55 +0800

    对称二叉树

---
+++ LeetCode/Explore/PrimaryAlgorithm/Tree/IsSymmetricSolution.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode.Explore.PrimaryAlgorithm.Tree
+{
+    class IsSymmetricSolution
+    {
+        public bool IsSymmetric(TreeNode root)
+        {
+            if(root == null)
+            {
+                return true;
+            }
+            return IsSymmetricCore(root.left, root.right);
+        }
+        private bool IsSymmetricCore(TreeNode left,TreeNode right)
+        {
+            if(left == null && right == null)
+            {
+                return true;
+            }
+            if(left != null && right != null && left.val == right.val)
+            {
+                return IsSymmetricCore(left.left, right.right) && IsSymmetricCore(left.right, right.left);
+            }
+            return false;
+        }
+    }
+}
