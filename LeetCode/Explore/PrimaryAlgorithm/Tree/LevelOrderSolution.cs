8ff2f3d2e38d52f0d91d15ab641856020982177c
XuhuiChu <736265181@qq.com>
2018-06-12 13:14:45 +0800

    二叉树的层次遍历

---
+++ LeetCode/Explore/PrimaryAlgorithm/Tree/LevelOrderSolution.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode.Explore.PrimaryAlgorithm.Tree
+{
+    class LevelOrderSolution
+    {
+        public IList<IList<int>> LevelOrder(TreeNode root)
+        {
+            TreeNode temp = new TreeNode('#');
+            List<int> list = new List<int>();
+            IList<IList<int>> result = new List<IList<int>>();
+            Queue<TreeNode> nodes = new Queue<TreeNode>();
+            if (root == null)
+            {
+                return result;
+            }
+            nodes.Enqueue(root);
+            nodes.Enqueue(temp);
+            while(nodes.Count != 0)
+            {
+                var node = nodes.Dequeue();
+                if(node == temp)
+                {
+                    if(nodes.Count != 0)
+                    {
+                        nodes.Enqueue(temp);
+                    }
+                    result.Add(list);
+                    list = new List<int>();
+                    continue;
+                }
+                list.Add(node.val);
+                if(node.left != null)
+                {
+                    nodes.Enqueue(node.left);
+                }
+                if(node.right != null)
+                {
+                    nodes.Enqueue(node.right);
+                }
+            }
+            return result;
+        }
+    }
+}
