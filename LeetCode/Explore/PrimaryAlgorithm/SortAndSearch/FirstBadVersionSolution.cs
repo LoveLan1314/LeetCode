74ff3981a8763aad54260e01edbf7fed71d4762c
XuhuiChu <736265181@qq.com>
2018-06-15 13:09:41 +0800

    第一个错误的版本

---
+++ LeetCode/Explore/PrimaryAlgorithm/SortAndSearch/FirstBadVersionSolution.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode.Explore.PrimaryAlgorithm.SortAndSearch
+{
+    class FirstBadVersionSolution : VersionControl
+    {
+        public int FirstBadVersion(int n)
+        {
+            int min = 1;
+            int max = n;
+            int mid = 0;
+            while (min <= max)
+            {
+                mid = min + (max - min) / 2;
+                if (IsBadVersion(mid))
+                {
+                    max = mid - 1;
+                }
+                else
+                {
+                    min = mid + 1;
+                }
+            }
+            return min;
+        }
+    }
+}
---
+++ LeetCode/Explore/PrimaryAlgorithm/SortAndSearch/VersionControl.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode.Explore.PrimaryAlgorithm.SortAndSearch
+{
+    class VersionControl
+    {
+        public bool IsBadVersion(int version)
+        {
+            return true;
+        }
+    }
+}
