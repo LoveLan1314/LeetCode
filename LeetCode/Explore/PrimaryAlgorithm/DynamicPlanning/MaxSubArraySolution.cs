fd8fd4493ec91274309c4043eee0b7a9ed1e787e
XuhuiChu <736265181@qq.com>
2018-07-13 12:51:14 +0800

    最大子序和

---
+++ LeetCode/Explore/PrimaryAlgorithm/DynamicPlanning/MaxSubArraySolution.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode.Explore.PrimaryAlgorithm.DynamicPlanning
+{
+    class MaxSubArraySolution
+    {
+        public int MaxSubArray(int[] nums)
+        {
+            if (nums.Length == 0)
+            {
+                return 0;
+            }
+            if (nums.Length == 1)
+            {
+                return nums[0];
+            }
+            int[] sum = new int[nums.Length];
+            sum[0] = nums[0];
+            int max = sum[0];
+            for (int i = 1; i < nums.Length; i++)
+            {
+                sum[i] = Math.Max(sum[i - 1] + nums[i], nums[i]);
+                if (max < sum[i])
+                {
+                    max = sum[i];
+                }
+            }
+            return max;
+        }
+    }
+}
