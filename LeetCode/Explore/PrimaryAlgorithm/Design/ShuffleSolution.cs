607751b63ace924dc2bb06e9f8a5f2a444b24aa4
XuhuiChu <736265181@qq.com>
2018-07-17 13:31:14 +0800

    打乱数组(无法通过测试用例)

---
+++ LeetCode/Explore/PrimaryAlgorithm/Design/ShuffleSolution.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode.Explore.PrimaryAlgorithm.Design
+{
+    class ShuffleSolution
+    {
+        private readonly int[] _nums;
+        public ShuffleSolution(int[] nums)
+        {
+            _nums = nums;
+        }
+
+        public int[] Reset()
+        {
+            return _nums;
+        }
+
+        public int[] Shuffle()
+        {
+            int[] shuffle = new int[_nums.Length];
+            System.Array.Copy(_nums, shuffle, _nums.Length);
+            int pos;
+            int temp;
+            Random random = new Random();
+            for (int i = 0; i < _nums.Length; i++)
+            {
+                pos = random.Next(_nums.Length);
+                temp = shuffle[pos];
+                shuffle[pos] = shuffle[i];
+                shuffle[i] = temp;
+            }
+            return shuffle;
+        }
+    }
+}
