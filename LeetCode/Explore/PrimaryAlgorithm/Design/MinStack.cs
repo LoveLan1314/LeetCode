5b5ee63b421d67088a67cb7021da2013140d1659
XuhuiChu <736265181@qq.com>
2018-07-18 12:52:22 +0800

    最小栈

---
+++ LeetCode/Explore/PrimaryAlgorithm/Design/MinStack.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode.Explore.PrimaryAlgorithm.Design
+{
+    class MinStack
+    {
+        Stack<int> _stack = new Stack<int>();
+        Stack<int> _min = new Stack<int>();
+        public MinStack()
+        {
+
+        }
+
+        public void Push(int x)
+        {
+            _stack.Push(x);
+            if (_min.Count == 0 || x <= _min.Peek())
+            {
+                _min.Push(x);
+            }
+        }
+
+        public void Pop()
+        {
+            int x = _stack.Pop();
+            if (x == _min.Peek())
+            {
+                _min.Pop();
+            }
+        }
+
+        public int Top()
+        {
+            return _stack.Peek();
+        }
+
+        public int GetMin()
+        {
+            return _min.Peek();
+        }
+    }
+}
