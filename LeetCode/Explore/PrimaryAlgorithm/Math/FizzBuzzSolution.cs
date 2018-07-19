75ace6dee28cf635fcfea1f7bf297794a8019170
XuhuiChu <736265181@qq.com>
2018-07-19 13:11:00 +0800

    Fizz Buzz

---
+++ LeetCode/Explore/PrimaryAlgorithm/Math/FizzBuzzSolution.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode.Explore.PrimaryAlgorithm.Math
+{
+    class FizzBuzzSolution
+    {
+        public IList<string> FizzBuzz(int n)
+        {
+            List<string> list = new List<string>();
+            for (int i = 1; i <= n; i++)
+            {
+                string str = "";
+                if (i % 3 == 0)
+                {
+                    str += "Fizz";
+                }
+                if (i % 5 == 0)
+                {
+                    str += "Buzz";
+                }
+                if (string.IsNullOrEmpty(str))
+                {
+                    str = i.ToString();
+                }
+                list.Add(str);
+            }
+            return list;
+        }
+    }
+}
