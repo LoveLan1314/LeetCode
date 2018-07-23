8fd29cd890686f2a28d8819b8c4acfe1c1db48c1
XuhuiChu <736265181@qq.com>
2018-07-23 13:16:22 +0800

    3的幂

---
+++ LeetCode/Explore/PrimaryAlgorithm/Math/IsPowerOfThreeSolution.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode.Explore.PrimaryAlgorithm.Math
+{
+    class IsPowerOfThreeSolution
+    {
+        public bool IsPowerOfThree(int n)
+        {
+            if (n == 1 || n == 3)
+            {
+                return true;
+            }
+            return GetIsPowerOfThree(n);
+        }
+        public bool GetIsPowerOfThree(int n)
+        {
+            if (n % 3 == 0)
+            {
+                int m = n / 3;
+                if (m == 3)
+                {
+                    return true;
+                }
+                else if (m < 3)
+                {
+                    return false;
+                }
+                else
+                {
+                    return GetIsPowerOfThree(m);
+                }
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
