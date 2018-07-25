351548564b64609345a4bc6c276ee5ac05ec5f7e
XuhuiChu <736265181@qq.com>
2018-07-25 13:27:23 +0800

    位1的个数

---
+++ LeetCode/Explore/PrimaryAlgorithm/Others/HammingWeightSolution.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode.Explore.PrimaryAlgorithm.Others
+{
+    class HammingWeightSolution
+    {
+        public int HammingWeight(uint n)
+        {
+            int result = 0;
+            for (int i = 0; i < 32; i++)
+            {
+                result += (int)(n % 2);
+                n /= 2;
+            }
+            return result;
+        }
+        public int HammingWeight2(uint n)
+        {
+            int result = 0;
+            uint m = 1;
+            while (m > 0)
+            {
+                if ((m & n) > 0)
+                {
+                    result++;
+                }
+                m = m << 1;
+            }
+            return result;
+        }
+    }
+}
