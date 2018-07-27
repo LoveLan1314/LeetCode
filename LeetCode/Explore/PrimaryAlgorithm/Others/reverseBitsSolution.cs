4e64513482c2774384e0400ffb485e7e86b9c99e
XuhuiChu <736265181@qq.com>
2018-07-27 14:47:32 +0800

    颠倒二进制位

---
+++ LeetCode/Explore/PrimaryAlgorithm/Others/reverseBitsSolution.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+
+namespace LeetCode.Explore.PrimaryAlgorithm.Others
+{
+    class ReverseBitsSolution
+    {
+        public uint ReverseBits(uint n)
+        {
+            var bits = new BitArray(BitConverter.GetBytes(n));
+            uint result = 0;
+            for (int i = 0; i < 32; i++)
+            {
+                if (bits[i])
+                {
+                    result = result | 1;
+                }
+                if(i != 31)
+                {
+                    result = result << 1;
+                }
+            }
+            return result;
+        }
+    }
+}
