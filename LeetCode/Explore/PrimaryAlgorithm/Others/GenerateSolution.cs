f0ae8bfddd90fb66eb8ef31e61aaad98913adbe7
XuhuiChu <736265181@qq.com>
2018-07-28 13:03:23 +0800

    杨辉三角

---
+++ LeetCode/Explore/PrimaryAlgorithm/Others/GenerateSolution.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode.Explore.PrimaryAlgorithm.Others
+{
+    class GenerateSolution
+    {
+        public IList<IList<int>> Generate(int numRows)
+        {
+            List<IList<int>> result = new List<IList<int>>();
+            if (numRows >= 1)
+            {
+                result.Add(new List<int>() { 1 });
+            }
+            if (numRows >= 2)
+            {
+                result.Add(new List<int>() { 1, 1 });
+            }
+            if (numRows >= 3)
+            {
+                for (int i = 3; i <= numRows; i++)
+                {
+                    List<int> data = new List<int>() { 1 };
+                    IList<int> prev = result[i - 2];
+                    for (int j = 1; j < i - 1; j++)
+                    {
+                        data.Add(prev[j - 1] + prev[j]);
+                    }
+                    data.Add(1);
+                    result.Add(data);
+                }
+            }
+            return result;
+        }
+    }
+}
