421bf3fb39d5e6f16d3e3b4111bce5d9b27c7fa3
XuhuiChu <736265181@qq.com>
2018-07-30 12:56:18 +0800

    有效的括号

---
+++ LeetCode/Explore/PrimaryAlgorithm/Others/IsValidSolution.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode.Explore.PrimaryAlgorithm.Others
+{
+    class IsValidSolution
+    {
+        public bool IsValid(string s)
+        {
+            if (s.Length == 0)
+            {
+                return true;
+            }
+            Stack<char> stack = new Stack<char>();
+            for (int i = 0; i < s.Length; i++)
+            {
+                char c = s[i];
+                if (c == '(' || c == '{' || c == '[')
+                {
+                    stack.Push(c);
+                }
+                else
+                {
+                    if (stack.Count == 0)
+                    {
+                        return false;
+                    }
+                    char top = stack.Pop();
+                    if (top == '(' && c == ')')
+                    {
+                        continue;
+                    }
+                    if (top == '{' && c == '}')
+                    {
+                        continue;
+                    }
+                    if (top == '[' && c == ']')
+                    {
+                        continue;
+                    }
+                    return false;
+                }
+            }
+            if (stack.Count == 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
