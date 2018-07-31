14b5e0347e41d0542d766de459ba40ad0d8c148d
XuhuiChu <736265181@qq.com>
2018-07-31 12:59:30 +0800

    缺失数字

---
+++ LeetCode/Explore/PrimaryAlgorithm/Others/MissingNumberSolution.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode.Explore.PrimaryAlgorithm.Others
+{
+    class MissingNumberSolution
+    {
+        public int MissingNumber(int[] nums)
+        {
+            int sum = 0;
+            int n = nums.Length;
+            for (int i = 0; i < n; i++)
+            {
+                sum += nums[i];
+            }
+            return (int)(0.5 * n * (n + 1) - sum);
+        }
+    }
+}
