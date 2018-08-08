4334e2b3b3087a7ca29df0f90f2a51c8da4ecb1b
XuhuiChu <736265181@qq.com>
2018-08-08 13:04:57 +0800

    递增的三元子序列

---
+++ LeetCode/Explore/IntermediateAlgorithm/ArrayAndString/IncreasingTripletSolution.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode.Explore.IntermediateAlgorithm.ArrayAndString
+{
+    class IncreasingTripletSolution
+    {
+        public bool IncreasingTriplet(int[] nums)
+        {
+            int m1 = int.MaxValue;
+            int m2 = int.MaxValue;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (m1 >= nums[i])
+                {
+                    m1 = nums[i];
+                }
+                else if (m2 >= nums[i])
+                {
+                    m2 = nums[i];
+                }
+                else
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+}
