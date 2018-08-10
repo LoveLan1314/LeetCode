2e7a08baa1a5b2c3f4bfd2236048614b723d4440
XuhuiChu <736265181@qq.com>
2018-08-10 13:05:58 +0800

    奇偶链表

---
+++ LeetCode/Explore/IntermediateAlgorithm/LinkedList/OddEvenListSolution.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode.Explore.IntermediateAlgorithm.LinkedList
+{
+    class OddEvenListSolution
+    {
+        public ListNode OddEvenList(ListNode head)
+        {
+            if (head == null || head.next == null || head.next.next == null)
+            {
+                return head;
+            }
+            ListNode first = head;
+            ListNode eventList = new ListNode(0);
+            ListNode eventFirst = eventList;
+            while (head.next != null)
+            {
+                eventList.next = head.next;
+                eventList = eventList.next;
+                head.next = head.next.next;
+                eventList.next = null;
+                if(head.next != null)
+                {
+                    head = head.next;
+                }
+            }
+            head.next = eventFirst.next;
+            return first;
+        }
+    }
+}
--- LeetCode/Program.cs
+++ LeetCode/Program.cs
@@ -1,15 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using ConsoleApplication.Algorithms.Fifty;
-using ConsoleApplication.Facade;
-
-namespace ConsoleApplication
+﻿namespace ConsoleApplication
 {
     public class Program
     {
         public static void Main(string[] args)
         {
-            
+
         }
     }
 }
