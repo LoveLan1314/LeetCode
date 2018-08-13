b8b90acc51f892ab6e2e9dcba7159fb3cb2dab36
XuhuiChu <736265181@qq.com>
2018-08-13 12:59:29 +0800

    相交链表

---
+++ LeetCode/Explore/IntermediateAlgorithm/LinkedList/GetIntersectionNodeSolution.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode.Explore.IntermediateAlgorithm.LinkedList
+{
+    class GetIntersectionNodeSolution
+    {
+        public ListNode GetIntersectionNode(ListNode headA, ListNode headB)
+        {
+            if (headA == null || headB == null)
+            {
+                return null;
+            }
+            ListNode p = headA;
+            ListNode q = headB;
+            void ToListNodeEnd()
+            {
+                while (p != null && q != null)
+                {
+                    p = p.next;
+                    q = q.next;
+                }
+                if (p == null)
+                {
+                    p = headB;
+                }
+                else
+                {
+                    q = headA;
+                }
+            }
+            ToListNodeEnd();
+            ToListNodeEnd();
+            while (p != null && q != null)
+            {
+                if (p == q)
+                {
+                    return p;
+                }
+                p = p.next;
+                q = q.next;
+            }
+            return null;
+        }
+    }
+}
