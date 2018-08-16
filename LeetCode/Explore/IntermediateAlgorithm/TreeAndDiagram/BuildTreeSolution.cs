4af4967160312272bccd5dd0fa1d4433b5495050
XuhuiChu <736265181@qq.com>
2018-08-16 13:06:48 +0800

    从前序与中序遍历序列构造二叉树

---
+++ LeetCode/Explore/IntermediateAlgorithm/TreeAndDiagram/BuildTreeSolution.cs
@@ -0,0 +1,44 @@
+﻿namespace LeetCode.Explore.IntermediateAlgorithm.TreeAndDiagram
+{
+    internal class BuildTreeSolution
+    {
+        public TreeNode BuildTree(int[] preorder, int[] inorder)
+        {
+            if (preorder == null || inorder == null || preorder.Length == 0)
+            {
+                return null;
+            }
+            return BuildCore(preorder, 0, preorder.Length - 1, inorder, 0, inorder.Length - 1);
+        }
+
+        private TreeNode BuildCore(int[] preorder, int preSt, int preEnd, int[] inorder, int inSt, int inEnd)
+        {
+            int rootValue = preorder[preSt];
+            TreeNode root = new TreeNode(rootValue);
+
+            if (preSt == preEnd)
+            {
+                return root;
+            }
+
+            int rootInorder = inSt;
+            while (inorder[rootInorder] != rootValue && rootInorder <= inEnd)
+            {
+                rootInorder++;
+            }
+
+            int leftLength = rootInorder - inSt;
+            int leftPreEnd = preSt + leftLength;
+
+            if (leftLength > 0)
+            {
+                root.left = BuildCore(preorder, preSt + 1, leftPreEnd, inorder, inSt, inEnd);
+            }
+            if (leftLength < preEnd - preSt)
+            {
+                root.right = BuildCore(preorder, leftPreEnd + 1, preEnd, inorder, rootInorder + 1, inEnd);
+            }
+            return root;
+        }
+    }
+}
