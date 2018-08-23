5aecbde5419c21640e3eb45a9ea377eb66dd4221
XuhuiChu <736265181@qq.com>
2018-08-23 13:00:48 +0800

    括号生成

---
+++ LeetCode/Explore/IntermediateAlgorithm/BackTracing/GenerateParenthesisSolution.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode.Explore.IntermediateAlgorithm.BackTracing
+{
+    internal class GenerateParenthesisSolution
+    {
+        public IList<string> GenerateParenthesis(int n)
+        {
+            List<string> result = new List<string>();
+            BackTracking(result, string.Empty, n, n);
+            return result;
+        }
+
+        private void BackTracking(List<string> list, string parenth, int left, int right)
+        {
+            if (left == 0 && right == 0)
+            {
+                list.Add(parenth);
+                return;
+            }
+            if (left > 0)
+            {
+                BackTracking(list, parenth + "(", left - 1, right);
+            }
+            if (right > 0 && left < right)
+            {
+                BackTracking(list, parenth + ")", left, right - 1);
+            }
+        }
+    }
+}
