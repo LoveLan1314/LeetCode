a5f71ecd04a495de6a0e028a81dd2ee59741dd54
XuhuiChu <736265181@qq.com>
2018-08-29 13:10:48 +0800

    子集

---
+++ LeetCode/Explore/IntermediateAlgorithm/BackTracing/SubsetsSolution.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode.Explore.IntermediateAlgorithm.BackTracing
+{
+    internal class SubsetsSolution
+    {
+        public IList<IList<int>> Subsets(int[] nums)
+        {
+            List<IList<int>> result = new List<IList<int>>();
+            BackTrack(result, new List<int>(), nums, 0);
+            return result;
+        }
+
+        private void BackTrack(List<IList<int>> list, List<int> temp, int[] nums, int j)
+        {
+            list.Add(new List<int>(temp));
+            for (int i = j; i < nums.Length; i++)
+            {
+                temp.Add(nums[i]);
+                BackTrack(list, temp, nums, i + 1);
+                temp.RemoveAt(temp.Count - 1);
+            }
+        }
+    }
+}
