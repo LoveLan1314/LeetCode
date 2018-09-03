3dcaf18722833bd2b2363fc8d25368ec195f89c7
XuhuiChu <736265181@qq.com>
2018-09-03 12:47:23 +0800

    前K个高频元素

---
+++ LeetCode/Explore/IntermediateAlgorithm/SortAndSearch/TopKFrequentSolution.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LeetCode.Explore.IntermediateAlgorithm.SortAndSearch
+{
+    class TopKFrequentSolution
+    {
+        public IList<int> TopKFrequent(int[] nums,int k)
+        {
+            Dictionary<int, int> pairs = new Dictionary<int, int>();
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (!pairs.ContainsKey(nums[i]))
+                {
+                    pairs.Add(nums[i], 1);
+                }
+                else
+                {
+                    pairs[nums[i]] = pairs[nums[i]] + 1;
+                }
+            }
+            return pairs.OrderByDescending(i => i.Value).Take(k).Select(i => i.Key).ToList();
+        }
+    }
+}
