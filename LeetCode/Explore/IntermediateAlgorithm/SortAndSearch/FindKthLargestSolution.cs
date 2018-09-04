0a24d62d512367926181e701674a9b0990ad9372
XuhuiChu <736265181@qq.com>
2018-09-04 13:23:39 +0800

    数组中第K个最大元素

---
+++ LeetCode/Explore/IntermediateAlgorithm/SortAndSearch/FindKthLargestSolution.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LeetCode.Explore.IntermediateAlgorithm.SortAndSearch
+{
+    class FindKthLargestSolution
+    {
+        public int FindKthLargest(int[] nums,int k)
+        {
+            return nums.Cast<int>().OrderByDescending(i => i).Skip(k - 1).First();
+        }
+    }
+}
