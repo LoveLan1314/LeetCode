52375e397a4e0045d5381d88757ecdc032652a4e
XuhuiChu <736265181@qq.com>
2018-09-18 13:11:07 +0800

    寻找峰值

---
+++ LeetCode/Explore/IntermediateAlgorithm/SortAndSearch/FindPeakElementSolution.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode.Explore.IntermediateAlgorithm.SortAndSearch
+{
+    class FindPeakElementSolution
+    {
+        public int FindPeakElement(int[] nums)
+        {
+            if(nums.Length == 0)
+            {
+                return 0;
+            }
+            int left = 0;
+            int right = nums.Length - 1;
+            while(left < right)
+            {
+                int mid = (left + right) / 2;
+                if (nums[mid] < nums[mid + 1])
+                {
+                    left = mid + 1;
+                }
+                else
+                {
+                    right = mid;
+                }
+            }
+            return left;
+        }
+    }
+}
