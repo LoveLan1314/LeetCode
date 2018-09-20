76202cfa8f07bb5ffccf3bfec9c6e52dcc8e9848
XuhuiChu <736265181@qq.com>
2018-09-20 13:01:30 +0800

    在排序数组中查找元素的第一个和最后一个位置

---
+++ LeetCode/Explore/IntermediateAlgorithm/SortAndSearch/SearchRangeSolution.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode.Explore.IntermediateAlgorithm.SortAndSearch
+{
+    class SearchRangeSolution
+    {
+        public int[] SearchRange(int[] nums,int target)
+        {
+            int start = -1;
+            int end = -1;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if(nums[i] == target)
+                {
+                    if(start == -1)
+                    {
+                        start = i;
+                    }
+                    end = i;
+                }
+                else if(nums[i] > target)
+                {
+                    break;
+                }
+            }
+            return new int[] { start, end };
+        }
+    }
+}
