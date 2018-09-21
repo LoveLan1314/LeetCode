4ff8416a564c2d825e6fb8212fb8d13ad729664a
XuhuiChu <736265181@qq.com>
2018-09-21 13:08:06 +0800

    合并区间

---
+++ LeetCode/Explore/IntermediateAlgorithm/SortAndSearch/Interval.cs
@@ -0,0 +1,20 @@
+﻿namespace LeetCode.Explore.IntermediateAlgorithm.SortAndSearch
+{
+    public class Interval
+    {
+        public int start;
+        public int end;
+
+        public Interval()
+        {
+            start = 0;
+            end = 0;
+        }
+
+        public Interval(int start, int end)
+        {
+            this.start = start;
+            this.end = end;
+        }
+    }
+}
---
+++ LeetCode/Explore/IntermediateAlgorithm/SortAndSearch/MergeSolution.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LeetCode.Explore.IntermediateAlgorithm.SortAndSearch
+{
+    internal class MergeSolution
+    {
+        public IList<Interval> Merge(IList<Interval> intervals)
+        {
+            if(intervals.Count == 0)
+            {
+                return new List<Interval>();
+            }
+            List<Interval> result = new List<Interval>();
+            List<Interval> list = intervals.Cast<Interval>().ToList();
+            list.Sort(new IntervalCompare());
+            result.Add(list[0]);
+            foreach (var item in list)
+            {
+                if (result.Last().end >= item.start)
+                {
+                    result.Last().end = Math.Max(result.Last().end, item.end);
+                }
+                else
+                {
+                    result.Add(item);
+                }
+            }
+            return result;
+        }
+
+        public class IntervalCompare : IComparer<Interval>
+        {
+            public int Compare(Interval x, Interval y)
+            {
+                if (x.start > y.start)
+                {
+                    return 1;
+                }
+                else if (x.start < y.start)
+                {
+                    return -1;
+                }
+                else
+                {
+                    return 0;
+                }
+            }
+        }
+
+        public IList<Interval> Merge1(IList<Interval> intervals)
+        {
+            List<Interval> result = new List<Interval>();
+            if(intervals == null || intervals.Count < 1)
+            {
+                return result;
+            }
+            List<Interval> list = intervals.OrderBy(i => i.start).ToList();
+            Interval pre = null;
+            foreach (var item in list)
+            {
+                if(pre == null || pre.end < item.start)
+                {
+                    result.Add(item);
+                    pre = item;
+                }
+                else if(pre.end < item.end)
+                {
+                    pre.end = item.end;
+                }
+            }
+            return result;
+        }
+    }
+}
