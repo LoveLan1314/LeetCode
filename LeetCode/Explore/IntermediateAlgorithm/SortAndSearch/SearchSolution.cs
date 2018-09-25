2d4e9afeca0f7f067a5aa633c00d1d3e63c065b3
XuhuiChu <736265181@qq.com>
2018-09-25 13:06:14 +0800

    搜索旋转排序数组

---
+++ LeetCode/Explore/IntermediateAlgorithm/SortAndSearch/SearchSolution.cs
@@ -0,0 +1,46 @@
+﻿namespace LeetCode.Explore.IntermediateAlgorithm.SortAndSearch
+{
+    internal class SearchSolution
+    {
+        public int Search(int[] nums, int target)
+        {
+            if (nums.Length == 0)
+            {
+                return -1;
+            }
+            int left = 0;
+            int right = nums.Length - 1;
+            while (left <= right)
+            {
+                int mid = (left + right) / 2;
+                if (nums[mid] == target)
+                {
+                    return mid;
+                }
+                else if (nums[mid] < nums[right])
+                {
+                    if (nums[mid] < target && nums[right] >= target)
+                    {
+                        left = mid + 1;
+                    }
+                    else
+                    {
+                        right = mid - 1;
+                    }
+                }
+                else
+                {
+                    if (nums[left] <= target && nums[mid] > target)
+                    {
+                        right = mid - 1;
+                    }
+                    else
+                    {
+                        left = mid + 1;
+                    }
+                }
+            }
+            return -1;
+        }
+    }
+}
