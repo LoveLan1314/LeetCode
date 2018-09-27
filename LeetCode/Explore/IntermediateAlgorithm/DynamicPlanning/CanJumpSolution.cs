b7c69d9d68f29c4adae95bbade2b24031d8af42d
XuhuiChu <736265181@qq.com>
2018-09-27 13:10:56 +0800

    跳跃游戏

---
+++ LeetCode/Explore/IntermediateAlgorithm/DynamicPlanning/CanJumpSolution.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LeetCode.Explore.IntermediateAlgorithm.DynamicPlanning
+{
+    internal class CanJumpSolution
+    {
+        public bool CanJump(int[] nums)
+        {
+            List<int> dp = new List<int>() { 0 };
+            for (int i = 1; i < nums.Length; i++)
+            {
+                dp.Add(Math.Max(dp[i - 1], nums[i - 1]) - 1);
+                if (dp[i] < 0)
+                {
+                    return false;
+                }
+            }
+            return dp.Last() >= 0;
+        }
+    }
+}
