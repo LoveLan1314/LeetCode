1d5ab82ea6df596dbe194a4d0e7d5eb7e323f887
LoveLan1314 <736265181@qq.com>
2018-10-31 22:34:50 +0800

    不同路径

---
+++ LeetCode/Explore/PrimaryAlgorithm/DynamicPlanning/UniquePathsSolution.cs
@@ -0,0 +1,25 @@
+﻿namespace LeetCode.Explore.PrimaryAlgorithm.DynamicPlanning
+{
+    internal class UniquePathsSolution
+    {
+        public int UniquePaths(int m, int n)
+        {
+            int[,] nums = new int[m, n];
+            for (int i = 0; i < m; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    if (i == 0 || j == 0)
+                    {
+                        nums[i, j] = 1;
+                    }
+                    else
+                    {
+                        nums[i, j] = nums[i - 1, j] + nums[i, j - 1];
+                    }
+                }
+            }
+            return nums[m - 1, n - 1];
+        }
+    }
+}
