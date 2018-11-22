bc1c3557e4d134a1ff5ceedc9daa7b2f3820c6a6
XuhuiChu <736265181@qq.com>
2018-11-22 13:05:32 +0800

    二叉树序列化未完成

---
+++ LeetCode/Explore/IntermediateAlgorithm/Design/Codec.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode.Explore.IntermediateAlgorithm.Design
+{
+    class Codec
+    {
+        public string Serialize(TreeNode root)
+        {
+
+        }
+
+        public TreeNode Deserialize(string data)
+        {
+
+        } 
+    }
+}
