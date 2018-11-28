ef3403a7f758e23f24a654dde695de228d4af192
LoveLan1314 <736265181@qq.com>
2018-11-28 21:57:58 +0800

    Insert Delete GetRandom O(1)

---
+++ LeetCode/Explore/IntermediateAlgorithm/Design/RandomizedSet.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LeetCode.Explore.IntermediateAlgorithm.Design
+{
+    internal class RandomizedSet
+    {
+        private readonly HashSet<int> vs = new HashSet<int>();
+
+        public RandomizedSet()
+        {
+
+        }
+
+        public bool Insert(int val)
+        {
+            return vs.Add(val);
+        }
+
+        public bool Remove(int val)
+        {
+            return vs.Remove(val);
+        }
+
+        public int GetRandom()
+        {
+            Random random = new Random();
+            int target = random.Next(vs.Count);
+            int index = 0;
+            foreach (var item in vs)
+            {
+                if (index == target)
+                {
+                    return item;
+                }
+                index++;
+            }
+            return 0;
+        }
+    }
+}
