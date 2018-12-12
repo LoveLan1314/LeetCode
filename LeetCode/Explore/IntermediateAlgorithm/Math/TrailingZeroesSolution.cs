7718aa8edc00da15d27aaa4c95584199eaca13f3
XuhuiChu <736265181@qq.com>
2018-12-12 11:38:45 +0800

    阶乘后的零

---
+++ LeetCode/Explore/IntermediateAlgorithm/Math/TrailingZeroesSolution.cs
@@ -0,0 +1,16 @@
+﻿namespace LeetCode.Explore.IntermediateAlgorithm.Math
+{
+    internal class TrailingZeroesSolution
+    {
+        public int TrailingZeroes(int n)
+        {
+            int sum = 0;
+            while (n != 0)
+            {
+                n /= 5;
+                sum += n;
+            }
+            return sum;
+        }
+    }
+}
