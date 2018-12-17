763542ddd6dab03ce92cdca074f972e1db0f2bf5
XuhuiChu <736265181@qq.com>
2018-12-17 20:35:25 +0800

    Pow(x, n)

---
+++ LeetCode/Explore/IntermediateAlgorithm/Math/MyPowSolution.cs
@@ -0,0 +1,34 @@
+﻿namespace LeetCode.Explore.IntermediateAlgorithm.Math
+{
+    internal class MyPowSolution
+    {
+        public double MyPow(double x, int n)
+        {
+            if (n < 0)
+            {
+                return 1 / Power(x, -n);
+            }
+            else
+            {
+                return Power(x, n);
+            }
+        }
+
+        private double Power(double x, int n)
+        {
+            if (n == 0)
+            {
+                return 1;
+            }
+            double half = Power(x, n / 2);
+            if (n % 2 == 0)
+            {
+                return half * half;
+            }
+            else
+            {
+                return x * half * half;
+            }
+        }
+    }
+}
