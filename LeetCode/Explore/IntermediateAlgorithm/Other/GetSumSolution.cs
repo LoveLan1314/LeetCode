9a697a0320ed9be740c1e3187f9156733fbf9468
XuhuiChu <736265181@qq.com>
2018-12-21 13:01:43 +0800

    两整数之和

---
+++ LeetCode/Explore/IntermediateAlgorithm/Other/GetSumSolution.cs
@@ -0,0 +1,23 @@
+﻿namespace LeetCode.Explore.IntermediateAlgorithm.Other
+{
+    internal class GetSumSolution
+    {
+        public int GetSum(int a, int b)
+        {
+            int res = 0;
+            int xor = a ^ b;
+
+            int forward = (a & b) << 1;
+            if (forward != 0)
+            {
+                res = GetSum(xor, forward);
+            }
+            else
+            {
+                res = xor;
+            }
+
+            return res;
+        }
+    }
+}
