bb701ba56654c94ed2f4df8c05d40475193bf26d
XuhuiChu <736265181@qq.com>
2018-12-27 13:18:26 +0800

    Task Scheduler

---
+++ LeetCode/Explore/IntermediateAlgorithm/Other/LeastIntervalSolution.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode.Explore.IntermediateAlgorithm.Other
+{
+    internal class LeastIntervalSolution
+    {
+        public int LeastInterval(char[] tasks, int n)
+        {
+            List<int> cnt = new List<int> { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+            foreach (var item in tasks)
+            {
+                cnt[item - 'A']++;
+            }
+            cnt.Sort();
+            int i = 25;
+            int mx = cnt[25];
+            int len = tasks.Length;
+            while (i >= 0 && cnt[i] == mx)
+            {
+                i--;
+            }
+            return System.Math.Max(len, (mx - 1) * (n + 1) + 25 - i);
+        }
+    }
+}
