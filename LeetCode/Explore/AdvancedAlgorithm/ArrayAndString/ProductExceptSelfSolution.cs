8b4ab69781c71c24693cb47fba47d50e829e7089
XuhuiChu <736265181@qq.com>
2018-12-28 13:18:59 +0800

    除自身以外数组的乘积

---
+++ LeetCode/Explore/AdvancedAlgorithm/ArrayAndString/ProductExceptSelfSolution.cs
@@ -0,0 +1,22 @@
+﻿namespace LeetCode.Explore.AdvancedAlgorithm.ArrayAndString
+{
+    internal class ProductExceptSelfSolution
+    {
+        public int[] ProductExceptSelf(int[] nums)
+        {
+            int[] vs = new int[nums.Length];
+            vs[0] = 1;
+            for (int i = 1; i < nums.Length; i++)
+            {
+                vs[i] = vs[i - 1] * nums[i - 1];
+            }
+            int right = 1;
+            for (int i = nums.Length - 1; i >= 0; i--)
+            {
+                vs[i] *= right;
+                right *= nums[i];
+            }
+            return vs;
+        }
+    }
+}
