846a345697e075d96c5b52966533a1e0836bd6fc
LoveLan1314 <736265181@qq.com>
2019-03-13 22:10:09 +0800

    盛最多水的容器

---
+++ LeetCode/Explore/AdvancedAlgorithm/ArrayAndString/MaxAreaSolution.cs
@@ -0,0 +1,33 @@
+﻿namespace LeetCode.Explore.AdvancedAlgorithm.ArrayAndString
+{
+    internal class MaxAreaSolution
+    {
+        public int MaxArea(int[] height)
+        {
+            int left = 0;
+            int right = height.Length - 1;
+            int max = 0;
+            while (left < right)
+            {
+                int leftLengh = height[left];
+                int rightLengh = height[right];
+                int area;
+                if (leftLengh > rightLengh)
+                {
+                    area = rightLengh * (right - left);
+                    right--;
+                }
+                else
+                {
+                    area = leftLengh * (right - left);
+                    left++;
+                }
+                if (area > max)
+                {
+                    max = area;
+                }
+            }
+            return max;
+        }
+    }
+}
