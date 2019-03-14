e16866edf785123094b2f717bb10249e8d59825e
LoveLan1314 <736265181@qq.com>
2019-03-14 22:34:01 +0800

    生命游戏

---
+++ LeetCode/Explore/AdvancedAlgorithm/ArrayAndString/GameOfLifeSolution.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode.Explore.AdvancedAlgorithm.ArrayAndString
+{
+    class GameOfLifeSolution
+    {
+        //引入四种状态
+        //状态0： 死细胞转为死细胞
+        //状态1： 活细胞转为活细胞
+        //状态2： 活细胞转为死细胞
+        //状态3： 死细胞转为活细胞
+        //最后对2取余即可
+        public void GameOfLife(int[][] board)
+        {
+            int m = board.Length;
+            int n = board[0].Length;
+            int[] dx = new int[] { -1, -1, -1, 0, 1, 1, 1, 0 };
+            int[] dy = new int[] { -1, 0, 1, 1, 1, 0, -1, -1 };
+            for (int i = 0; i < m; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    int cnt = 0;
+                    for (int k = 0; k < 8; k++)
+                    {
+                        int x = i + dx[k];
+                        int y = j + dy[k];
+                        if (x >= 0 && x < m && y >= 0 && y < n && (board[x][y] == 1 || board[x][y] == 2))
+                        {
+                            cnt++;
+                        }
+                    }
+                    if (board[i][j] == 1 && (cnt < 2 || cnt > 3))
+                    {
+                        board[i][j] = 2;
+                    }
+                    else if (board[i][j] == 0 && cnt == 3)
+                    {
+                        board[i][j] = 3;
+                    }
+                }
+            }
+            for (int i = 0; i < m; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    board[i][j] %= 2;
+                }
+            }
+        }
+    }
+}
