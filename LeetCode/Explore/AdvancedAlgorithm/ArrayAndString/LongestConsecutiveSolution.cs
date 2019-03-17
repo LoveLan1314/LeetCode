fe5ace866be2952d720c019821612f9fbe608890
LoveLan1314 <736265181@qq.com>
2019-03-17 19:24:56 +0800

    最长连续序列

---
+++ LeetCode/Explore/AdvancedAlgorithm/ArrayAndString/LongestConsecutiveSolution.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode.Explore.AdvancedAlgorithm.ArrayAndString
+{
+    internal class LongestConsecutiveSolution
+    {
+        public int LongestConsecutive(int[] nums)
+        {
+            if (nums.Length == 0)
+            {
+                return 0;
+            }
+            Dictionary<int, int> pairs = new Dictionary<int, int>();
+            foreach (var item in nums)
+            {
+                if (!pairs.ContainsKey(item))
+                {
+                    pairs.Add(item, 1);
+                    int temp = item;
+                    bool hasBig = false;
+                    if (pairs.ContainsKey(temp + 1))
+                    {
+                        while (pairs.ContainsKey(temp + 1))
+                        {
+                            temp++;
+                        }
+                        pairs[temp]++;
+                        pairs[item] = pairs[temp];
+                        hasBig = true;
+                    }
+                    int temp2 = item;
+                    if (pairs.ContainsKey(temp2 - 1))
+                    {
+                        while (pairs.ContainsKey(temp2 - 1))
+                        {
+                            temp2--;
+                        }
+                        if (hasBig)
+                        {
+                            pairs[temp] += (item - temp2);
+                            pairs[temp2] = pairs[temp];
+                        }
+                        else
+                        {
+                            pairs[temp2]++;
+                            pairs[item] = pairs[temp2];
+                        }
+                    }
+                }
+            }
+            int max = int.MinValue;
+            foreach (var item in pairs)
+            {
+                if (max < item.Value)
+                {
+                    max = item.Value;
+                }
+            }
+            return max;
+        }
+    }
+}
