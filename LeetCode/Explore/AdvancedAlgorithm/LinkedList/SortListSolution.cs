dc14b855cc9f9bd4c49709b0c3e4dbf65008e2b9
LoveLan1314 <736265181@qq.com>
2019-04-10 00:24:10 +0800

    排序链表

---
+++ LeetCode/Explore/AdvancedAlgorithm/LinkedList/SortListSolution.cs
@@ -0,0 +1,48 @@
+﻿namespace LeetCode.Explore.AdvancedAlgorithm.LinkedList
+{
+    internal class SortListSolution
+    {
+        public ListNode SortList(ListNode head)
+        {
+            if (head == null || head.next == null)
+            {
+                return head;
+            };
+            QsortList(head, null);
+            return head;
+        }
+
+        private void QsortList(ListNode head, ListNode tail)
+        {
+            if (head != tail && head.next != tail)
+            {
+                ListNode mid = PartitionList(head, tail);
+                QsortList(head, mid);
+                QsortList(mid.next, tail);
+            }
+        }
+
+        private ListNode PartitionList(ListNode low, ListNode high)
+        {
+            int key = low.val;
+            ListNode loc = low;
+            for (ListNode i = low.next; i != high; i = i.next)
+            {
+                if (i.val < key)
+                {
+                    loc = loc.next;
+                    Swap(i, loc);
+                }
+            }
+            Swap(loc, low);
+            return loc;
+        }
+
+        private void Swap(ListNode left, ListNode right)
+        {
+            int temp = right.val;
+            right.val = left.val;
+            left.val = temp;
+        }
+    }
+}
