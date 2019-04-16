10933773f69d7e1919549b9f4c857142fbf309b1
LoveLan1314 <736265181@qq.com>
2019-04-17 00:03:06 +0800

    32.最长有效括号

---
+++ LeetCode/Algorithms/Fifty/LongestValidParenthesesSolution.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LeetCode.Algorithms.Fifty
+{
+    internal class LongestValidParenthesesSolution
+    {
+        public int LongestValidParentheses(string s)
+        {
+            int max = 0;
+            int start = 0;
+            if (s == null)
+            {
+                return 0;
+            }
+
+            int len = s.Length;
+
+            Stack<int> stack = new Stack<int>();
+            for (int i = 0; i < len; i++)
+            {
+                if (s[i] == '(')
+                {
+                    stack.Push(i);
+                }
+                else
+                {
+                    if (stack.Count == 0)
+                    {
+                        start = i + 1;
+                    }
+                    else
+                    {
+                        stack.Pop();
+                        if (stack.Count == 0)
+                        {
+                            max = Math.Max(max, i - start + 1);
+                        }
+                        else
+                        {
+                            max = Math.Max(max, i - stack.Peek());
+                        }
+                    }
+                }
+            }
+            return max;
+        }
+    }
+}
