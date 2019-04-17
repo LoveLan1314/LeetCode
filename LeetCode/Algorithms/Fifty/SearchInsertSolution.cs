5c39b5fdaf77c237c46adca3774e383bd9fe475f
LoveLan1314 <736265181@qq.com>
2019-04-17 22:51:01 +0800

    35.搜索插入位置

---
+++ LeetCode/Algorithms/Fifty/SearchInsertSolution.cs
@@ -0,0 +1,21 @@
+﻿namespace LeetCode.Algorithms.Fifty
+{
+    internal class SearchInsertSolution
+    {
+        public int SearchInsert(int[] nums, int target)
+        {
+            if (nums == null)
+            {
+                return 0;
+            }
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (nums[i] >= target)
+                {
+                    return i;
+                }
+            }
+            return nums.Length;
+        }
+    }
+}
