c7da44c63deb2097098d8f5bc9f5bcff92536b5a
LoveLan1314 <736265181@qq.com>
2019-08-17 21:27:29 +0800

    寻找数组的中心索引

---
+++ LeetCode/DataStructure/ArrayAndString/PivotIndexSolution.cs
@@ -0,0 +1,22 @@
+﻿using System.Linq;
+
+namespace LeetCode.DataStructure.ArrayAndString
+{
+    internal class PivotIndexSolution
+    {
+        public int PivotIndex(int[] nums)
+        {
+            int sum = nums.Sum();
+            int total = 0;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (total == (double)(sum - nums[i]) / 2)
+                {
+                    return i;
+                }
+                total += nums[i];
+            }
+            return -1;
+        }
+    }
+}
--- LeetCode/Program.cs
+++ LeetCode/Program.cs
@@ -1,10 +1,14 @@
-﻿namespace LeetCode
+﻿using LeetCode.DataStructure.ArrayAndString;
+
+namespace LeetCode
 {
     public class Program
     {
         public static void Main(string[] args)
         {
-
+            PivotIndexSolution solution = new PivotIndexSolution();
+            solution.PivotIndex(new int[] { -1, -1, -1, -1, -1, -1 });
         }
+
     }
 }
