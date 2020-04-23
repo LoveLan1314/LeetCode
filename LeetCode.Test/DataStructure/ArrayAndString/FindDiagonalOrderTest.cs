cdf2657f4cac92b8f45096df315aca94b1700282
XuhuiChu <736265181@qq.com>
2020-04-23 11:04:47 +0800

    对角线遍历

---
+++ LeetCode.Test/DataStructure/ArrayAndString/FindDiagonalOrderTest.cs
@@ -0,0 +1,27 @@
+﻿using LeetCode.DataStructure.ArrayAndString;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LeetCode.Test.DataStructure.ArrayAndString
+{
+    [TestClass]
+    public class FindDiagonalOrderTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var solution = new FindDiagonalOrderSolution();
+            var result = solution.FindDiagonalOrder(new int[][]
+            {
+                new int[] { 1, 2, 3 },
+                new int[] { 4, 5, 6},
+                new int[] { 7, 8, 9}
+            });
+            var rightResult = new int[] { 1, 2, 4, 7, 5, 3, 6, 8, 9 };
+            Assert.IsTrue(result.Length == rightResult.Length);
+            for (int i = 0; i < result.Length; i++)
+            {
+                Assert.IsTrue(result[i] == rightResult[i]);
+            }
+        }
+    }
+}
---
+++ LeetCode/DataStructure/ArrayAndString/FindDiagonalOrderSolution.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode.DataStructure.ArrayAndString
+{
+    internal sealed class FindDiagonalOrderSolution
+    {
+        public int[] FindDiagonalOrder(int[][] matrix)
+        {
+            if (matrix.Length == 0)
+            {
+                return new int[] { };
+            }
+            int m = matrix.Length;
+            int n = matrix[0].Length;
+            List<int> result = new List<int>();
+            for (int curveLine = 0; curveLine < m + n - 1; curveLine++)
+            {
+                int rowBegin;
+                int rowEnd;
+                if (curveLine < n - 1)
+                {
+                    rowBegin = 0;
+                }
+                else
+                {
+                    rowBegin = curveLine + 1 - n;
+                }
+                if (curveLine + 1 >= m)
+                {
+                    rowEnd = m - 1;
+                }
+                else
+                {
+                    rowEnd = curveLine;
+                }
+
+                if (curveLine % 2 == 1)
+                {
+                    for (int i = rowBegin; i <= rowEnd; i++)
+                    {
+                        result.Add(matrix[i][curveLine - i]);
+                    }
+                }
+                else
+                {
+                    for (int i = rowEnd; i >= rowBegin; i--)
+                    {
+                        result.Add(matrix[i][curveLine - i]);
+                    }
+                }
+            }
+            return result.ToArray();
+        }
+    }
+}
