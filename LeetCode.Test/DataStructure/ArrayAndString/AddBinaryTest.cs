5e55b9903414936d2758593e2436a2cb1c19ecdb
XuhuiChu <736265181@qq.com>
2020-04-23 11:34:16 +0800

    二进制求和

---
+++ LeetCode.Test/DataStructure/ArrayAndString/AddBinaryTest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using LeetCode.DataStructure.ArrayAndString;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LeetCode.Test.DataStructure.ArrayAndString
+{
+    [TestClass]
+    public sealed class AddBinaryTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var solution = new AddBinarySolution();
+            var result = solution.AddBinary("11", "1");
+            Assert.IsTrue(result == "100");
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            var solution = new AddBinarySolution();
+            var result = solution.AddBinary("1010", "1011");
+            Assert.IsTrue(result == "10101");
+        }
+    }
+}
---
+++ LeetCode/DataStructure/ArrayAndString/AddBinarySolution.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LeetCode.DataStructure.ArrayAndString
+{
+    internal sealed class AddBinarySolution
+    {
+        public string AddBinary(string a, string b)
+        {
+            var aList = a.ToCharArray().Reverse().ToList();
+            var bList = b.ToCharArray().Reverse().ToList();
+            var length = Math.Max(aList.Count, bList.Count);
+            List<char> result = new List<char>();
+            char carryFlag = '0';
+            void AddValueToResult(char aChar, char bChar)
+            {
+                if (aChar == '0' && bChar == '0')
+                {
+                    result.Add(carryFlag);
+                    carryFlag = '0';
+                }
+                else if (aChar == '1' && bChar == '1')
+                {
+                    result.Add(carryFlag);
+                    carryFlag = '1';
+                }
+                else
+                {
+                    if (carryFlag == '1')
+                    {
+                        result.Add('0');
+                    }
+                    else
+                    {
+                        result.Add('1');
+                    }
+                }
+            }
+            for (int i = 0; i < length; i++)
+            {
+                if (aList.Count == i)
+                {
+                    for (int j = i; j < length; j++)
+                    {
+                        AddValueToResult(bList[j], '0');
+                    }
+                    break;
+                }
+                if (bList.Count == i)
+                {
+                    for (int j = i; j < length; j++)
+                    {
+                        AddValueToResult(aList[j], '0');
+                    }
+                    break;
+                }
+                AddValueToResult(aList[i], bList[i]);
+            }
+            if (carryFlag == '1')
+            {
+                result.Add(carryFlag);
+            }
+            result.Reverse();
+            return new string(result.ToArray());
+        }
+    }
+}
