51aea585b022d292d2b5b95062ac6ca0ce3e8252
LoveLan1314 <736265181@qq.com>
2020-09-23 22:48:55 +0800

    合并两个有序数组

---
+++ LeetCode/AlgorithmIntervie/BeforeStart/MergeSolution.cs
@@ -0,0 +1,29 @@
+﻿namespace LeetCode.AlgorithmIntervie.BeforeStart
+{
+    internal sealed class MergeSolution
+    {
+        public void Merge(int[] nums1, int m, int[] nums2, int n)
+        {
+            int i = m - 1;
+            int j = n - 1;
+            for (; i >= 0 && j >= 0;)
+            {
+                if (nums1[i] > nums2[j])
+                {
+                    nums1[i + j + 1] = nums1[i];
+                    i--;
+                }
+                else
+                {
+                    nums1[i + j + 1] = nums2[j];
+                    j--;
+                }
+            }
+            while (j >= 0)
+            {
+                nums1[j] = nums2[j];
+                j--;
+            }
+        }
+    }
+}
