ef4a50918e88099af9738585864886eff1f7c1fc
LoveLan1314 <736265181@qq.com>
2020-11-08 21:39:03 +0800

    有效的字母异位词 字符串中的第一唯一字符

---
+++ LeetCode/AlgorithmIntervie/String/FirstUniqCharSolution.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode.AlgorithmIntervie.String
+{
+    internal sealed class FirstUniqCharSolution
+    {
+        public int FirstUniqChar(string s)
+        {
+            Dictionary<char, int> pairs = new Dictionary<char, int>();
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (pairs.ContainsKey(s[i]))
+                {
+                    pairs[s[i]] = -1;
+                }
+                else
+                {
+                    pairs.Add(s[i], i);
+                }
+            }
+            foreach (var item in pairs)
+            {
+                if (item.Value != -1)
+                {
+                    return item.Value;
+                }
+            }
+            return -1;
+        }
+
+        public int FirstUniqChar2(string s)
+        {
+            if (s == null || s.Length < 1)
+            {
+                return -1;
+            }
+            int[] temp = new int[26];
+            for (int i = 0; i < s.Length; i++)
+            {
+                temp[s[i] - 'a']++;
+            }
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (temp[s[i] - 'a'] == 1)
+                {
+                    return i;
+                }
+            }
+            return -1;
+        }
+    }
+}
---
+++ LeetCode/AlgorithmIntervie/String/IsAnagramSolution.cs
@@ -0,0 +1,30 @@
+﻿namespace LeetCode.AlgorithmIntervie.String
+{
+    internal sealed class IsAnagramSolution
+    {
+        public bool IsAnagram(string s, string t)
+        {
+            if (s.Length != t.Length)
+            {
+                return false;
+            }
+
+            int[] sList = new int[26];
+            int[] tList = new int[26];
+            for (int i = 0; i < s.Length; i++)
+            {
+                sList[s[i] - 97]++;
+                tList[t[i] - 97]++;
+            }
+
+            for (int i = 0; i < 26; i++)
+            {
+                if (sList[i] != tList[i])
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
