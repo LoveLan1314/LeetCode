44886bc7bdbcb7b52b0467df5546e46e14dd9721
XuhuiChu <736265181@qq.com>
2020-11-10 12:28:15 +0800

    多数元素

---
+++ LeetCode/AlgorithmIntervie/Array/MajorityElementSolution.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode.AlgorithmIntervie.Array
+{
+    internal sealed class MajorityElementSolution
+    {
+        public int MajorityElement(int[] nums)
+        {
+            int result = nums[0];
+            int cnt = 1;
+            for (int i = 1; i < nums.Length; i++)
+            {
+                if (nums[i] == result)
+                {
+                    cnt++;
+                }
+                else
+                {
+                    cnt--;
+                    if (cnt == 0)
+                    {
+                        result = nums[i];
+                        cnt = 1;
+                    }
+                }
+            }
+            return result;
+        }
+    }
+}
