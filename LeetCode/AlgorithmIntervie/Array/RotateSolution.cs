fe9832b65745d306b9665426bb3eeeeec2b213c8
XuhuiChu <736265181@qq.com>
2020-11-11 18:18:22 +0800

    旋转数组

---
+++ LeetCode/AlgorithmIntervie/Array/RotateSolution.cs
@@ -0,0 +1,33 @@
+﻿namespace LeetCode.AlgorithmIntervie.Array
+{
+    internal sealed class RotateSolution
+    {
+        public void Rotate(int[] nums, int k)
+        {
+            if (nums == null || nums.Length == 0 || k % nums.Length == 0)
+            {
+                return;
+            }
+
+            int turns = k % nums.Length;
+            int middle = nums.Length - turns;
+
+            Reverse(nums, 0, middle - 1);
+            Reverse(nums, middle, nums.Length - 1);
+            Reverse(nums, 0, nums.Length - 1);
+        }
+
+        private void Reverse(int[] arr, int s, int e)
+        {
+            while (s < e)
+            {
+                int temp = arr[s];
+                arr[s] = arr[e];
+                arr[e] = temp;
+
+                s++;
+                e--;
+            }
+        }
+    }
+}
