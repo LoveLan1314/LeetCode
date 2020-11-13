02d9fd7327e0df5502c8e33a53e1741963ea1b56
LoveLan1314 <736265181@qq.com>
2020-11-13 21:39:50 +0800

    打乱数组

---
+++ LeetCode/AlgorithmIntervie/Array/ShuffleSolution.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode.AlgorithmIntervie.Array
+{
+    internal sealed class ShuffleSolution
+    {
+        private readonly int[] _nums;
+        private readonly int[] _originNums;
+        private readonly Random _random = new Random();
+        public ShuffleSolution(int[] nums)
+        {
+            _nums = nums;
+            _originNums = new int[nums.Length];
+            nums.CopyTo(_originNums, 0);
+        }
+
+        public int[] Reset()
+        {
+            return _originNums;
+        }
+
+        public int[] Shuffle()
+        {
+            int length = _nums.Length;
+            for (int i = length; i > 0; i--)
+            {
+                int randInd = _random.Next(i);
+                int temp = _nums[randInd];
+                _nums[randInd] = _nums[i - 1];
+                _nums[i - 1] = temp;
+            }
+            return _nums;
+        }
+    }
+}
