8c54cee3679b7a9c971627ba49ab200b2f791a22
LoveLan1314 <736265181@qq.com>
2020-11-15 21:12:56 +0800

    递增的三元子序列

---
+++ LeetCode/AlgorithmIntervie/Array/IncreasingTripletSolution.cs
@@ -0,0 +1,27 @@
+﻿namespace LeetCode.AlgorithmIntervie.Array
+{
+    internal sealed class IncreasingTripletSolution
+    {
+        public bool IncreasingTriplet(int[] nums)
+        {
+            int m1 = int.MaxValue;
+            int m2 = int.MaxValue;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (m1 >= nums[i])
+                {
+                    m1 = nums[i];
+                }
+                else if (m2 >= nums[i])
+                {
+                    m2 = nums[i];
+                }
+                else
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+}
