9c22957eb61642c4853e3d374040d3a63d0c51db
LoveLan1314 <736265181@qq.com>
2020-11-16 22:41:59 +0800

    探索二维矩阵Ⅱ

---
+++ LeetCode/AlgorithmIntervie/Array/SearchMatrixSolution.cs
@@ -0,0 +1,40 @@
+﻿namespace LeetCode.AlgorithmIntervie.Array
+{
+    internal sealed class SearchMatrixSolution
+    {
+        public bool SearchMatrix(int[,] matrix, int target)
+        {
+            int len = matrix.GetLength(0);
+            int row = matrix.GetLength(1);
+            if (len == 0 || row == 0)
+            {
+                return false;
+            }
+            if (matrix[0, 0] > target || matrix[len - 1, row - 1] < target)
+            {
+                return false;
+            }
+            int x = len - 1;
+            int y = 0;
+            while (true)
+            {
+                if (matrix[x, y] > target)
+                {
+                    x--;
+                }
+                else if (matrix[x, y] < target)
+                {
+                    y++;
+                }
+                else
+                {
+                    return true;
+                }
+                if (x < 0 || y >= row)
+                {
+                    return false;
+                }
+            }
+        }
+    }
+}
