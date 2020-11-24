c853bacf6ab2cb14ceeb120285b4c05af175898d
LoveLan1314 <736265181@qq.com>
2020-11-24 21:44:38 +0800

    前K个高频元素

---
+++ LeetCode/AlgorithmIntervie/StackAndArray/TopKFrequentSolution.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace LeetCode.AlgorithmIntervie.StackAndArray
+{
+    internal sealed class TopKFrequentSolution
+    {
+        public int[] TopKFrequent(int[] nums, int k)
+        {
+            Dictionary<int, int> pairs = new Dictionary<int, int>();
+            for (int i = 0; i < nums.Length; i++)
+            {
+                var num = nums[i];
+                if (pairs.ContainsKey(num))
+                {
+                    pairs[num]++;
+                }
+                else
+                {
+                    pairs.Add(num, 1);
+                }
+            }
+            return pairs.OrderByDescending(i => i.Value).Take(k).Select(i => i.Key).ToArray();
+        }
+    }
+}
