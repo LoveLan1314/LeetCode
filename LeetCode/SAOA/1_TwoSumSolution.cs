c731f60736f524a459cd01ddd962bcda2d3ab7e1
LoveLan1314 <736265181@qq.com>
2021-03-08 22:34:15 +0800

    1.两数之和

---
+++ LeetCode/SAOA/1_TwoSumSolution.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.SAOA
+{
+    internal class TwoSumSolution
+    {
+        public int[] TwoSum(int[] nums, int target)
+        {
+            for (int i = 0; i < nums.Length; i++)
+            {
+                for (int j = i + 1; j < nums.Length; j++)
+                {
+                    if (nums[i] + nums[j] == target)
+                    {
+                        return new int[] { i, j };
+                    }
+                }
+            }
+            return null;
+        }
+
+        public int[] TwoSum1(int[] nums, int target)
+        {
+            Dictionary<int, int> pairs = new Dictionary<int, int>();
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (pairs.ContainsKey(target - nums[i]))
+                {
+                    return new int[] { pairs[target - nums[i]], i };
+                }
+                else
+                {
+                    pairs.Add(nums[i], i);
+                }
+            }
+            return null;
+        }
+    }
+}
