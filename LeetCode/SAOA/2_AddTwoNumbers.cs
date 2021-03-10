ffce115e69fc9e545b1becf208324792c6d5e889
LoveLan1314 <736265181@qq.com>
2021-03-10 22:33:29 +0800

    2.两数相加

---
+++ LeetCode/SAOA/2_AddTwoNumbers.cs
@@ -0,0 +1,43 @@
+﻿namespace LeetCode.SAOA
+{
+    internal sealed class AddTwoNumbersSolution
+    {
+        public ListNode AddTwoNumbers(ListNode l1, ListNode l2)
+        {
+            ListNode result = new ListNode();
+            var root = result;
+            bool needAdd = false;
+            do
+            {
+                int value = 0;
+                if (l1 != null)
+                {
+                    value += l1.val;
+                    l1 = l1.next;
+                }
+                if (l2 != null)
+                {
+                    value += l2.val;
+                    l2 = l2.next;
+                }
+                if (needAdd)
+                {
+                    value++;
+                    needAdd = false;
+                }
+                if (value >= 10)
+                {
+                    needAdd = true;
+                    value -= 10;
+                }
+                result.next = new ListNode(value);
+                result = result.next;
+            } while (l1 != null || l2 != null);
+            if (needAdd)
+            {
+                result.next = new ListNode(1);
+            }
+            return root.next;
+        }
+    }
+}
---
+++ LeetCode/SAOA/ListNode.cs
@@ -0,0 +1,13 @@
+﻿namespace LeetCode.SAOA
+{
+    internal sealed class ListNode
+    {
+        public int val;
+        public ListNode next;
+        public ListNode(int val = 0, ListNode next = null)
+        {
+            this.val = val;
+            this.next = next;
+        }
+    }
+}
