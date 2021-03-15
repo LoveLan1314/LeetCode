81fd2133e49d2fc238c0c1959120a4d4aa0667c3
LoveLan1314 <736265181@qq.com>
2021-03-15 22:11:49 +0800

    5.最长回文串

---
+++ LeetCode/SAOA/5_LongestPalindrome.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.SAOA
+{
+    internal sealed class LongestPalindromeSolution
+    {
+        public string LongestPalindrome(string s)
+        {
+            int n = s.Length;
+            bool[,] dp = new bool[n, n];
+            string ans = "";
+            for (int l = 0; l < n; ++l)
+            {
+                for (int i = 0; i + l < n; ++i)
+                {
+                    int j = i + l;
+                    if (l == 0)
+                    {
+                        dp[i, j] = true;
+                    }
+                    else if (l == 1)
+                    {
+                        dp[i, j] = s[i] == s[j];
+                    }
+                    else
+                    {
+                        dp[i, j] = s[i] == s[j] && dp[i + 1, j - 1];
+                    }
+                    if (dp[i, j] && l + 1 > ans.Length)
+                    {
+                        ans = s.Substring(i, l + 1);
+                    }
+                }
+            }
+            return ans;
+        }
+    }
+}
