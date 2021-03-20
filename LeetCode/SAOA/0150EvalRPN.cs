6954a4486cd3840ce5e1c5d4d773790ca3cd8a01
LoveLan1314 <736265181@qq.com>
2021-03-20 14:57:17 +0800

    150. 逆波兰表达式求值

---
+++ LeetCode/SAOA/0150EvalRPN.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode.SAOA.One
+{
+    internal sealed class EvalRPNSolution
+    {
+        private static readonly string[] _operationKeys = new string[] { "+", "-", "*", "/" };
+        public int EvalRPN(string[] tokens)
+        {
+            Stack<int> stack = new Stack<int>();
+            for (int i = 0; i < tokens.Length; i++)
+            {
+                var value = tokens[i];
+                switch (value)
+                {
+                    case "+":
+                        {
+                            int value2 = stack.Pop();
+                            int value1 = stack.Pop();
+                            stack.Push(value1 + value2);
+                        }
+                        break;
+                    case "-":
+                        {
+                            int value2 = stack.Pop();
+                            int value1 = stack.Pop();
+                            stack.Push(value1 - value2);
+                        }
+                        break;
+                    case "*":
+                        {
+                            int value2 = stack.Pop();
+                            int value1 = stack.Pop();
+                            stack.Push(value1 * value2);
+                        }
+                        break;
+                    case "/":
+                        {
+                            int value2 = stack.Pop();
+                            int value1 = stack.Pop();
+                            stack.Push(value1 / value2);
+                        }
+                        break;
+                    default:
+                        stack.Push(int.Parse(value));
+                        break;
+                }
+            }
+            return stack.Pop();
+        }
+    }
+}
