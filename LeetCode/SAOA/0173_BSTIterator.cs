930c0c6a2b781f434d8f5c92ca062653a8637190
LoveLan1314 <736265181@qq.com>
2021-03-28 15:59:31 +0800

    173. 二叉搜索树迭代器

---
+++ LeetCode/SAOA/0173_BSTIterator.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode.SAOA
+{
+    internal sealed class BSTIteratorSolution
+    {
+        private TreeNode _node;
+        private readonly Stack<TreeNode> _stack = new Stack<TreeNode>();
+        public BSTIteratorSolution(TreeNode root)
+        {
+            _node = root;
+        }
+
+        public int Next()
+        {
+            while (_node != null)
+            {
+                _stack.Push(_node);
+                _node = _node.left;
+            }
+            _node = _stack.Pop();
+            var result = _node.val;
+            _node = _node.right;
+            return result;
+        }
+
+        public bool HasNext()
+        {
+            return _node != null || _stack.Count > 0;
+        }
+    }
+}
