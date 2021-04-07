d3f1a9d63e98acf60758c628190b7958af2b1831
LoveLan1314 <736265181@qq.com>
2021-04-07 20:28:25 +0800

    81. 搜索旋转排序数组 II

---
+++ LeetCode/SAOA/0081_Search.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.SAOA
+{
+    internal sealed class SearchSolution
+    {
+        public bool Search(int[] nums, int target)
+        {
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (nums[i] == target)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public bool Search2(int[] nums, int target)
+        {
+            int n = nums.Length;
+            if (n == 0)
+            {
+                return false;
+            }
+            if (n == 1)
+            {
+                return nums[0] == target;
+            }
+
+            int l = 0, r = n - 1;
+            while (l <= r)
+            {
+                int mid = (l + r) / 2;
+                if (nums[mid] == target)
+                {
+                    return true;
+                }
+                if (nums[l] == nums[mid] && nums[r] == nums[mid])
+                {
+                    l++;
+                    r--;
+                }
+                else if (nums[l] <= nums[mid])
+                {
+                    if (nums[l] <= target && target < nums[mid])
+                    {
+                        r = mid - 1;
+                    }
+                    else
+                    {
+                        l = mid + 1;
+                    }
+                }
+                else
+                {
+                    if (nums[mid] < target && target <= nums[n - 1])
+                    {
+                        l = mid + 1;
+                    }
+                    else
+                    {
+                        r = mid - 1;
+                    }
+                }
+            }
+            return false;
+        }
+    }
+}
