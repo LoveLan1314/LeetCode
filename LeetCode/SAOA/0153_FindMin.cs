d95507574d3c50f1f5a0dd5dd0173f879a18fe14
XuhuiChu <736265181@qq.com>
2021-04-08 12:16:04 +0800

    153. 寻找旋转排序数组中的最小值

---
+++ LeetCode/SAOA/0153_FindMin.cs
@@ -0,0 +1,19 @@
+﻿namespace LeetCode.SAOA
+{
+    internal sealed class FindMinSolution
+    {
+        public int FindMin(int[] nums)
+        {
+            int min = nums[0];
+            for (int i = 1; i < nums.Length; i++)
+            {
+                //排序数组旋转后得到，则出现的第一个小于第一个值的数就是最小值
+                if (nums[i] < min)
+                {
+                    return nums[i];
+                }
+            }
+            return min;
+        }
+    }
+}
