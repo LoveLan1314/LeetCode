0433a8535c9441e1334eec77c4b83b780cc13cff
LoveLan1314 <736265181@qq.com>
2021-04-10 15:46:41 +0800

    263. 丑数

---
+++ LeetCode/SAOA/0263_IsUgly.cs
@@ -0,0 +1,22 @@
+﻿namespace LeetCode.SAOA
+{
+    internal sealed class IsUglySolution
+    {
+        public bool IsUgly(int n)
+        {
+            if (n <= 0)
+            {
+                return false;
+            }
+            int[] factors = new int[] { 2, 3, 5 };
+            for (int i = 0; i < factors.Length; i++)
+            {
+                while (n % factors[i] == 0)
+                {
+                    n /= factors[i];
+                }
+            }
+            return n == 1;
+        }
+    }
+}
