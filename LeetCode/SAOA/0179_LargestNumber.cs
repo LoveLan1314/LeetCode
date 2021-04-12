f8fb3e10784c6ef106f4cf0409378ecdc5432a73
LoveLan1314 <736265181@qq.com>
2021-04-12 23:16:44 +0800

    179. 最大数

---
+++ LeetCode/SAOA/0179_LargestNumber.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode.SAOA
+{
+    internal sealed class LargestNumberSolution
+    {
+        public string LargestNumber(int[] nums)
+        {
+            Array.Sort(nums, new NumberComparer());
+            if (nums[0] == 0)
+            {
+                return "0";
+            }
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < nums.Length; i++)
+            {
+                sb.Append(nums[i]);
+            }
+            return sb.ToString();
+        }
+
+        private class NumberComparer : IComparer<int>
+        {
+            public int Compare(int x, int y)
+            {
+                var value1 = long.Parse(x.ToString() + y.ToString());
+                var value2 = long.Parse(y.ToString() + x.ToString());
+                return (int)(value2 - value1);
+            }
+        }
+    }
+}
