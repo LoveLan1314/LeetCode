3ef057dacaf74bcc5febc62034941ef78ebd14ef
LoveLan1314 <736265181@qq.com>
2021-04-13 22:37:50 +0800

    783. 二叉搜索树节点最小距离

---
+++ LeetCode/SAOA/0783_MinDiffInBST.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace LeetCode.SAOA
+{
+    internal sealed class MinDiffInBSTSolution
+    {
+        private int _pre;
+        private int _ans;
+        public int MinDiffInBST(TreeNode root)
+        {
+            _ans = int.MaxValue;
+            _pre = -1;
+            Dfs(root);
+            return _ans;
+        }
+
+        private void Dfs(TreeNode root)
+        {
+            if (root == null)
+            {
+                return;
+            }
+            Dfs(root.left);
+            if (_pre == -1)
+            {
+                _pre = root.val;
+            }
+            else
+            {
+                _ans = Math.Min(_ans, root.val - _pre);
+                _pre = root.val;
+            }
+            Dfs(root.right);
+        }
+    }
+}
