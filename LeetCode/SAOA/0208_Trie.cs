bd5bc7288d811589da03f9cf51bbb120d5c23a54
LoveLan1314 <736265181@qq.com>
2021-04-14 22:03:08 +0800

    208. 实现 Trie (前缀树)

---
+++ LeetCode/SAOA/0208_Trie.cs
@@ -0,0 +1,57 @@
+﻿namespace LeetCode.SAOA
+{
+    internal sealed class Trie
+    {
+        private readonly Trie[] _children;
+        private bool _isEnd;
+
+        public Trie()
+        {
+            _children = new Trie[26];
+            _isEnd = false;
+        }
+
+        public void Insert(string word)
+        {
+            Trie node = this;
+            for (int i = 0; i < word.Length; i++)
+            {
+                char c = word[i];
+                int index = c - 'a';
+                if (node._children[index] == null)
+                {
+                    node._children[index] = new Trie();
+                }
+                node = node._children[index];
+            }
+            node._isEnd = true;
+        }
+
+        public bool Search(string word)
+        {
+            Trie node = SearchPrefix(word);
+            return node != null && node._isEnd;
+        }
+
+        public bool StartWith(string prefix)
+        {
+            return SearchPrefix(prefix) != null;
+        }
+
+        private Trie SearchPrefix(string prefix)
+        {
+            Trie node = this;
+            for (int i = 0; i < prefix.Length; i++)
+            {
+                char c = prefix[i];
+                int index = c - 'a';
+                if (node._children[index] == null)
+                {
+                    return null;
+                }
+                node = node._children[index];
+            }
+            return node;
+        }
+    }
+}
