c18bd2602066290a4e5e045fa2773ad8991b86dd
LoveLan1314 <736265181@qq.com>
2021-04-19 22:47:09 +0800

    27. 移除元素

---
+++ LeetCode/SAOA/0027_RemoveElement.cs
@@ -0,0 +1,22 @@
+﻿namespace LeetCode.SAOA
+{
+    internal sealed class RemoveElementSolution
+    {
+        public int RemoveElement(int[] nums, int val)
+        {
+            int n = nums.Length;
+            int slow = 0;
+            int fast = 0;
+            while (fast < n)
+            {
+                if (nums[fast] != val)
+                {
+                    nums[slow] = nums[fast];
+                    slow++;
+                }
+                fast++;
+            }
+            return slow;
+        }
+    }
+}
