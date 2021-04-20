0b58c657b027490bf25bd41feae5ff09cea0fff2
LoveLan1314 <736265181@qq.com>
2021-04-20 23:30:47 +0800

    28. 实现 strStr()

---
+++ LeetCode.Test/SAOA/0028_StrStr.cs
@@ -0,0 +1,17 @@
+﻿using LeetCode.Explore.PrimaryAlgorithm.String;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LeetCode.Test.SAOA
+{
+    [TestClass]
+    public sealed class StrStrTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            StrStrSolution solution = new StrStrSolution();
+            Assert.IsTrue(solution.StrStr("aaa", "aaaa") == -1);
+        }
+    }
+}
---
+++ LeetCode/SAOA/0028_StrStr.cs
@@ -0,0 +1,28 @@
+﻿namespace LeetCode.SAOA
+{
+    internal sealed class StrStrSolution
+    {
+        public int StrStr(string haystack, string needle)
+        {
+            if (needle.Length == 0)
+            {
+                return 0;
+            }
+            for (int i = 0; i < haystack.Length; i++)
+            {
+                for (int j = 0; j < needle.Length && i + j < haystack.Length; j++)
+                {
+                    if (haystack[i + j] != needle[j])
+                    {
+                        break;
+                    }
+                    else if (j == needle.Length - 1)
+                    {
+                        return i;
+                    }
+                }
+            }
+            return -1;
+        }
+    }
+}
