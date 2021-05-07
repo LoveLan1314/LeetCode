521c9040f890da0ac3c1a4075411f81db5528dd7
XuhuiChu <736265181@qq.com>
2021-05-07 11:54:13 +0800

    1304. 和为零的N个唯一整数

---
+++ LeetCode/SAOA/1304_SumZero.cs
@@ -0,0 +1,21 @@
+﻿namespace LeetCode.SAOA
+{
+    internal sealed class SumZeroSolution
+    {
+        public int[] SumZero(int n)
+        {
+            var result = new int[n];
+            for (int i = 0; i < n / 2; i++)
+            {
+                int val = i + 1;
+                result[i * 2] = -val;
+                result[i * 2 + 1] = val;
+            }
+            if (n % 2 == 1)
+            {
+                result[n - 1] = 0;
+            }
+            return result;
+        }
+    }
+}
