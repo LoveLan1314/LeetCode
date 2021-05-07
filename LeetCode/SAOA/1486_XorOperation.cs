9599fcfcf24d4959589cd9168e893a736291f6df
XuhuiChu <736265181@qq.com>
2021-05-07 11:41:13 +0800

    1486. 数组异或操作

---
+++ LeetCode/SAOA/1486_XorOperation.cs
@@ -0,0 +1,15 @@
+﻿namespace LeetCode.SAOA
+{
+    internal sealed class XorOperationSolution
+    {
+        public int XorOperation(int n, int start)
+        {
+            int result = start;
+            for (int i = 1; i < n; i++)
+            {
+                result ^= start + 2 * i;
+            }
+            return result;
+        }
+    }
+}
