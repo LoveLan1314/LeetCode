be4fb6d1aea9970f0b0430b2df61a8a448e36dd9
LoveLan1314 <736265181@qq.com>
2021-05-10 21:23:57 +0800

    872. 叶子相似的树

---
+++ LeetCode/SAOA/0872_LeafSimilar.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LeetCode.SAOA
+{
+    internal sealed class LeafSimilarSolution
+    {
+        public bool LeafSimilar(TreeNode root1, TreeNode root2)
+        {
+            var seq1 = new List<int>();
+            if (root1 != null)
+            {
+                DFS(root1, seq1);
+            }
+
+            var seq2 = new List<int>();
+            if (root2 != null)
+            {
+                DFS(root2, seq2);
+            }
+
+            return seq1.SequenceEqual(seq2);
+        }
+
+        private void DFS(TreeNode node, IList<int> seq)
+        {
+            if (node.left == null && node.right == null)
+            {
+                seq.Add(node.val);
+            }
+            else
+            {
+                if (node.left != null)
+                {
+                    DFS(node.left, seq);
+                }
+                if (node.right != null)
+                {
+                    DFS(node.right, seq);
+                }
+            }
+        }
+    }
+}
