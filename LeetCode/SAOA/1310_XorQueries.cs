bcf8287bfbce87eef2bf7a39abc7118d693288a3
LoveLan1314 <736265181@qq.com>
2021-05-12 00:27:19 +0800

    1310. 子数组异或查询

---
+++ LeetCode/SAOA/1310_XorQueries.cs
@@ -0,0 +1,22 @@
+﻿namespace LeetCode.SAOA
+{
+    internal sealed class XorQueriesSolution
+    {
+        public int[] XorQueries(int[] arr, int[][] queries)
+        {
+            int n = arr.Length;
+            int[] pre = new int[n + 1];
+            for (int i = 1; i <= n; i++)
+            {
+                pre[i] = pre[i - 1] ^ arr[i - 1];
+            }
+            int resLen = queries.Length;
+            int[] result = new int[resLen];
+            for (int i = 0; i < resLen; i++)
+            {
+                result[i] = pre[queries[i][0]] ^ pre[queries[i][1] + 1];
+            }
+            return result;
+        }
+    }
+}
