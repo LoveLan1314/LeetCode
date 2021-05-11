24442f2c6faa34a0e1acbcabfcd14c84e1e42442
LoveLan1314 <736265181@qq.com>
2021-05-12 00:01:23 +0800

    1734. 解码异或后的排列

---
+++ LeetCode/SAOA/1734_Decode.cs
@@ -0,0 +1,27 @@
+﻿namespace LeetCode.SAOA
+{
+    internal sealed class DecodeSolution
+    {
+        public int[] Decode(int[] encoded)
+        {
+            int n = encoded.Length + 1;
+            int total = 0;
+            for (int i = 0; i <= n; i++)
+            {
+                total ^= i;
+            }
+            int odd = 0;
+            for (int i = 1; i < n - 1; i += 2)
+            {
+                odd ^= encoded[i];
+            }
+            int[] perm = new int[n];
+            perm[0] = total ^ odd;
+            for (int i = 0; i < n - 1; i++)
+            {
+                perm[i + 1] = perm[i] ^ encoded[i];
+            }
+            return perm;
+        }
+    }
+}
