235b354eba684decfd1e8f3524c84c91a75af97f
LoveLan1314 <736265181@qq.com>
2021-06-04 22:47:01 +0800

    160. 相交链表

---
+++ LeetCode/SAOA/0160_GetIntersectionNode.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode.SAOA
+{
+    internal sealed class GetIntersectionNodeSolution
+    {
+        public ListNode GetIntersectionNode(ListNode headA, ListNode headB)
+        {
+            HashSet<ListNode> set = new HashSet<ListNode>();
+            ListNode temp = headA;
+            while (temp != null)
+            {
+                set.Add(temp);
+                temp = temp.next;
+            }
+
+            temp = headB;
+            while (temp != null)
+            {
+                if (set.Contains(temp))
+                {
+                    return temp;
+                }
+                temp = temp.next;
+            }
+            return null;
+        }
+
+        public ListNode GetIntersectionNode1(ListNode headA, ListNode headB)
+        {
+            if (headA == null || headB == null)
+            {
+                return null;
+            }
+
+            ListNode pA = headA, pB = headB;
+            while (pA != pB)
+            {
+                pA = pA == null ? headB : pA.next;
+                pB = pB == null ? headA : pB.next;
+            }
+            return pA;
+        }
+    }
+}
