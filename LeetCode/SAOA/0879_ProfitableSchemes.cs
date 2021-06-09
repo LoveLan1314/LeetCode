4cc36b9c350f56228492239d352ac70e8178dc16
LoveLan1314 <736265181@qq.com>
2021-06-09 21:31:52 +0800

    879. 盈利计划

---
+++ LeetCode/SAOA/0879_ProfitableSchemes.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace LeetCode.SAOA
+{
+    internal sealed class ProfitableSchemesSolution
+    {
+        public int ProfitableSchemes(int n, int minProfit, int[] group, int[] profit)
+        {
+            int len = group.Length;
+            int mod = (int)1e9 + 7;
+            int[,,] dp = new int[len + 1, n + 1, minProfit + 1];
+            dp[0, 0, 0] = 1;
+            for (int i = 1; i <= len; i++)
+            {
+                int members = group[i - 1];
+                int earn = profit[i - 1];
+                for (int j = 0; j <= n; j++)
+                {
+                    for (int k = 0; k <= minProfit; k++)
+                    {
+                        if (j < members)
+                        {
+                            dp[i, j, k] = dp[i - 1, j, k];
+                        }
+                        else
+                        {
+                            dp[i, j, k] = (dp[i - 1, j, k] + dp[i - 1, j - members, Math.Max(0, k - earn)]) % mod;
+                        }
+                    }
+                }
+            }
+
+            int sum = 0;
+            for (int j = 0; j <= n; j++)
+            {
+                sum = (sum + dp[len, j, minProfit]) % mod;
+            }
+            return sum;
+        }
+    }
+}
