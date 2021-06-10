336bb86cda244dd18c3d46ebbb28fa6034280189
LoveLan1314 <736265181@qq.com>
2021-06-10 22:50:38 +0800

    518. 零钱兑换 II

---
+++ LeetCode/SAOA/0518_Change.cs
@@ -0,0 +1,19 @@
+﻿namespace LeetCode.SAOA
+{
+    internal sealed class ChangeSolution2
+    {
+        public int Change(int amount, int[] coins)
+        {
+            int[] dp = new int[amount + 1];
+            dp[0] = 1;
+            foreach (var coin in coins)
+            {
+                for (int i = coin; i <= amount; i++)
+                {
+                    dp[i] += dp[i - coin];
+                }
+            }
+            return dp[amount];
+        }
+    }
+}
