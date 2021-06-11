7d20e8598ca65f839ac430863b4827a3c1e87eb5
LoveLan1314 <736265181@qq.com>
2021-06-11 22:20:31 +0800

    279. 完全平方数

---
+++ LeetCode/SAOA/0279_NumSquares.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace LeetCode.SAOA
+{
+    internal sealed class NumSquaresSolution
+    {
+        public int NumSquares(int n)
+        {
+            int[] dp = new int[n + 1];
+            for (int i = 1; i <= n; i++)
+            {
+                int min = int.MaxValue;
+                for (int j = 1; j * j <= i; j++)
+                {
+                    min = Math.Min(min, dp[i - j * j]);
+                }
+                dp[i] = min + 1;
+            }
+            return dp[n];
+        }
+    }
+}
