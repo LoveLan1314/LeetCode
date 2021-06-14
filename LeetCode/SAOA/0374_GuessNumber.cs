1bc54c840ec4f6853d8267eb66561f439aeb1be4
XuhuiChu <736265181@qq.com>
2021-06-14 22:37:33 +0800

    374. 猜数字大小

---
+++ LeetCode/SAOA/0374_GuessNumber.cs
@@ -0,0 +1,29 @@
+﻿namespace LeetCode.SAOA
+{
+    internal sealed class GuessNumberSolution : GuessGame
+    {
+        public int GuessNumber(int n)
+        {
+            int left = 1;
+            int right = n;
+            while (left < right)
+            {
+                int mid = left + (right - left) / 2;
+                int value = guess(mid);
+                if (value == 0)
+                {
+                    return mid;
+                }
+                else if (value > 0)
+                {
+                    left = mid + 1;
+                }
+                else
+                {
+                    right = mid;
+                }
+            }
+            return left;
+        }
+    }
+}
---
+++ LeetCode/SAOA/GuessGame.cs
@@ -0,0 +1,12 @@
+﻿namespace LeetCode.SAOA
+{
+    internal abstract class GuessGame
+    {
+#pragma warning disable IDE1006 // 命名样式
+        public int guess(int num)
+#pragma warning restore IDE1006 // 命名样式
+        {
+            return 0;
+        }
+    }
+}
