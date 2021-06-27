bea5d47e561cf53f382cf8edf02b79d2f389b340
LoveLan1314 <736265181@qq.com>
2021-06-28 00:31:19 +0800

    909. 蛇梯棋

---
+++ LeetCode.Test/SAOA/0909_SnakesAndLadders.cs
@@ -0,0 +1,65 @@
+﻿using LeetCode.SAOA;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LeetCode.Test.SAOA
+{
+    [TestClass]
+    public sealed class SnakesAndLaddersTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            SnakesAndLaddersSolution solution = new();
+            Assert.IsTrue(solution.SnakesAndLadders(new int[][]
+            {
+                new int[] {-1,-1,-1,-1,-1,-1,},
+                new int[] {-1, -1, -1, -1, -1, -1,},
+                new int[] {-1, -1, -1, -1, -1, -1},
+                new int[] { -1, 35, -1, -1, 13, -1 },
+                new int[] { -1, -1, -1, -1, -1, -1 },
+                new int[] { -1, 15, -1, -1, -1, -1 }
+            }) == 4);
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            SnakesAndLaddersSolution solution = new();
+            Assert.IsTrue(solution.SnakesAndLadders(new int[][]
+            {
+                new int[] {-1,-1,-1},
+                new int[] {-1,9,8},
+                new int[] {-1,8,9},
+            }) == 1);
+        }
+
+
+        [TestMethod]
+        public void TestMethod3()
+        {
+            SnakesAndLaddersSolution solution = new();
+            Assert.IsTrue(solution.SnakesAndLadders(new int[][]
+            {
+                new int[] {1,1,-1},
+                new int[] {1,1,1},
+                new int[] {-1,1,1},
+            }) == -1);
+        }
+
+
+        [TestMethod]
+        public void TestMethod4()
+        {
+            SnakesAndLaddersSolution solution = new();
+            Assert.IsTrue(solution.SnakesAndLadders(new int[][]
+            {
+                new int[] {-1,-1,19,10,-1},
+                new int[] {2,-1,-1,6,-1},
+                new int[] {-1,17,-1,19,-1},
+                new int[] {25,-1,20,-1,-1},
+                new int[] {-1,-1,-1,-1,15},
+            }) == 2);
+        }
+    }
+}
---
+++ LeetCode/SAOA/0909_SnakesAndLadders.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LeetCode.SAOA
+{
+    internal sealed class SnakesAndLaddersSolution
+    {
+        public int SnakesAndLadders(int[][] board)
+        {
+            int n = board.Length;
+            int target = n * n;
+            var start = 1;
+            if (start == target)
+            {
+                return 0;
+            }
+
+            var set = new HashSet<int>()
+            {
+                start
+            };
+            var queue = new Queue<int>();
+            queue.Enqueue(start);
+            int step = 0;
+            while (queue.Count > 0)
+            {
+                step++;
+                var size = queue.Count;
+                for (int i = 0; i < size; i++)
+                {
+                    var current = queue.Dequeue();
+                    for (int j = 1; j <= 6; j++)
+                    {
+                        var next = current + j;
+                        if (next > target)
+                        {
+                            break;
+                        }
+                        var nextValue = GetValue(board, next);
+                        if (nextValue != -1)
+                        {
+                            next = nextValue;
+                        }
+                        if (next == target)
+                        {
+                            return step;
+                        }
+                        if (!set.Contains(next))
+                        {
+                            set.Add(next);
+                            queue.Enqueue(next);
+                        }
+                    }
+                }
+            }
+            return -1;
+        }
+
+        private int GetValue(int[][] board, int number)
+        {
+            int n = board.Length;
+            int rowCount = (int)Math.Ceiling((decimal)number / n);
+            var row = n - rowCount;
+            int column;
+            if (rowCount % 2 == 0)
+            {
+                column = n - (number - n * (rowCount - 1));
+            }
+            else
+            {
+                column = number - n * (rowCount - 1) - 1;
+            }
+            return board[row][column];
+        }
+    }
+}
