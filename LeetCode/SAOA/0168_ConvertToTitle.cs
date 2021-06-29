57428f7abb21cb772fe61f6761938cdd64f4f690
XuhuiChu <736265181@qq.com>
2021-06-29 18:04:50 +0800

    168. Excel表列名称

---
+++ LeetCode/SAOA/0168_ConvertToTitle.cs
@@ -0,0 +1,25 @@
+﻿using System.Text;
+
+namespace LeetCode.SAOA
+{
+    internal sealed class ConvertToTitleSolution
+    {
+        private readonly string[] _keyValues = new string[]
+        {
+            "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M",
+            "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"
+        };
+
+        public string ConvertToTitle(int columnNumber)
+        {
+            var sb = new StringBuilder();
+            while (columnNumber > 0)
+            {
+                var key = (columnNumber - 1) % 26;
+                sb.Insert(0, _keyValues[key]);
+                columnNumber = (columnNumber - (key + 1)) / 26;
+            }
+            return sb.ToString();
+        }
+    }
+}
