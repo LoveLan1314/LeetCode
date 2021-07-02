33a54ea6a5967547c01381047dad44a200e96e1e
XuhuiChu <736265181@qq.com>
2021-07-02 17:53:18 +0800

    1833. 雪糕的最大数量

---
+++ LeetCode/SAOA/1833_MaxIceCream.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace LeetCode.SAOA
+{
+    internal sealed class MaxIceCreamSolution
+    {
+        public int MaxIceCream(int[] costs, int coins)
+        {
+            Array.Sort(costs);
+            int used = 0;
+            int count = 0;
+            for (int i = 0; i < costs.Length; i++)
+            {
+                used += costs[i];
+                if (used <= coins)
+                {
+                    count++;
+                }
+                else
+                {
+                    break;
+                }
+            }
+            return count;
+        }
+    }
+}
