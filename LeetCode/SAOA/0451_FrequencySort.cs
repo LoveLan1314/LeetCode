9b491f670d3ea7a553219d5a4f0f148c681e55c5
XuhuiChu <736265181@qq.com>
2021-07-03 17:10:03 +0800

    451. 根据字符出现频率排序

---
+++ LeetCode/SAOA/0451_FrequencySort.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LeetCode.SAOA
+{
+    internal sealed class FrequencySortSolution
+    {
+        public string FrequencySort(string s)
+        {
+            var list = s.ToArray();
+            var pairs = new Dictionary<char, int>();
+            foreach (var item in list)
+            {
+                if (pairs.ContainsKey(item))
+                {
+                    pairs[item]++;
+                }
+                else
+                {
+                    pairs.Add(item, 1);
+                }
+            }
+            var sb = new StringBuilder();
+            foreach (var item in pairs.OrderByDescending(i => i.Value))
+            {
+                sb.Append(item.Key, item.Value);
+            }
+            return sb.ToString();
+        }
+    }
+}
