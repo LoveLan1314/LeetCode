c23cdefbbbef8216c80d5245297ec479071e6fc4
XuhuiChu <736265181@qq.com>
2021-07-07 18:24:29 +0800

    1711. 大餐计数

---
+++ LeetCode/SAOA/1711_CountPairs.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace LeetCode.SAOA
+{
+    internal sealed class CountPairsSolution
+    {
+        public int CountPairs(int[] deliciousness)
+        {
+            const int MOD = 1000000007;
+            int maxSum = deliciousness.Max() * 2;
+            int pairs = 0;
+            Dictionary<int, int> dictionary = new Dictionary<int, int>();
+            int n = deliciousness.Length;
+            for (int i = 0; i < n; i++)
+            {
+                int val = deliciousness[i];
+                for (int sum = 1; sum <= maxSum; sum <<= 1)
+                {
+                    dictionary.TryGetValue(sum - val, out int count);
+                    pairs = (pairs + count) % MOD;
+                }
+                if (dictionary.ContainsKey(val))
+                {
+                    dictionary[val]++;
+                }
+                else
+                {
+                    dictionary.Add(val, 1);
+                }
+            }
+            return pairs;
+        }
+    }
+}
