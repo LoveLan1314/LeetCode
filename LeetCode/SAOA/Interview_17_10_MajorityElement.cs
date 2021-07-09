f012c47f6bcd825a6284d1e70045966d22cd2519
XuhuiChu <736265181@qq.com>
2021-07-09 15:48:44 +0800

    面试题 17.10. 主要元素

---
+++ LeetCode/SAOA/Interview_17_10_MajorityElement.cs
@@ -0,0 +1,35 @@
+﻿namespace LeetCode.SAOA
+{
+    internal sealed class MajorityElementSolution
+    {
+        public int MajorityElement(int[] nums)
+        {
+            int result = -1;
+            int count = 0;
+            foreach (var num in nums)
+            {
+                if (count == 0)
+                {
+                    result = num;
+                }
+                if (num == result)
+                {
+                    count++;
+                }
+                else
+                {
+                    count--;
+                }
+            }
+            count = 0;
+            foreach (var num in nums)
+            {
+                if (num == result)
+                {
+                    count++;
+                }
+            }
+            return count * 2 > nums.Length ? result : -1;
+        }
+    }
+}
