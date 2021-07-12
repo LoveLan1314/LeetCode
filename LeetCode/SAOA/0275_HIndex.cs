ed9a86ddedfc99a367d4974d55039316fc2f0f08
LoveLan1314 <736265181@qq.com>
2021-07-12 22:02:38 +0800

    275. H 指数 II

---
+++ LeetCode/SAOA/0275_HIndex.cs
@@ -0,0 +1,25 @@
+﻿namespace LeetCode.SAOA
+{
+    internal sealed class HIndexSolution
+    {
+        public int HIndex(int[] citations)
+        {
+            int n = citations.Length;
+            int left = 0;
+            int right = n - 1;
+            while (left <= right)
+            {
+                int mid = (right - left) / 2 + left;
+                if (citations[mid] >= n - mid)
+                {
+                    right = mid - 1;
+                }
+                else
+                {
+                    left = mid + 1;
+                }
+            }
+            return n - left;
+        }
+    }
+}
