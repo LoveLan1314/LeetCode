f14e615362fe51bbdb2f2e086c42ce0aff4a3a1a
XuhuiChu <736265181@qq.com>
2021-07-15 18:00:25 +0800

    1846. 减小和重新排列数组后的最大元素

---
+++ LeetCode/SAOA/1846_MaximumElementAfterDecrementingAndRearranging.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace LeetCode.SAOA
+{
+    internal sealed class MaximumElementAfterDecrementingAndRearrangingSolution
+    {
+        public int MaximumElementAfterDecrementingAndRearranging(int[] arr)
+        {
+            Array.Sort(arr);
+            int prev = 1;
+            for (int i = 1; i < arr.Length; i++)
+            {
+                int value = arr[i];
+                if (value - prev > 1)
+                {
+                    value = prev + 1;
+                }
+                prev = value;
+            }
+            return prev;
+        }
+    }
+}
