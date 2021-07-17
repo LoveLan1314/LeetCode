0ccc202e5a12e154611aac58c8e197d6e2b18a3c
褚许辉 <736265181@qq.com>
2021-07-17 10:23:39 +0800

    剑指 Offer 42. 连续子数组的最大和

---
+++ LeetCode/SAOA/Offer42_MaxSubArray.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace LeetCode.SAOA
+{
+    internal sealed class Offer42_MaxSubArraySolution
+    {
+        public int MaxSubArray(int[] nums)
+        {
+            int pre = 0;
+            int maxAns = nums[0];
+            foreach (var item in nums)
+            {
+                pre = Math.Max(pre + item, item);
+                maxAns = Math.Max(maxAns, pre);
+            }
+            return maxAns;
+        }
+    }
+}
