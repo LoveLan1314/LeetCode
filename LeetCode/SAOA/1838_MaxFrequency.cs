f77d3ccbaad3cb1c7f37e9f8b06f9128bfe42332
XuhuiChu <736265181@qq.com>
2021-07-19 11:29:43 +0800

    1838. 最高频元素的频数

---
+++ LeetCode/SAOA/1838_MaxFrequency.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace LeetCode.SAOA
+{
+    internal sealed class MaxFrequencySolution
+    {
+        public int MaxFrequency(int[] nums, int k)
+        {
+            Array.Sort(nums);
+            long total = 0;
+            int result = 1;
+            int l = 0;
+            for (int r = 1; r < nums.Length; r++)
+            {
+                total += (long)(nums[r] - nums[r - 1]) * (r - l);
+                while (total > k)
+                {
+                    total -= nums[r] - nums[l];
+                    l++;
+                }
+                result = Math.Max(result, r - l + 1);
+            }
+            return result;
+        }
+    }
+}
