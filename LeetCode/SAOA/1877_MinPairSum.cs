20d92818cf45597241102eec2dfd4a485b0e76ec
XuhuiChu <736265181@qq.com>
2021-07-20 10:25:16 +0800

    1877. 数组中最大数对和的最小值

---
+++ LeetCode/SAOA/1877_MinPairSum.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace LeetCode.SAOA
+{
+    internal sealed class MinPairSumSolution
+    {
+        public int MinPairSum(int[] nums)
+        {
+            Array.Sort(nums);
+            int left = 0;
+            int right = nums.Length - 1;
+            int result = int.MinValue;
+            while (left < right)
+            {
+                result = Math.Max(result, nums[left] + nums[right]);
+                left++;
+                right--;
+            }
+            return result;
+        }
+    }
+}
