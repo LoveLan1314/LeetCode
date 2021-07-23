6a606bca3a0cb36fb07cab346be557ba44a99203
XuhuiChu <736265181@qq.com>
2021-07-23 14:57:24 +0800

    1893. 检查是否区域内所有整数都被覆盖

---
+++ LeetCode/SAOA/1893_IsCovered.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode.SAOA
+{
+    internal sealed class IsCoveredSolution
+    {
+        public bool IsCovered(int[][] ranges, int left, int right)
+        {
+            HashSet<int> set = new HashSet<int>();
+            for (int i = left; i <= right; i++)
+            {
+                set.Add(i);
+            }
+            int n = ranges.Length;
+            for (int i = 0; i < n; i++)
+            {
+                var range = ranges[i];
+                int start = range[0];
+                int end = range[1];
+                set.RemoveWhere(i => i >= start && i <= end);
+            }
+            return set.Count == 0;
+        }
+    }
+}
