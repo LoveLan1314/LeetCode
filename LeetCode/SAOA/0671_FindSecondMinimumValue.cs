e9efd5c287bca7815134cc4543b02d5f5b22a9f1
XuhuiChu <736265181@qq.com>
2021-07-27 18:23:18 +0800

    671. 二叉树中第二小的节点

---
+++ LeetCode/SAOA/0671_FindSecondMinimumValue.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace LeetCode.SAOA
+{
+    internal sealed class FindSecondMinimumValueSolution
+    {
+        public int FindSecondMinimumValue(TreeNode root)
+        {
+            return FirstBigger(root, root.val);
+        }
+
+        private int FirstBigger(TreeNode node, int val)
+        {
+            if (node == null)
+            {
+                return -1;
+            }
+            if (node.val > val)
+            {
+                return node.val;
+            }
+            int left = FirstBigger(node.left, val);
+            int right = FirstBigger(node.right, val);
+            if (left < 0)
+            {
+                return right;
+            }
+            if (right < 0)
+            {
+                return left;
+            }
+            return Math.Min(left, right);
+        }
+    }
+}
