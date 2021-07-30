1fd21143627e3d238b93d1642e1ec09e78d8eafe
XuhuiChu <736265181@qq.com>
2021-07-30 18:12:06 +0800

    171. Excel 表列序号

---
+++ LeetCode/SAOA/0171_TitleToNumber.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace LeetCode.SAOA
+{
+    internal sealed class TitleToNumberSolution
+    {
+        public int TitleToNumber(string columnTitle)
+        {
+            int result = 0;
+            int length = columnTitle.Length;
+            foreach (var item in columnTitle)
+            {
+                result += (int)((item - 65 + 1) * Math.Pow(26, length - 1));
+                length--;
+            }
+            return result;
+        }
+    }
+}
