b06f420d0a9a3aafaa942ff69ecba1783036e15d
褚许辉 <736265181@qq.com>
2021-10-08 20:51:05 +0800

    187. 重复的DNA序列

---
+++ LeetCode/SAOA/0187_FindRepeatedDnaSequences.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode.SAOA
+{
+    internal sealed class FindRepeatedDnaSequencesSolution
+    {
+        public IList<string> FindRepeatedDnaSequences(string s)
+        {
+            const int l = 10;
+            IList<string> result = new List<string>();
+            Dictionary<string, int> pairs = new Dictionary<string, int>();
+            int n = s.Length;
+            for (int i = 0; i <= n - l; i++)
+            {
+                string subStr = s.Substring(i, l);
+                if (pairs.ContainsKey(subStr))
+                {
+                    pairs[subStr]++;
+                }
+                else
+                {
+                    pairs.Add(subStr, 1);
+                }
+                if (pairs[subStr] == 2)
+                {
+                    result.Add(subStr);
+                }
+            }
+            return result;
+        }
+    }
+}
