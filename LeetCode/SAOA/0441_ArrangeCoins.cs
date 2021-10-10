9ef9a7d77e550624e4d7b37d280f1393816ec72f
褚许辉 <736265181@qq.com>
2021-10-10 14:39:00 +0800

    441. 排列硬币

---
+++ LeetCode/SAOA/0441_ArrangeCoins.cs
@@ -0,0 +1,23 @@
+﻿namespace LeetCode.SAOA
+{
+    internal sealed class ArrangeCoinsSolution
+    {
+        public int ArrangeCoins(int n)
+        {
+            int result = 0;
+            for (int i = 1; i < int.MaxValue; i++)
+            {
+                if (n >= i)
+                {
+                    result++;
+                    n -= i;
+                }
+                else
+                {
+                    break;
+                }
+            }
+            return result;
+        }
+    }
+}
