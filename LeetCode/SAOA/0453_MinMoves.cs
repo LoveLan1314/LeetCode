b4a24358cff695d9a639461240f26a1e37fd5ca0
XuhuiChu <736265181@qq.com>
2021-10-20 15:17:52 +0800

    453. 最小操作次数使数组元素相等

---
+++ LeetCode/SAOA/0453_MinMoves.cs
@@ -0,0 +1,18 @@
+﻿using System.Linq;
+
+namespace LeetCode.SAOA
+{
+    internal sealed class MinMovesSolution
+    {
+        public int MinMoves(int[] nums)
+        {
+            int min = nums.Min();
+            int result = 0;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                result += nums[i] - min;
+            }
+            return result;
+        }
+    }
+}
