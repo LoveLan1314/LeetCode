005dabf6a2f65440d74747219774cb782f7b6e7f
XuhuiChu <736265181@qq.com>
2021-10-21 17:55:00 +0800

    66. 加一

---
+++ LeetCode/SAOA/0066_PlusOne.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode.SAOA
+{
+    internal sealed class PlusOneSolution
+    {
+        public int[] PlusOne(int[] digits)
+        {
+            bool needAdd = true;
+            for (int i = digits.Length - 1; i >= 0; i--)
+            {
+                int value = digits[i];
+                if (needAdd)
+                {
+                    if (value == 9)
+                    {
+                        digits[i] = 0;
+                    }
+                    else
+                    {
+                        digits[i] = value + 1;
+                        needAdd = false;
+                        break;
+                    }
+                }
+            }
+            if (needAdd)
+            {
+                var list = new List<int>() { 1 };
+                list.AddRange(digits);
+                return list.ToArray();
+            }
+            else
+            {
+                return digits;
+            }
+        }
+    }
+}
