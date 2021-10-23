9f6ece496fbf5845f14a37488224d165daa3f808
XuhuiChu <736265181@qq.com>
2021-10-23 18:26:08 +0800

    492. 构造矩形

---
+++ LeetCode/SAOA/0492_ConstructRectangle.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace LeetCode.SAOA
+{
+    internal sealed class ConstructRectangleSolution
+    {
+        public int[] ConstructRectangle(int area)
+        {
+            int w = (int)Math.Sqrt(area);
+            while (area % w != 0)
+            {
+                w--;
+            }
+            return new int[] { area / w, w };
+        }
+    }
+}
