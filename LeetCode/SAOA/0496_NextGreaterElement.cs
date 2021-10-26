f5e94bdd360eeeb314a483dfca68507a670ae7d0
XuhuiChu <736265181@qq.com>
2021-10-26 14:23:24 +0800

    496. 下一个更大元素 I

---
+++ LeetCode/SAOA/0496_NextGreaterElement.cs
@@ -0,0 +1,39 @@
+﻿namespace LeetCode.SAOA
+{
+    internal sealed class NextGreaterElementSolution
+    {
+        public int[] NextGreaterElement(int[] nums1, int[] nums2)
+        {
+            for (int i = 0; i < nums1.Length; i++)
+            {
+                int value = nums1[i];
+                bool findRepeat = false;
+                bool find = false;
+                for (int j = 0; j < nums2.Length; j++)
+                {
+                    if (findRepeat)
+                    {
+                        if (nums2[j] > value)
+                        {
+                            find = true;
+                            nums1[i] = nums2[j];
+                            break;
+                        }
+                    }
+                    else
+                    {
+                        if (nums2[j] == value)
+                        {
+                            findRepeat = true;
+                        }
+                    }
+                }
+                if (!find)
+                {
+                    nums1[i] = -1;
+                }
+            }
+            return nums1;
+        }
+    }
+}
