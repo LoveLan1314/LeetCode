df5a08e49a0a9c41b2b411141838b6a674d32b26
褚许辉 <736265181@qq.com>
2021-11-26 22:03:58 +0800

    700. 二叉搜索树中的搜索

---
+++ LeetCode/SAOA/0700_SearchBST.cs
@@ -0,0 +1,25 @@
+﻿namespace LeetCode.SAOA
+{
+    internal sealed class SearchBSTSolution
+    {
+        public TreeNode SearchBST(TreeNode root, int val)
+        {
+            while (root != null)
+            {
+                if (root.val == val)
+                {
+                    return root;
+                }
+                else if (root.val > val)
+                {
+                    root = root.left;
+                }
+                else
+                {
+                    root = root.right;
+                }
+            }
+            return null;
+        }
+    }
+}
