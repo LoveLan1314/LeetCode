9633513e864379aac6408d834845b2846f961c9a
XuhuiChu <736265181@qq.com>
2022-01-13 12:17:51 +0800

    747. 至少是其他数字两倍的最大数

---
+++ LeetCode/SAOA/0747_DominantIndex.cs
@@ -0,0 +1,45 @@
+﻿namespace LeetCode.SAOA
+{
+    internal sealed class DominantIndexSolution
+    {
+        public int DominantIndex(int[] nums)
+        {
+            if (nums.Length == 0)
+            {
+                return -1;
+            }
+            if (nums.Length == 1)
+            {
+                return 0;
+            }
+            int max = int.MinValue;
+            var maxIndex = -1;
+            int second = int.MinValue;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                int value = nums[i];
+                if (value > second)
+                {
+                    if (value > max)
+                    {
+                        second = max;
+                        max = value;
+                        maxIndex = i;
+                    }
+                    else
+                    {
+                        second = value;
+                    }
+                }
+            }
+            if (second * 2 <= max)
+            {
+                return maxIndex;
+            }
+            else
+            {
+                return -1;
+            }
+        }
+    }
+}
