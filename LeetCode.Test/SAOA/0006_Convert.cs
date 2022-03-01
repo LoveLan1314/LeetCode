2f2cd862424cb21c016d8c152a01b6ef15936122
XuhuiChu <736265181@qq.com>
2022-03-01 12:32:34 +0800

    6. Z 字形变换

---
+++ LeetCode.Test/SAOA/0006_Convert.cs
@@ -0,0 +1,16 @@
+﻿using LeetCode.SAOA;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LeetCode.Test.SAOA
+{
+    [TestClass]
+    public sealed class ConvertTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var solution = new ConvertSolution();
+            Assert.IsTrue(solution.Convert("PAYPALISHIRING", 4) == "PINALSIGYAHRPI");
+        }
+    }
+}
---
+++ LeetCode/SAOA/0006_Convert.cs
@@ -0,0 +1,53 @@
+﻿using System.Text;
+
+namespace LeetCode.SAOA
+{
+    internal sealed class ConvertSolution
+    {
+        public string Convert(string s, int numRows)
+        {
+            int n = s.Length, r = numRows;
+            if (r == 1 || r >= n)
+            {
+                return s;
+            }
+            //每个周期需要的字符个数
+            int t = r * 2 - 2;
+            //矩阵的列数
+            //n + t - 1表示总字符个数，最后一个周期视为完整周期
+            //r - 1为每个周期需要的列数
+            int c = (n + t - 1) / t * (r - 1);
+            char[][] mat = new char[r][];
+            for (int i = 0; i < r; ++i)
+            {
+                mat[i] = new char[c];
+            }
+            for (int i = 0, x = 0, y = 0; i < n; ++i)
+            {
+                mat[x][y] = s[i];
+                //判断在周期内的位置
+                if (i % t < r - 1)
+                {
+                    ++x; // 向下移动
+                }
+                else
+                {
+                    --x;
+                    ++y; // 向右上移动
+                }
+            }
+            StringBuilder ans = new StringBuilder();
+            foreach (char[] row in mat)
+            {
+                foreach (char ch in row)
+                {
+                    if (ch != 0)
+                    {
+                        ans.Append(ch);
+                    }
+                }
+            }
+            return ans.ToString();
+        }
+    }
+}
