ace916159f2aa5c6cadea7a1105d930151133595
褚许辉 <736265181@qq.com>
2022-03-04 21:09:43 +0800

    2104. 子数组范围和

---
+++ LeetCode/SAOA/2104_SubArrayRanges.cs
@@ -0,0 +1,30 @@
+﻿namespace LeetCode.SAOA
+{
+    internal sealed class SubArrayRangesSolution
+    {
+        public long SubArrayRanges(int[] nums)
+        {
+            long result = 0;
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                int min = nums[i];
+                int max = nums[i];
+                for (int j = i + 1; j < nums.Length; j++)
+                {
+                    var value = nums[j];
+                    if (value > max)
+                    {
+                        max = value;
+                    }
+                    if (value < min)
+                    {
+                        min = value;
+                    }
+                    result += max - min;
+                }
+            }
+            return result;
+        }
+    }
+}
