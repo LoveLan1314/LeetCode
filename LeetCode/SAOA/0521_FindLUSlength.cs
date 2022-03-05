6c1fcbba5852b988336d333b8977f86d7573a86c
褚许辉 <736265181@qq.com>
2022-03-05 23:18:47 +0800

    521. 最长特殊序列 Ⅰ

---
+++ LeetCode/SAOA/0521_FindLUSlength.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LeetCode.SAOA
+{
+    internal sealed class FindLUSlengthSolution
+    {
+        public int FindLUSlength(string a, string b)
+        {
+            return a != b ? Math.Max(a.Length, b.Length) : -1;
+        }
+    }
+}
