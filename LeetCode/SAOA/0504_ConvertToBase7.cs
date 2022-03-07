405350af5258f5deed4b2b78ffa0e12cfd87dfde
XuhuiChu <736265181@qq.com>
2022-03-07 18:10:23 +0800

    504. 七进制数

---
+++ LeetCode/SAOA/0504_ConvertToBase7.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode.SAOA
+{
+    internal sealed class ConvertToBase7Solution
+    {
+        public string ConvertToBase7(int num)
+        {
+            StringBuilder sb = new StringBuilder();
+            if (num < 0)
+            {
+                sb.Append('-');
+                num = Math.Abs(num);
+            }
+            Stack<int> stack = new Stack<int>();
+            while (num >= 7)
+            {
+                stack.Push(num % 7);
+                num /= 7;
+            }
+            stack.Push(num);
+            while (stack.Count > 0)
+            {
+                sb.Append(stack.Pop());
+            }
+            return sb.ToString();
+        }
+    }
+}
