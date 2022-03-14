55d315d2b4dbf547760b3418a9d8a0bfca9b8a6e
XuhuiChu <736265181@qq.com>
2022-03-14 14:36:45 +0800

    599. 两个列表的最小索引总和

---
+++ LeetCode/SAOA/0599_FindRestaurant.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode.SAOA
+{
+    internal sealed class FindRestaurantSolution
+    {
+        public string[] FindRestaurant(string[] list1, string[] list2)
+        {
+            Dictionary<string, int> pairs2 = new Dictionary<string, int>();
+            for (int i = 0; i < list2.Length; i++)
+            {
+                pairs2.Add(list2[i], i);
+            }
+            List<string> result = new List<string>();
+            var indexSum = int.MaxValue;
+            for (int i = 0; i < list1.Length; i++)
+            {
+                if (i > indexSum)
+                {
+                    break;
+                }
+                if (pairs2.TryGetValue(list1[i], out var j))
+                {
+                    var index = i + j;
+                    if (index < indexSum)
+                    {
+                        result = new List<string>()
+                        {
+                            list1[i]
+                        };
+                        indexSum = index;
+                    }
+                    else if (index == indexSum)
+                    {
+                        result.Add(list1[i]);
+                    }
+                }
+            }
+            return result.ToArray();
+        }
+    }
+}
