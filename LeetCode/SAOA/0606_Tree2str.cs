69ba8794371f4ea689201e3051e5ee3a32286bda
褚许辉 <736265181@qq.com>
2022-03-19 20:55:29 +0800

    606. 根据二叉树创建字符串

---
+++ LeetCode/SAOA/0606_Tree2str.cs
@@ -0,0 +1,38 @@
+﻿using System.Text;
+
+namespace LeetCode.SAOA
+{
+    internal sealed class Tree2strSolution
+    {
+        public string Tree2str(TreeNode root)
+        {
+            if (root == null)
+            {
+                return "";
+            }
+            var sb = new StringBuilder();
+            DFS(root, sb);
+            return sb.ToString();
+        }
+
+        private void DFS(TreeNode node, StringBuilder sb)
+        {
+            if (node != null)
+            {
+                sb.Append(node.val);
+                if (node.left != null || node.right != null)
+                {
+                    sb.Append('(');
+                    DFS(node.left, sb);
+                    sb.Append(')');
+                }
+                if (node.right != null)
+                {
+                    sb.Append('(');
+                    DFS(node.right, sb);
+                    sb.Append(')');
+                }
+            }
+        }
+    }
+}
