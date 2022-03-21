f1942558bbcc0b02f48233a79b20bf9dea215ccd
XuhuiChu <736265181@qq.com>
2022-03-21 12:33:25 +0800

    653. 两数之和 IV - 输入 BST

---
+++ LeetCode/SAOA/0653_FindTarget.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode.SAOA
+{
+    internal sealed class FindTargetSolution
+    {
+        private readonly HashSet<int> _set = new HashSet<int>();
+
+        public bool FindTarget(TreeNode root, int k)
+        {
+            if (root == null)
+            {
+                return false;
+            }
+            if (_set.Contains(k - root.val))
+            {
+                return true;
+            }
+            _set.Add(root.val);
+            return FindTarget(root.left, k) || FindTarget(root.right, k);
+        }
+    }
+}
