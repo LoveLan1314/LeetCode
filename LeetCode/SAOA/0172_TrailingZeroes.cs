6f813cf765ca4771386efc8a06fb831b9ca017e9
褚许辉 <736265181@qq.com>
2022-03-25 23:00:15 +0800

    172. 阶乘后的零

---
+++ LeetCode/SAOA/0172_TrailingZeroes.cs
@@ -0,0 +1,19 @@
+﻿namespace LeetCode.SAOA
+{
+    internal sealed class TrailingZeroesSolution
+    {
+        public int TrailingZeroes(int n)
+        {
+            //计算一共有多少个10，2*5=10，2一定比5多，则为计算一共有多少个5
+            int ans = 0;
+            for (int i = 5; i <= n; i += 5)
+            {
+                for (int x = i; x % 5 == 0; x /= 5)
+                {
+                    ++ans;
+                }
+            }
+            return ans;
+        }
+    }
+}
