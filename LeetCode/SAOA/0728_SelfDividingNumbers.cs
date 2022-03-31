a6f54d968b4ee0c91cb0df1fd2bbd65a8333484b
XuhuiChu <736265181@qq.com>
2022-03-31 19:12:14 +0800

    728. 自除数

---
+++ LeetCode/SAOA/0728_SelfDividingNumbers.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode.SAOA
+{
+    internal sealed class SelfDividingNumbersSolution
+    {
+        public IList<int> SelfDividingNumbers(int left, int right)
+        {
+            var list = new List<int>();
+            for (int i = left; i <= right; i++)
+            {
+                if (IsSelfDividingNumber(i))
+                {
+                    list.Add(i);
+                }
+            }
+            return list;
+        }
+
+        private bool IsSelfDividingNumber(int number)
+        {
+            foreach (var item in number.ToString())
+            {
+                var value = int.Parse(item.ToString());
+                if (value == 0 || number % value != 0)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
