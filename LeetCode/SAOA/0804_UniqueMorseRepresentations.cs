026049d5c9a7d3ced359b81c0c69e68d5becf060
褚许辉 <736265181@qq.com>
2022-04-10 21:35:40 +0800

    804. 唯一摩尔斯密码词

---
+++ LeetCode/SAOA/0804_UniqueMorseRepresentations.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode.SAOA
+{
+    internal sealed class UniqueMorseRepresentationsSolution
+    {
+        private readonly string[] _data =
+            new string[] { ".-", "-...", "-.-.", "-..", ".", "..-.", "--.", "....", "..",
+                ".---", "-.-", ".-..", "--", "-.", "---", ".--.", "--.-", ".-.", "...",
+                "-", "..-", "...-", ".--", "-..-", "-.--", "--.." };
+
+        public int UniqueMorseRepresentations(string[] words)
+        {
+            HashSet<string> result = new HashSet<string>();
+            foreach (var item in words)
+            {
+                StringBuilder sb = new StringBuilder();
+                foreach (var c in item)
+                {
+                    sb.Append(_data[c - 97]);
+                }
+                var s = sb.ToString();
+                if (!result.Contains(s))
+                {
+                    result.Add(s);
+                }
+            }
+            return result.Count;
+        }
+    }
+}
