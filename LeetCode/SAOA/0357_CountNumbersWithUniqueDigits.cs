e1e81bc1fd898559193b169981169e3e633dcdd2
褚许辉 <736265181@qq.com>
2022-04-11 21:35:40 +0800

    357. 统计各位数字都不同的数字个数

---
+++ LeetCode/SAOA/0357_CountNumbersWithUniqueDigits.cs
@@ -0,0 +1,28 @@
+﻿namespace LeetCode.SAOA
+{
+    internal sealed class CountNumbersWithUniqueDigitsSolution
+    {
+        public int CountNumbersWithUniqueDigits(int n)
+        {
+            if (n == 0)
+            {
+                return 1;
+            }
+            else if (n == 1)
+            {
+                return 10;
+            }
+            else
+            {
+                int result = 10;
+                int cur = 9;
+                for (int i = 0; i < n - 1; i++)
+                {
+                    cur *= 9 - i;
+                    result += cur;
+                }
+                return result;
+            }
+        }
+    }
+}
