452ec7fa1bd0a656c7c2fa144de9e276f6c7b7dd
褚许辉 <736265181@qq.com>
2022-04-12 20:55:29 +0800

    806. 写字符串需要的行数

---
+++ LeetCode/SAOA/0806_NumberOfLines.cs
@@ -0,0 +1,22 @@
+﻿namespace LeetCode.SAOA
+{
+    internal sealed class NumberOfLinesSolution
+    {
+        public int[] NumberOfLines(int[] widths, string s)
+        {
+            int length = 1;
+            int count = 0;
+            foreach (var item in s)
+            {
+                var itemLength = widths[item - 97];
+                if (count + itemLength > 100)
+                {
+                    length++;
+                    count = 0;
+                }
+                count += itemLength;
+            }
+            return new int[] { length, count };
+        }
+    }
+}
