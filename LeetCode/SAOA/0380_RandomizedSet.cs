1ccfbf889db84bff01f30396c128da02e99bfdb9
褚许辉 <736265181@qq.com>
2022-04-13 21:44:34 +0800

    380. O(1) 时间插入、删除和获取随机元素

---
+++ LeetCode/SAOA/0380_RandomizedSet.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LeetCode.SAOA
+{
+    internal sealed class RandomizedSet
+    {
+        private readonly Dictionary<int, int> _pairs = new Dictionary<int, int>();
+        private readonly List<int> _list = new List<int>();
+
+        public RandomizedSet()
+        {
+
+        }
+
+        public bool Insert(int val)
+        {
+            if (_pairs.ContainsKey(val))
+            {
+                return false;
+            }
+            _list.Add(val);
+            _pairs.Add(val, _list.Count - 1);
+            return true;
+        }
+
+        public bool Remove(int val)
+        {
+            if (!_pairs.ContainsKey(val))
+            {
+                return false;
+            }
+            var index = _pairs[val];
+            var last = _list.Last();
+            _list[index] = last;
+            _pairs[last] = index;
+            _list.RemoveAt(_list.Count - 1);
+            _pairs.Remove(val);
+            return true;
+        }
+
+        public int GetRandom()
+        {
+            var random = new Random();
+            var index = random.Next(_list.Count);
+            return _list[index];
+        }
+    }
+}
