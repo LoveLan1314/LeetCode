11d13d5cd32dbaa0665e1741cc843c0219bbdf33
褚许辉 <736265181@qq.com>
2022-04-14 23:08:19 +0800

    1672. 最富有客户的资产总量

---
+++ LeetCode/SAOA/1672_MaximumWealth.cs
@@ -0,0 +1,21 @@
+﻿using System.Linq;
+
+namespace LeetCode.SAOA
+{
+    internal sealed class MaximumWealthSolution
+    {
+        public int MaximumWealth(int[][] accounts)
+        {
+            var max = 0;
+            foreach (var item in accounts)
+            {
+                var sum = item.Sum();
+                if (sum > max)
+                {
+                    max = sum;
+                }
+            }
+            return max;
+        }
+    }
+}
