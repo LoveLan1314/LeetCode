50a8c7daf41ef4eb732d9524a585fd32d8665164
XuhuiChu <736265181@qq.com>
2022-05-04 18:14:20 +0800

    1823. 找出游戏的获胜者

---
+++ LeetCode/SAOA/1823_FindTheWinner.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode.SAOA
+{
+    internal sealed class FindTheWinnerSolution
+    {
+        public int FindTheWinner1(int n, int k)
+        {
+            var queue = new Queue<int>();
+            for (int i = 1; i <= n; i++)
+            {
+                queue.Enqueue(i);
+            }
+            while (queue.Count > 1)
+            {
+                for (int i = 1; i < k; i++)
+                {
+                    queue.Enqueue(queue.Dequeue());
+                }
+                queue.Dequeue();
+            }
+            return queue.Peek();
+        }
+
+        public int FindTheWinner(int n, int k)
+        {
+            if (n == 1)
+            {
+                return 1;
+            }
+            return (k + FindTheWinner(n - 1, k) - 1) % n + 1;
+        }
+    }
+}
