ea076d1668825868f4ca92503955972ec269d6ae
褚许辉 <736265181@qq.com>
2022-05-08 13:24:20 +0800

    442. 数组中重复的数据

---
+++ LeetCode/SAOA/0442_FindDuplicates.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LeetCode.SAOA
+{
+    internal sealed class FindDuplicatesSolution
+    {
+        public IList<int> FindDuplicates(int[] nums)
+        {
+            int n = nums.Length;
+            var ans = new List<int>();
+            for (int i = 0; i < n; ++i)
+            {
+                int x = Math.Abs(nums[i]);
+                if (nums[x - 1] > 0)
+                {
+                    nums[x - 1] = -nums[x - 1];
+                }
+                else
+                {
+                    ans.Add(x);
+                }
+            }
+            return ans;
+        }
+    }
+}
