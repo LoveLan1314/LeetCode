3d2a6c228d62eefb970ea8931c98704414a0a9b9
褚许辉 <736265181@qq.com>
2022-05-09 21:12:44 +0800

    942. 增减字符串匹配

---
+++ LeetCode/SAOA/0942_DiStringMatch.cs
@@ -0,0 +1,17 @@
+﻿namespace LeetCode.SAOA
+{
+    internal sealed class DiStringMatchSolution
+    {
+        public int[] DiStringMatch(string s)
+        {
+            int n = s.Length, lo = 0, hi = n;
+            int[] perm = new int[n + 1];
+            for (int i = 0; i < n; ++i)
+            {
+                perm[i] = s[i] == 'I' ? lo++ : hi--;
+            }
+            perm[n] = lo; // 最后剩下一个数，此时 lo == hi
+            return perm;
+        }
+    }
+}
