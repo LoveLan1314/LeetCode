96bc60187e5f1e69c29cf6e7b0f707c7fb6d1833
褚许辉 <736265181@qq.com>
2022-05-12 21:00:31 +0800

    944. 删列造序

---
+++ LeetCode/SAOA/0944_MinDeletionSize.cs
@@ -0,0 +1,24 @@
+﻿namespace LeetCode.SAOA
+{
+    internal sealed class MinDeletionSizeSolution
+    {
+        public int MinDeletionSize(string[] strs)
+        {
+            int n = strs[0].Length;
+            int m = strs.Length;
+            int count = 0;
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < m - 1; j++)
+                {
+                    if (strs[j][i] > strs[j + 1][i])
+                    {
+                        count++;
+                        break;
+                    }
+                }
+            }
+            return count;
+        }
+    }
+}
