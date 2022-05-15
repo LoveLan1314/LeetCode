76a2e4d64e7620f5b0e0713e7959e0e8e85fff86
褚许辉 <736265181@qq.com>
2022-05-15 11:03:58 +0800

    6064. 不含特殊楼层的最大连续楼层数

---
+++ LeetCode/SAOA/6064_MaxConsecutive.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace LeetCode.SAOA
+{
+    internal sealed class MaxConsecutiveSolution
+    {
+        public int MaxConsecutive(int bottom, int top, int[] special)
+        {
+            if (special.Length == 0)
+            {
+                return top - bottom + 1;
+            }
+            Array.Sort(special);
+            int max = 0;
+            for (int i = 0; i <= special.Length; i++)
+            {
+                int current;
+                if (i == 0)
+                {
+                    current = special[i] - bottom;
+                }
+                else if (i == special.Length)
+                {
+                    current = top - special[i - 1];
+                }
+                else
+                {
+                    current = special[i] - special[i - 1] - 1;
+                }
+                if (current > max)
+                {
+                    max = current;
+                }
+            }
+            return max;
+        }
+    }
+}
