82b05d13da5ea513a4c1992bf752fb849e487a3e
褚许辉 <736265181@qq.com>
2022-05-16 23:25:22 +0800

    面试题 04.06. 后继者

---
+++ LeetCode/SAOA/Interview_04_06_InorderSuccessor.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode.SAOA
+{
+    internal sealed class InorderSuccessorSolution
+    {
+        public TreeNode InorderSuccessor(TreeNode root, TreeNode p)
+        {
+            var stack = new Stack<TreeNode>();
+            TreeNode prev = null;
+            TreeNode curr = root;
+            while (stack.Count > 0 || curr != null)
+            {
+                while (curr != null)
+                {
+                    stack.Push(curr);
+                    curr = curr.left;
+                }
+                curr = stack.Pop();
+                if (prev == p)
+                {
+                    return curr;
+                }
+                prev = curr;
+                curr = curr.right;
+            }
+            return null;
+        }
+
+        public sealed class TreeNode
+        {
+            public int val;
+            public TreeNode left;
+            public TreeNode right;
+            public TreeNode(int x) { val = x; }
+        }
+
+    }
+}
