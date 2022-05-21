8bf8b8751be1bd17301ac1991e131433ec87fa30
褚许辉 <736265181@qq.com>
2022-05-21 12:16:12 +0800

    961. 在长度 2N 的数组中找出重复 N 次的元素

---
+++ LeetCode/SAOA/0961_RepeatedNTimes.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode.SAOA
+{
+    internal sealed class RepeatedNTimesSolution
+    {
+        public int RepeatedNTimes(int[] nums)
+        {
+            var found = new HashSet<int>();
+            foreach (var item in nums)
+            {
+                if (!found.Add(item))
+                {
+                    return item;
+                }
+            }
+            return -1;
+        }
+    }
+}
