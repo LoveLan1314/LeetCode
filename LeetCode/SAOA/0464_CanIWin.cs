911049ba701ec9191cfd319a3b436c5a55199c8e
褚许辉 <736265181@qq.com>
2022-05-22 22:15:38 +0800

    464. 我能赢吗

---
+++ LeetCode/SAOA/0464_CanIWin.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.SAOA
+{
+    internal sealed class CanIWinSolution
+    {
+        readonly Dictionary<int, bool> memo = new Dictionary<int, bool>();
+
+        public bool CanIWin(int maxChoosableInteger, int desiredTotal)
+        {
+            if ((1 + maxChoosableInteger) * (maxChoosableInteger) / 2 < desiredTotal)
+            {
+                return false;
+            }
+            return DFS(maxChoosableInteger, 0, desiredTotal, 0);
+        }
+
+        private bool DFS(int maxChoosableInteger, int usedNumbers, int desiredTotal, int currentTotal)
+        {
+            if (!memo.ContainsKey(usedNumbers))
+            {
+                bool res = false;
+                for (int i = 0; i < maxChoosableInteger; i++)
+                {
+                    if (((usedNumbers >> i) & 1) == 0)
+                    {
+                        if (i + 1 + currentTotal >= desiredTotal)
+                        {
+                            res = true;
+                            break;
+                        }
+                        if (!DFS(maxChoosableInteger, usedNumbers | (1 << i), desiredTotal, currentTotal + i + 1))
+                        {
+                            res = true;
+                            break;
+                        }
+                    }
+                }
+                memo.Add(usedNumbers, res);
+            }
+            return memo[usedNumbers];
+        }
+    }
+}
