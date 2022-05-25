ccf9189bd1a46dec5cc95b8f2ba4d36cd46e8e66
褚许辉 <736265181@qq.com>
2022-05-25 21:35:39 +0800

    467. 环绕字符串中唯一的子字符串

---
+++ LeetCode/SAOA/0467_FindSubstringInWraproundString.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+
+namespace LeetCode.SAOA
+{
+    internal sealed class FindSubstringInWraproundStringSolution
+    {
+        public int FindSubstringInWraproundString(string p)
+        {
+            int[] dp = new int[26];
+            int k = 0;
+            for (int i = 0; i < p.Length; ++i)
+            {
+                if (i > 0 && (p[i] - p[i - 1] + 26) % 26 == 1)
+                { // 字符之差为 1 或 -25
+                    ++k;
+                }
+                else
+                {
+                    k = 1;
+                }
+                dp[p[i] - 'a'] = Math.Max(dp[p[i] - 'a'], k);
+            }
+            return dp.Sum();
+        }
+    }
+}
