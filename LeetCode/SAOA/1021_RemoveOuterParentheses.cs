0eea9ebc028c13ce742e3dce56a96bffc30d7758
褚许辉 <736265181@qq.com>
2022-05-28 17:25:38 +0800

    1021. 删除最外层的括号

---
+++ LeetCode/SAOA/1021_RemoveOuterParentheses.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.SAOA
+{
+    internal sealed class RemoveOuterParenthesesSolution
+    {
+        public string RemoveOuterParentheses(string s)
+        {
+            var res = new StringBuilder();
+            Stack<char> stack = new Stack<char>();
+            foreach (char c in s)
+            {
+                if (c == ')')
+                {
+                    stack.Pop();
+                }
+                if (stack.Count > 0)
+                {
+                    res.Append(c);
+                }
+                if (c == '(')
+                {
+                    stack.Push(c);
+                }
+            }
+            return res.ToString();
+        }
+    }
+}
