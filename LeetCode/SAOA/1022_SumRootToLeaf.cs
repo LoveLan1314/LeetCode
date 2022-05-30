4cec6c6c615b53426c8f323a6eddf398f925ddde
褚许辉 <736265181@qq.com>
2022-05-30 22:40:40 +0800

    1022. 从根到叶的二进制数之和

---
+++ LeetCode/SAOA/1022_SumRootToLeaf.cs
@@ -0,0 +1,24 @@
+﻿namespace LeetCode.SAOA
+{
+    internal sealed class SumRootToLeafSolution
+    {
+        public int SumRootToLeaf(TreeNode root)
+        {
+            return DFS(root, 0);
+        }
+
+        private int DFS(TreeNode node, int value)
+        {
+            if (node == null)
+            {
+                return 0;
+            }
+            value = (value << 1) | node.val;
+            if (node.left == null && node.right == null)
+            {
+                return value;
+            }
+            return DFS(node.left, value) + DFS(node.right, value);
+        }
+    }
+}
