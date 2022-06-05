e52a488410cd6d8f41a7ccc2ef16869ba697229a
褚许辉 <736265181@qq.com>
2022-06-05 22:35:12 +0800

    478. 在圆内随机生成点

---
+++ LeetCode/SAOA/0478_RandPoint.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace LeetCode.SAOA
+{
+    internal sealed class RandPointSolution
+    {
+        private readonly Random random;
+        private readonly double xc;
+        private readonly double yc;
+        private readonly double r;
+
+        public RandPointSolution(double radius, double x_center, double y_center)
+        {
+            random = new Random();
+            xc = x_center;
+            yc = y_center;
+            r = radius;
+        }
+
+        public double[] RandPoint()
+        {
+            while (true)
+            {
+                double x = random.NextDouble() * (2 * r) - r;
+                double y = random.NextDouble() * (2 * r) - r;
+                if (x * x + y * y <= r * r)
+                {
+                    return new double[] { xc + x, yc + y };
+                }
+            }
+        }
+    }
+}
