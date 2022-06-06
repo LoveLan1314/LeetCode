791a8f9dda5da8aa1e53e77850a1962ef936c432
褚许辉 <736265181@qq.com>
2022-06-06 17:31:21 +0800

    929. 独特的电子邮件地址

---
+++ LeetCode/SAOA/0929_NumUniqueEmails.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode.SAOA
+{
+    internal sealed class NumUniqueEmailsSolution
+    {
+        public int NumUniqueEmails(string[] emails)
+        {
+            var emailSet = new HashSet<string>();
+            foreach (string email in emails)
+            {
+                int i = email.IndexOf('@');
+                string local = email.Substring(0, i).Split("+")[0]; // 去掉本地名第一个加号之后的部分
+                local = local.Replace(".", ""); // 去掉本地名中所有的句点
+                emailSet.Add(local + email.Substring(i));
+            }
+            return emailSet.Count;
+        }
+    }
+}
