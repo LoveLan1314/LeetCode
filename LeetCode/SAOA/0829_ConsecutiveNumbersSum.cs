3aa6820c4affd50a82c687c6f01a7116cb535760
褚许辉 <736265181@qq.com>
2022-06-06 17:27:31 +0800

    829. 连续整数求和

---
+++ LeetCode/SAOA/0829_ConsecutiveNumbersSum.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.SAOA
+{
+    internal sealed class ConsecutiveNumbersSumSolution
+    {
+        public int ConsecutiveNumbersSum(int n)
+        {
+            int ans = 0;
+            int bound = 2 * n;
+            for (int k = 1; k * (k + 1) <= bound; k++)
+            {
+                if (IsKConsecutive(n, k))
+                {
+                    ans++;
+                }
+            }
+            return ans;
+        }
+
+        private bool IsKConsecutive(int n, int k)
+        {
+            if (k % 2 == 1)
+            {
+                return n % k == 0;
+            }
+            else
+            {
+                return n % k != 0 && 2 * n % k == 0;
+            }
+        }
+    }
+}
