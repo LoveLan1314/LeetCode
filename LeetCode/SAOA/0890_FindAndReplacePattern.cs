0df5151dce39983c214cbe893b8319ce7fb4d0d1
褚许辉 <736265181@qq.com>
2022-06-12 23:42:52 +0800

    890. 查找和替换模式

---
+++ LeetCode/SAOA/0890_FindAndReplacePattern.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode.SAOA
+{
+    internal sealed class FindAndReplacePatternSolution
+    {
+        public IList<string> FindAndReplacePattern(string[] words, string pattern)
+        {
+            IList<string> ans = new List<string>();
+            foreach (string word in words)
+            {
+                if (Match(word, pattern) && Match(pattern, word))
+                {
+                    ans.Add(word);
+                }
+            }
+            return ans;
+        }
+
+        private bool Match(string word, string pattern)
+        {
+            Dictionary<char, char> dic = new Dictionary<char, char>();
+            for (int i = 0; i < word.Length; ++i)
+            {
+                char x = word[i], y = pattern[i];
+                if (!dic.ContainsKey(x))
+                {
+                    dic.Add(x, y);
+                }
+                else if (dic[x] != y)
+                { // word 中的同一字母必须映射到 pattern 中的同一字母上
+                    return false;
+                }
+            }
+            return true;
+        }
+
+    }
+}
