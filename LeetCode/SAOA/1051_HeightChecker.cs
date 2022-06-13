5bfc4cce0e1441c5aebe2599249bdb8006060ce5
XuhuiChu <736265181@qq.com>
2022-06-13 10:20:12 +0800

    1051. 高度检查器

---
+++ LeetCode/SAOA/1051_HeightChecker.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace LeetCode.SAOA
+{
+    internal sealed class HeightCheckerSolution
+    {
+        public int HeightChecker(int[] heights)
+        {
+            int n = heights.Length;
+            int result = 0;
+            int[] copy = new int[n];
+            Array.Copy(heights, copy, n);
+            Array.Sort(copy);
+            for (int i = 0; i < n; i++)
+            {
+                if (heights[i] != copy[i])
+                {
+                    result++;
+                }
+            }
+            return result;
+        }
+    }
+}
