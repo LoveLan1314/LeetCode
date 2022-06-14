17d394862fa641dcdc1088883216abadf14e6633
褚许辉 <736265181@qq.com>
2022-06-14 21:54:50 +0800

    面试题 16.20. T9键盘

---
+++ LeetCode/SAOA/Interview_16_20_GetValidT9Words.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode.SAOA
+{
+    internal sealed class GetValidT9WordsSolution
+    {
+        public IList<string> GetValidT9Words(string num, string[] words)
+        {
+            var pairs = new List<List<char>>() {
+                new List<char>(){ 'a', 'b', 'c' },
+                new List<char>(){ 'd', 'e', 'f' },
+                new List<char>(){ 'g', 'h', 'i' },
+                new List<char>(){ 'j', 'k', 'l' },
+                new List<char>(){ 'm', 'n', 'o' },
+                new List<char>(){ 'p', 'q', 'r', 's' },
+                new List<char>(){ 't', 'u', 'v' },
+                new List<char>(){ 'w', 'x', 'y', 'z' },
+            };
+            var result = new List<string>();
+            foreach (var word in words)
+            {
+                bool isMatch = true;
+                for (int i = 0; i < word.Length; i++)
+                {
+                    if (!pairs[num[i] - 50].Contains(word[i]))
+                    {
+                        isMatch = false;
+                        break;
+                    }
+                }
+                if (isMatch)
+                {
+                    result.Add(word);
+                }
+            }
+            return result;
+        }
+    }
+}
