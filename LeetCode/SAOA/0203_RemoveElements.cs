8f562b2ac5b4a3fa86c5ae33e7baa937b93b703a
褚许辉 <736265181@qq.com>
2022-06-15 21:49:57 +0800

    203. 移除链表元素

---
+++ LeetCode/SAOA/0203_RemoveElements.cs
@@ -0,0 +1,41 @@
+﻿namespace LeetCode.SAOA
+{
+    internal sealed class RemoveElementsSolution1
+    {
+        public ListNode RemoveElements(ListNode head, int val)
+        {
+            var node = head;
+            ListNode prev = null;
+            while (node != null)
+            {
+                if (node.val == val)
+                {
+                    if (node == head)
+                    {
+                        head = node.next;
+                    }
+                    else
+                    {
+                        prev.next = node.next;
+                    }
+                }
+                else
+                {
+                    prev = node;
+                }
+                node = node.next;
+            }
+            return head;
+        }
+
+        public ListNode RemoveElements1(ListNode head, int val)
+        {
+            if (head == null)
+            {
+                return head;
+            }
+            head.next = RemoveElements1(head.next, val);
+            return head.val == val ? head.next : head;
+        }
+    }
+}
