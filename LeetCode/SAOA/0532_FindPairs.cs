fe5a7b11c75e2481146d4f2cc4c00bbdcba3918e
XuhuiChu <736265181@qq.com>
2022-06-16 16:35:38 +0800

    Revert "Revert "532. 数组中的 k-diff 数对""
    This reverts commit af617226511e63a02b6451ff90dbe73129a0d9cb.

---
+++ LeetCode/SAOA/0532_FindPairs.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode.SAOA
+{
+    internal sealed class FindPairsSolution
+    {
+        public int FindPairs(int[] nums, int k)
+        {
+            var visited = new HashSet<int>();
+            var result = new HashSet<int>();
+            foreach (var item in nums)
+            {
+                if (visited.Contains(item - k))
+                {
+                    result.Add(item - k);
+                }
+                if (visited.Contains(item + k))
+                {
+                    result.Add(item);
+                }
+                visited.Add(item);
+            }
+            return result.Count;
+        }
+    }
+}
