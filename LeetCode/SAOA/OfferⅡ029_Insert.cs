36fe5912b8aee3a2a0d900a6d4ea7039ec3c9d4d
褚许辉 <736265181@qq.com>
2022-06-18 22:02:19 +0800

    剑指 Offer II 029. 排序的循环链表

---
+++ LeetCode/SAOA/OfferⅡ029_Insert.cs
@@ -0,0 +1,41 @@
+﻿namespace LeetCode.SAOA
+{
+    internal sealed class InsertSolution
+    {
+        public Node Insert(Node head, int insertVal)
+        {
+            Node node = new Node(insertVal);
+            if (head == null)
+            {
+                node.next = node;
+                return node;
+            }
+            if (head.next == head)
+            {
+                head.next = node;
+                node.next = head;
+                return head;
+            }
+            Node curr = head, next = head.next;
+            while (next != head)
+            {
+                if (insertVal >= curr.val && insertVal <= next.val)
+                {
+                    break;
+                }
+                if (curr.val > next.val)
+                {
+                    if (insertVal > curr.val || insertVal < next.val)
+                    {
+                        break;
+                    }
+                }
+                curr = curr.next;
+                next = next.next;
+            }
+            curr.next = node;
+            node.next = next;
+            return head;
+        }
+    }
+}
