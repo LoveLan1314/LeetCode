b132a8bfc4d806598d60298ec8719d6934bf6e19
XuhuiChu <736265181@qq.com>
2022-06-20 12:15:01 +0800

    1282. 用户分组

---
+++ LeetCode/SAOA/1282_GroupThePeople.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode.SAOA
+{
+    internal sealed class GroupThePeopleSolution
+    {
+        public IList<IList<int>> GroupThePeople(int[] groupSizes)
+        {
+            var dict = new Dictionary<int, List<int>>();
+            var result = new List<IList<int>>();
+            for (int i = 0; i < groupSizes.Length; i++)
+            {
+                var size = groupSizes[i];
+                if (dict.TryGetValue(size, out var list) && list.Count < size)
+                {
+                    list.Add(i);
+                }
+                else
+                {
+                    list = new List<int>()
+                    {
+                        i
+                    };
+                    result.Add(list);
+                    dict[size] = list;
+                }
+            }
+            return result;
+        }
+    }
+}
