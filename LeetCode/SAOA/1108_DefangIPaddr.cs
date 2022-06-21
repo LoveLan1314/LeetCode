c3fbfba93cf0858785d73c36d0f598c675369e73
褚许辉 <736265181@qq.com>
2022-06-21 20:12:19 +0800

    1108. IP 地址无效化

---
+++ LeetCode/SAOA/1108_DefangIPaddr.cs
@@ -0,0 +1,10 @@
+﻿namespace LeetCode.SAOA
+{
+    internal sealed class DefangIPaddrSolution
+    {
+        public string DefangIPaddr(string address)
+        {
+            return address.Replace(".", "[.]");
+        }
+    }
+}
