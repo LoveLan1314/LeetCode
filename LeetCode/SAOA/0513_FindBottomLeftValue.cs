e5e471bb18b54dc551b679c9ebc769134d7a4cb1
褚许辉 <736265181@qq.com>
2022-06-22 21:13:29 +0800

    513. 找树左下角的值

---
+++ LeetCode/SAOA/0513_FindBottomLeftValue.cs
@@ -0,0 +1,29 @@
+﻿namespace LeetCode.SAOA
+{
+    internal sealed class FindBottomLeftValueSolution
+    {
+        private int curVal;
+        private int curHeight;
+        public int FindBottomLeftValue(TreeNode root)
+        {
+            DFS(root, 0);
+            return curVal;
+        }
+
+        private void DFS(TreeNode root, int height)
+        {
+            if (root == null)
+            {
+                return;
+            }
+            height++;
+            DFS(root.left, height);
+            DFS(root.right, height);
+            if (height > curHeight)
+            {
+                curHeight = height;
+                curVal = root.val;
+            }
+        }
+    }
+}
