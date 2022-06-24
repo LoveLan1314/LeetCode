d2da8443e958a4cae5898d71d963b1ab30f5b5d1
XuhuiChu <736265181@qq.com>
2022-06-24 16:01:30 +0800

    515. 在每个树行中找最大值

---
+++ LeetCode/SAOA/0515_LargestValues.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode.SAOA
+{
+    internal sealed class LargestValuesSolution
+    {
+        public IList<int> LargestValues(TreeNode root)
+        {
+            var result = new List<int>();
+            var current = new Queue<TreeNode>();
+            var next = new Queue<TreeNode>();
+            current.Enqueue(root);
+            var max = int.MinValue;
+            while (current.Count > 0)
+            {
+                var node = current.Dequeue();
+                if (node != null)
+                {
+                    next.Enqueue(node.left);
+                    next.Enqueue(node.right);
+                    if (node.val > max)
+                    {
+                        max = node.val;
+                    }
+                }
+                if (current.Count == 0 && next.Count > 0)
+                {
+                    current = next;
+                    next = new Queue<TreeNode>();
+                    result.Add(max);
+                    max = int.MinValue;
+                }
+            }
+            return result;
+        }
+    }
+}
