76d19c87476075d2483a35d47777932672147fd3
褚许辉 <736265181@qq.com>
2022-06-26 22:38:11 +0800

    710. 黑名单中的随机数

---
+++ LeetCode/SAOA/0710_Pick.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LeetCode.SAOA
+{
+    internal sealed class PickSolution1
+    {
+        private readonly Dictionary<int, int> b2w;
+        private readonly Random random;
+        private readonly int bound;
+        public PickSolution1(int n, int[] blacklist)
+        {
+            b2w = new Dictionary<int, int>();
+            random = new Random();
+            int m = blacklist.Length;
+            bound = n - m;
+            ISet<int> black = new HashSet<int>();
+            foreach (int b in blacklist)
+            {
+                if (b >= bound)
+                {
+                    black.Add(b);
+                }
+            }
+
+            int w = bound;
+            foreach (int b in blacklist)
+            {
+                if (b < bound)
+                {
+                    while (black.Contains(w))
+                    {
+                        ++w;
+                    }
+                    b2w.Add(b, w);
+                    ++w;
+                }
+            }
+        }
+
+        public int Pick()
+        {
+            int x = random.Next(bound);
+            return b2w.ContainsKey(x) ? b2w[x] : x;
+        }
+    }
+}
