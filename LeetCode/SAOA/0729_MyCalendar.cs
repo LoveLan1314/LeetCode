31d297fee9ce41d147607562f9c6b19f0f91d82a
褚许辉 <736265181@qq.com>
2022-07-05 22:39:08 +0800

    729. 我的日程安排表 I

---
+++ LeetCode/SAOA/0729_MyCalendar.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LeetCode.SAOA
+{
+    internal sealed class MyCalendar
+    {
+        private readonly IList<Tuple<int, int>> _booked;
+
+        public MyCalendar()
+        {
+            _booked = new List<Tuple<int, int>>();
+        }
+
+        public bool Book(int start, int end)
+        {
+            foreach (var tuple in _booked)
+            {
+                int l = tuple.Item1, r = tuple.Item2;
+                if (l < end && start < r)
+                {
+                    return false;
+                }
+            }
+            _booked.Add(new Tuple<int, int>(start, end));
+            return true;
+        }
+    }
+}
