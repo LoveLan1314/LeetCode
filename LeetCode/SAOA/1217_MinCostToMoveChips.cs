a7fd6fa9bab61e9b829a08a83bd4ecc3771a8805
XuhuiChu <736265181@qq.com>
2022-07-08 18:54:11 +0800

    1217. 玩筹码

---
+++ LeetCode/SAOA/1217_MinCostToMoveChips.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace LeetCode.SAOA
+{
+    internal sealed class MinCostToMoveChipsSolution
+    {
+        public int MinCostToMoveChips(int[] position)
+        {
+            int even = 0, odd = 0;
+            foreach (var pos in position)
+            {
+                if ((pos & 1) != 0)
+                {
+                    odd++;
+                }
+                else
+                {
+                    even++;
+                }
+            }
+            return Math.Min(odd, even);
+        }
+    }
+}
