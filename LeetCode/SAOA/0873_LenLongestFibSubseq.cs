82a28f7c470004320454c1ff08da924691ad8091
褚许辉 <736265181@qq.com>
2022-07-09 23:48:00 +0800

    873. 最长的斐波那契子序列的长度

---
+++ LeetCode/SAOA/0873_LenLongestFibSubseq.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LeetCode.SAOA
+{
+    internal sealed class LenLongestFibSubseqSolution
+    {
+        public int LenLongestFibSubseq(int[] arr)
+        {
+            var indices = new Dictionary<int, int>();
+            int n = arr.Length;
+            for (int i = 0; i < n; i++)
+            {
+                indices.Add(arr[i], i);
+            }
+            int[,] dp = new int[n, n];
+            int ans = 0;
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = i - 1; j >= 0 && arr[j] * 2 > arr[i]; j--)
+                {
+                    int k = indices.ContainsKey(arr[i] - arr[j]) ? indices[arr[i] - arr[j]] : -1;
+                    if (k >= 0)
+                    {
+                        dp[j, i] = Math.Max(dp[k, j] + 1, 3);
+                    }
+                    ans = Math.Max(ans, dp[j, i]);
+                }
+            }
+            return ans;
+        }
+    }
+}
