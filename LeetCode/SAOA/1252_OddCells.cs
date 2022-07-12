06ef0ddb133067f98e836eb4c7accc8e987dee51
褚许辉 <736265181@qq.com>
2022-07-12 22:36:30 +0800

    1252. 奇数值单元格的数目

---
+++ LeetCode/SAOA/1252_OddCells.cs
@@ -0,0 +1,33 @@
+﻿namespace LeetCode.SAOA
+{
+    internal sealed class OddCellsSolution
+    {
+        public int OddCells(int m, int n, int[][] indices)
+        {
+            var data = new int[m, n];
+            foreach (var item in indices)
+            {
+                for (int i = 0; i < n; i++)
+                {
+                    data[item[0], i]++;
+                }
+                for (int i = 0; i < m; i++)
+                {
+                    data[i, item[1]]++;
+                }
+            }
+            var result = 0;
+            for (int i = 0; i < m; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    if (data[i, j] % 2 == 1)
+                    {
+                        result++;
+                    }
+                }
+            }
+            return result;
+        }
+    }
+}
