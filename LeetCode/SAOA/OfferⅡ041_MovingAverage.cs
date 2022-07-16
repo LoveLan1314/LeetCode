57499e0971b7d310a71c63f08047e375276ed557
褚许辉 <736265181@qq.com>
2022-07-16 22:44:54 +0800

    剑指 Offer II 041. 滑动窗口的平均值

---
+++ LeetCode/SAOA/OfferⅡ041_MovingAverage.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace LeetCode.SAOA
+{
+    internal sealed class MovingAverage
+    {
+        private readonly Queue<int> _list = new Queue<int>();
+        private readonly int _size;
+
+        public MovingAverage(int size)
+        {
+            _size = size;
+        }
+
+        public double Next(int val)
+        {
+            if (_list.Count == _size)
+            {
+                _list.Dequeue();
+            }
+            _list.Enqueue(val);
+            return (double)_list.Sum() / _list.Count;
+        }
+    }
+}
