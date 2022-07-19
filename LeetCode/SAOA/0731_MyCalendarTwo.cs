8abccc02e011e7f6126513b058b59648d695fd0c
褚许辉 <736265181@qq.com>
2022-07-19 09:14:12 +0800

    731. 我的日程安排表 II

---
+++ LeetCode/SAOA/0731_MyCalendarTwo.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LeetCode.SAOA
+{
+    internal sealed class MyCalendarTwo
+    {
+        private readonly List<Tuple<int, int>> booked;
+        private readonly List<Tuple<int, int>> overlaps;
+
+        public MyCalendarTwo()
+        {
+            booked = new List<Tuple<int, int>>();
+            overlaps = new List<Tuple<int, int>>();
+        }
+
+        public bool Book(int start, int end)
+        {
+            foreach (Tuple<int, int> tuple in overlaps)
+            {
+                int l = tuple.Item1, r = tuple.Item2;
+                if (l < end && start < r)
+                {
+                    return false;
+                }
+            }
+            foreach (Tuple<int, int> tuple in booked)
+            {
+                int l = tuple.Item1, r = tuple.Item2;
+                if (l < end && start < r)
+                {
+                    overlaps.Add(new Tuple<int, int>(Math.Max(l, start), Math.Min(r, end)));
+                }
+            }
+            booked.Add(new Tuple<int, int>(start, end));
+            return true;
+        }
+    }
+}
