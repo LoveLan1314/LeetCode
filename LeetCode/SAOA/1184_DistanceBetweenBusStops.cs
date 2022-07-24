7139f2fd2b4e0ba0214a7be773a3305477f2a95f
褚许辉 <736265181@qq.com>
2022-07-24 23:27:44 +0800

    1184. 公交站间的距离

---
+++ LeetCode/SAOA/1184_DistanceBetweenBusStops.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+
+namespace LeetCode.SAOA
+{
+    internal sealed class DistanceBetweenBusStopsSolution
+    {
+        public int DistanceBetweenBusStops(int[] distance, int start, int destination)
+        {
+            var sum = distance.Sum();
+            var length = 0;
+            int left = Math.Min(start, destination);
+            int right = Math.Max(start, destination);
+            for (int i = left; i < right; i++)
+            {
+                length += distance[i];
+            }
+            return Math.Min(length, sum - length);
+        }
+    }
+}
