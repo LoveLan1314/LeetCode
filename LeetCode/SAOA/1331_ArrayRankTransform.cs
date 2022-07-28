972e9228a4b552c3463a4712933a97ed5141c713
XuhuiChu <736265181@qq.com>
2022-07-28 18:16:43 +0800

    1331. 数组序号转换

---
+++ LeetCode/SAOA/1331_ArrayRankTransform.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace LeetCode.SAOA
+{
+    internal sealed class ArrayRankTransformSolution
+    {
+        public int[] ArrayRankTransform(int[] arr)
+        {
+            var pairs = new Dictionary<int, int>();
+            for (int i = 0; i < arr.Length; i++)
+            {
+                pairs.Add(i, arr[i]);
+            }
+            var order = 0;
+            var prevValue = int.MinValue;
+            foreach (var item in pairs.OrderBy(i => i.Value))
+            {
+                if (order == 0 || prevValue != item.Value)
+                {
+                    order++;
+                }
+                arr[item.Key] = order;
+                prevValue = item.Value;
+            }
+            return arr;
+        }
+    }
+}
