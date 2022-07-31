5ee2c1516279d66f1ad6588f29a22d8708b0eb00
褚许辉 <736265181@qq.com>
2022-07-31 22:52:26 +0800

    1161. 最大层内元素和

---
+++ LeetCode/SAOA/1161_MaxLevelSum.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode.SAOA
+{
+    internal sealed class MaxLevelSumSolution
+    {
+        public int MaxLevelSum(TreeNode root)
+        {
+            var queue = new Queue<TreeNode>();
+            queue.Enqueue(root);
+            var nextQueue = new Queue<TreeNode>();
+            var level = 1;
+            var max = int.MinValue;
+            var sum = 0;
+            var maxLevel = -1;
+            while (queue.Count > 0)
+            {
+                var node = queue.Dequeue();
+                sum += node.val;
+                if (node.left != null)
+                {
+                    nextQueue.Enqueue(node.left);
+                }
+                if (node.right != null)
+                {
+                    nextQueue.Enqueue(node.right);
+                }
+                if (queue.Count == 0)
+                {
+                    if (sum > max)
+                    {
+                        maxLevel = level;
+                        max = sum;
+                    }
+                    sum = 0;
+                    queue = nextQueue; 
+                    nextQueue = new Queue<TreeNode>();
+                    level++;
+                }
+            }
+            return maxLevel;
+        }
+    }
+}
