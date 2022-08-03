c43f3882fb3e43d4bf529bb5bb7a5caa9f3f83f7
XuhuiChu <736265181@qq.com>
2022-08-03 17:42:08 +0800

    899. 有序队列

---
+++ LeetCode/SAOA/0899_OrderlyQueue.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+
+namespace LeetCode.SAOA
+{
+    internal sealed class OrderlyQueueSolution
+    {
+        public string OrderlyQueue(string s, int k)
+        {
+            if (k == 1)
+            {
+                var smallest = s;
+                var sb = new StringBuilder(s);
+                int n = s.Length;
+                for (int i = 1; i < n; i++)
+                {
+                    var c = sb[0];
+                    sb.Remove(0, 1);
+                    sb.Append(c);
+                    var newStr = sb.ToString();
+                    if (newStr.CompareTo(smallest) < 0)
+                    {
+                        smallest = newStr;
+                    }
+                }
+                return smallest;
+            }
+            else
+            {
+                var arr = s.ToArray();
+                Array.Sort(arr);
+                return new string(arr);
+            }
+        }
+    }
+}
