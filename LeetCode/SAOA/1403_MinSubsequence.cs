90bf62d76e8f98e34c8deec5f1a835669c781a62
XuhuiChu <736265181@qq.com>
2022-08-04 16:47:34 +0800

    1403. 非递增顺序的最小子序列

---
+++ LeetCode/SAOA/1403_MinSubsequence.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LeetCode.SAOA
+{
+    internal sealed class MinSubsequenceSolution
+    {
+        public IList<int> MinSubsequence(int[] nums)
+        {
+            Array.Sort(nums);
+            var half = nums.Sum() / 2;
+            var sum = 0;
+            var result = new List<int>();
+            for (int i = nums.Length - 1; i >= 0; i--)
+            {
+                result.Add(nums[i]);
+                sum += nums[i];
+                if (sum > half)
+                {
+                    break;
+                }
+            }
+            return result;
+        }
+    }
+}
