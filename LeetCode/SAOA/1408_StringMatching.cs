ca1fb7bdaa93d4e4b49cc7226657a2c83e70a48e
褚许辉 <736265181@qq.com>
2022-08-06 19:34:25 +0800

    1408. 数组中的字符串匹配

---
+++ LeetCode/SAOA/1408_StringMatching.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode.SAOA
+{
+    internal sealed class StringMatchingSolution
+    {
+        public IList<string> StringMatching(string[] words)
+        {
+            var ret = new List<string>();
+            for (int i = 0; i < words.Length; i++)
+            {
+                for (int j = 0; j < words.Length; j++)
+                {
+                    if (i != j && words[j].Contains(words[i]))
+                    {
+                        ret.Add(words[i]);
+                        break;
+                    }
+                }
+            }
+            return ret;
+        }
+    }
+}
