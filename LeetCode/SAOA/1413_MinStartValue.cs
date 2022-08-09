3e5a67b9628162605892168e2e902bc1c3391933
XuhuiChu <736265181@qq.com>
2022-08-09 12:04:04 +0800

    1413. 逐步求和得到正数的最小值

---
+++ LeetCode/SAOA/1413_MinStartValue.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace LeetCode.SAOA
+{
+    internal sealed class MinStartValueSolution
+    {
+        public int MinStartValue(int[] nums)
+        {
+            var minValue = int.MaxValue;
+            var current = 0;
+            foreach (var item in nums)
+            {
+                current += item;
+                minValue = Math.Min(minValue, current);
+            }
+            var result = 1 - minValue;
+            if (result > 0)
+            {
+                return result;
+            }
+            else
+            {
+                return 1;
+            }
+        }
+    }
+}
