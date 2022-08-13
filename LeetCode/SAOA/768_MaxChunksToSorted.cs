2f2f64a36312741d9546989e02598fc3d51b690d
褚许辉 <736265181@qq.com>
2022-08-13 12:49:45 +0800

    768. 最多能完成排序的块 II

---
+++ LeetCode/SAOA/768_MaxChunksToSorted.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode.SAOA
+{
+    internal sealed class MaxChunksToSortedSolution
+    {
+        public int MaxChunksToSorted(int[] arr)
+        {
+            var stack = new Stack<int>();
+            foreach (var num in arr)
+            {
+                if (stack.Count == 0 || num >= stack.Peek())
+                {
+                    stack.Push(num);
+                }
+                else
+                {
+                    int mx = stack.Pop();
+                    while (stack.Count > 0 && stack.Peek() > num)
+                    {
+                        stack.Pop();
+                    }
+                    stack.Push(mx);
+                }
+            }
+            return stack.Count;
+        }
+    }
+}
