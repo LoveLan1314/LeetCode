19905464b523fc4543f21ce13666a4449815c55b
褚许辉 <736265181@qq.com>
2022-08-14 14:39:29 +0800

    1422. 分割字符串的最大得分

---
+++ LeetCode/SAOA/1422_MaxScore.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace LeetCode.SAOA
+{
+    internal sealed class MaxScoreSolution
+    {
+        public int MaxScore(string s)
+        {
+            var result = 0;
+            for (int i = 1; i < s.Length; i++)
+            {
+                var sum = 0;
+                for (int j = 0; j < i; j++)
+                {
+                    if (s[j] == '0')
+                    {
+                        sum++;
+                    }
+                }
+                for (int j = i; j < s.Length; j++)
+                {
+                    if (s[j] == '1')
+                    {
+                        sum++;
+                    }
+                }
+                result = Math.Max(result, sum);
+            }
+            return result;
+        }
+    }
+}
