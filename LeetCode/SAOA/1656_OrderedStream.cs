257c269cba0682221c6675ae27191ce8c0042fd4
XuhuiChu <736265181@qq.com>
2022-08-16 17:41:50 +0800

    1656. 设计有序流

---
+++ LeetCode/SAOA/1656_OrderedStream.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode.SAOA
+{
+    internal sealed class OrderedStream
+    {
+        private readonly string[] _stream;
+        private int _ptr;
+
+        public OrderedStream(int n)
+        {
+            _stream = new string[n + 1];
+            _ptr = 1;
+        }
+
+        public IList<string> Insert(int idKey, string value)
+        {
+            _stream[idKey] = value;
+            IList<string> res = new List<string>();
+            while (_ptr < _stream.Length && _stream[_ptr] != null)
+            {
+                res.Add(_stream[_ptr]);
+                ++_ptr;
+            }
+            return res;
+        }
+    }
+}
