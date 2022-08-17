7083e7f94f9c312e3c9bfb07ce532ee63238230d
褚许辉 <736265181@qq.com>
2022-08-17 20:28:24 +0800

    1302. 层数最深叶子节点的和

---
+++ LeetCode/SAOA/1302_DeepestLeavesSum.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode.SAOA
+{
+    internal sealed class DeepestLeavesSumSolution
+    {
+        public int DeepestLeavesSum(TreeNode root)
+        {
+            var sum = 0;
+            var queue = new Queue<TreeNode>();
+            queue.Enqueue(root);
+            while (queue.Count > 0)
+            {
+                sum = 0;
+                var size = queue.Count;
+                for (int i = 0; i < size; i++)
+                {
+                    var node = queue.Dequeue();
+                    sum += node.val;
+                    if (node.left != null)
+                    {
+                        queue.Enqueue(node.left);
+                    }
+                    if (node.right != null)
+                    {
+                        queue.Enqueue(node.right);
+                    }
+                }
+            }
+            return sum;
+        }
+    }
+}
