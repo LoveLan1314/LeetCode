224149b54f450dfbe8426be8facab4381f5a7a4f
褚许辉 <736265181@qq.com>
2022-08-20 16:33:15 +0800

    654. 最大二叉树

---
+++ LeetCode/SAOA/0654_ConstructMaximumBinaryTree.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.SAOA
+{
+    internal sealed class ConstructMaximumBinaryTreeSolution
+    {
+        public TreeNode ConstructMaximumBinaryTree(int[] nums)
+        {
+            return Construct(nums, 0, nums.Length - 1);
+        }
+
+        private TreeNode Construct(int[] nums, int left, int right)
+        {
+            if (left > right)
+            {
+                return null;
+            }
+            int best = left;
+            for (int i = left + 1; i <= right; ++i)
+            {
+                if (nums[i] > nums[best])
+                {
+                    best = i;
+                }
+            }
+            var node = new TreeNode(nums[best]);
+            node.left = Construct(nums, left, best - 1);
+            node.right = Construct(nums, best + 1, right);
+            return node;
+        }
+    }
+}
