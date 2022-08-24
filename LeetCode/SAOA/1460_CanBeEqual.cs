343fc3b83dcd958843d99b15112c14a99a9e8309
XuhuiChu <736265181@qq.com>
2022-08-24 12:16:32 +0800

    1460. 通过翻转子数组使两个数组相等

---
+++ LeetCode/SAOA/1460_CanBeEqual.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Linq;
+
+namespace LeetCode.SAOA
+{
+    internal sealed class CanBeEqualSolution
+    {
+        public bool CanBeEqual(int[] target, int[] arr)
+        {
+            Array.Sort(target);
+            Array.Sort(arr);
+            return target.SequenceEqual(arr);
+        }
+    }
+}
