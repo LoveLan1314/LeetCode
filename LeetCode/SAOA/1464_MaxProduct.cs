e0afec913f2b4e4782429145f23437e8aa65457d
XuhuiChu <736265181@qq.com>
2022-08-26 16:23:58 +0800

    1464. 数组中两元素的最大乘积

---
+++ LeetCode/SAOA/1464_MaxProduct.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace LeetCode.SAOA
+{
+    internal sealed class MaxProductSolution
+    {
+        public int MaxProduct(int[] nums)
+        {
+            Array.Sort(nums);
+            return (nums[^1] - 1) * (nums[^2] - 1);
+        }
+    }
+}
