853b76cbb56ea60dd80bafe1e0be8b449274edec
XuhuiChu <736265181@qq.com>
2022-08-29 10:48:43 +0800

    1470. 重新排列数组

---
+++ LeetCode/SAOA/1470_Shuffle.cs
@@ -0,0 +1,16 @@
+﻿namespace LeetCode.SAOA
+{
+    internal sealed class ShuffleSolution
+    {
+        public int[] Shuffle(int[] nums, int n)
+        {
+            var result = new int[2 * n];
+            for (int i = 0; i < n; i++)
+            {
+                result[2 * i] = nums[i];
+                result[2 * i + 1] = nums[n + i];
+            }
+            return result;
+        }
+    }
+}
