32b8d6df7f2c9831f0c5678b7f81a838b472509d
XuhuiChu <736265181@qq.com>
2022-08-31 15:21:33 +0800

    946. 验证栈序列

---
+++ LeetCode/SAOA/0946_ValidateStackSequences.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode.SAOA
+{
+    internal sealed class ValidateStackSequencesSolution
+    {
+        public bool ValidateStackSequences(int[] pushed, int[] popped)
+        {
+            var stack = new Stack<int>();
+            for (int i = 0, j = 0; i < pushed.Length; i++)
+            {
+                stack.Push(pushed[i]);
+                while (stack.Count > 0 && stack.Peek() == popped[j])
+                {
+                    stack.Pop();
+                    j++;
+                }
+            }
+            return stack.Count == 0;
+        }
+    }
+}
