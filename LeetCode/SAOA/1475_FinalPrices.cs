e6f9c822605321a15fc0fed173b8b56ccf2a0795
XuhuiChu <736265181@qq.com>
2022-09-01 16:45:39 +0800

    1475. 商品折扣后的最终价格

---
+++ LeetCode/SAOA/1475_FinalPrices.cs
@@ -0,0 +1,21 @@
+﻿namespace LeetCode.SAOA
+{
+    internal sealed class FinalPricesSolution
+    {
+        public int[] FinalPrices(int[] prices)
+        {
+            for (int i = 0; i < prices.Length; i++)
+            {
+                for (int j = i + 1; j < prices.Length; j++)
+                {
+                    if (prices[j] <= prices[i])
+                    {
+                        prices[i] -= prices[j];
+                        break;
+                    }
+                }
+            }
+            return prices;
+        }
+    }
+}
