fad0483d4f353483f2cfd96a9a36c58a9318d115
褚许辉 <736265181@qq.com>
2022-09-04 14:21:35 +0800

    309周赛

---
+++ LeetCode/SAOA/6167_CheckDistances.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode.SAOA
+{
+    internal sealed class CheckDistancesSolution
+    {
+        public bool CheckDistances(string s, int[] distance)
+        {
+            var set = new HashSet<char>();
+            for (int i = 0; i < s.Length; i++)
+            {
+                var c = s[i];
+                if (!set.Contains(c))
+                {
+                    set.Add(c);
+                    for (int j = i + 1; j < s.Length; j++)
+                    {
+                        if (s[j] == c)
+                        {
+                            if (distance[c - 'a'] != j - i - 1)
+                            {
+                                return false;
+                            }
+                        }
+                    }
+                }
+            }
+            return true;
+        }
+    }
+}
