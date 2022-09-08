a319d4b74d41f5ddce213405401077cee8c8eed0
XuhuiChu <736265181@qq.com>
2022-09-08 17:24:54 +0800

    667. 优美的排列 II

---
+++ LeetCode/SAOA/0667_ConstructArray.cs
@@ -0,0 +1,27 @@
+﻿namespace LeetCode.SAOA
+{
+    internal sealed class ConstructArraySolution
+    {
+        public int[] ConstructArray(int n, int k)
+        {
+            int[] answer = new int[n];
+            int idx = 0;
+            for (int i = 1; i < n - k; ++i)
+            {
+                answer[idx] = i;
+                ++idx;
+            }
+            for (int i = n - k, j = n; i <= j; ++i, --j)
+            {
+                answer[idx] = i;
+                ++idx;
+                if (i != j)
+                {
+                    answer[idx] = j;
+                    ++idx;
+                }
+            }
+            return answer;
+        }
+    }
+}
