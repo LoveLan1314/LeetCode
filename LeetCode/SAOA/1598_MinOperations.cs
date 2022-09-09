8f59a01037ffa1d7e6873a509205cbb043d6a8bf
XuhuiChu <736265181@qq.com>
2022-09-09 11:15:12 +0800

    1598. 文件夹操作日志搜集器

---
+++ LeetCode/SAOA/1598_MinOperations.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode.SAOA
+{
+    internal sealed class MinOperationsSolution
+    {
+        public int MinOperations(string[] logs)
+        {
+            var stack = new Stack<string>();
+            foreach (var item in logs)
+            {
+                if (item == "../")
+                {
+                    if (stack.Count > 0)
+                    {
+                        stack.Pop();
+                    }
+                }
+                else if (item == "./")
+                {
+
+                }
+                else
+                {
+                    stack.Push(item);
+                }
+            }
+            return stack.Count;
+        }
+
+        public int MinOperations1(string[] logs)
+        {
+            var depth = 0;
+            foreach (var item in logs)
+            {
+                if (item == "../")
+                {
+                    if (depth > 0)
+                    {
+                        depth--;
+                    }
+                }
+                else if (item == "./")
+                {
+
+                }
+                else
+                {
+                    depth++;
+                }
+            }
+            return depth;
+        }
+    }
+}
