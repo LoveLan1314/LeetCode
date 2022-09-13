4b5e3fd159c2dc91152d34a2b15aa8083a448789
XuhuiChu <736265181@qq.com>
2022-09-13 16:51:22 +0800

    670. 最大交换

---
+++ LeetCode/SAOA/0670_MaximumSwap.cs
@@ -0,0 +1,37 @@
+﻿namespace LeetCode.SAOA
+{
+    internal sealed class MaximumSwapSolution
+    {
+        public int MaximumSwap(int num)
+        {
+            //每一位数字应该不小于所有排它后面的数字
+            char[] charArray = num.ToString().ToCharArray();
+            int n = charArray.Length;
+            int maxIdx = n - 1;
+            int idx1 = -1, idx2 = -1;
+            for (int i = n - 1; i >= 0; i--)
+            {
+                if (charArray[i] > charArray[maxIdx])
+                {
+                    maxIdx = i;
+                }
+                else if (charArray[i] < charArray[maxIdx])
+                {
+                    idx1 = i;
+                    idx2 = maxIdx;
+                }
+            }
+            if (idx1 >= 0)
+            {
+                char temp = charArray[idx1];
+                charArray[idx1] = charArray[idx2];
+                charArray[idx2] = temp;
+                return int.Parse(charArray);
+            }
+            else
+            {
+                return num;
+            }
+        }
+    }
+}
