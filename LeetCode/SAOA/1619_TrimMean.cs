ff8f09c343e707f75bbf8a79b466e81be17352e0
XuhuiChu <736265181@qq.com>
2022-09-14 09:53:17 +0800

    1619. 删除某些元素后的数组均值

---
+++ LeetCode/SAOA/1619_TrimMean.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace LeetCode.SAOA
+{
+    internal sealed class TrimMeanSolution
+    {
+        public double TrimMean(int[] arr)
+        {
+            var n = arr.Length;
+            var removeCount = n / 20;
+            Array.Sort(arr);
+            var sum = 0;
+            for (int i = removeCount; i < n - removeCount; i++)
+            {
+                sum += arr[i];
+            }
+            return (double)sum / (n - removeCount * 2);
+        }
+    }
+}
