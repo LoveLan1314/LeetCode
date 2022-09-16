47da34dd167d87a634b352ff73d2b3a09f71bb0f
褚许辉 <736265181@qq.com>
2022-09-17 07:36:08 +0800

    1624. 两个相同字符之间的最长子字符串

---
+++ LeetCode/SAOA/1624_MaxLengthBetweenEqualCharacters.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LeetCode.SAOA
+{
+    internal sealed class MaxLengthBetweenEqualCharactersSolution
+    {
+        public int MaxLengthBetweenEqualCharacters(string s)
+        {
+            var pairs = new Dictionary<char, List<int>>();
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (pairs.TryGetValue(s[i], out var list))
+                {
+                    list.Add(i);
+                }
+                else
+                {
+                    pairs.Add(s[i], new List<int>() { i });
+                }
+            }
+            var max = -1;
+            foreach (var item in pairs)
+            {
+                if (item.Value.Count >= 2)
+                {
+                    max = Math.Max(max, item.Value[^1] - item.Value[0] - 1);
+                }
+            }
+            return max;
+        }
+    }
+}
