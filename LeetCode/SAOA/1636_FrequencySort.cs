06dfd85412d43133cfb763d61570edaeee383cf6
XuhuiChu <736265181@qq.com>
2022-09-19 17:21:29 +0800

    1636. 按照频率将数组升序排序

---
+++ LeetCode/SAOA/1636_FrequencySort.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode.SAOA
+{
+    internal sealed class FrequencySortSolution1
+    {
+        public int[] FrequencySort(int[] nums)
+        {
+            var pairs = new Dictionary<int, int>();
+            foreach (int num in nums)
+            {
+                if (pairs.ContainsKey(num))
+                {
+                    pairs[num]++;
+                }
+                else
+                {
+                    pairs.Add(num, 1);
+                }
+            }
+            var list = new List<int>();
+            foreach (int num in nums)
+            {
+                list.Add(num);
+            }
+            list.Sort((a, b) =>
+            {
+                int cnt1 = pairs[a], cnt2 = pairs[b];
+                return cnt1 != cnt2 ? cnt1 - cnt2 : b - a;
+            });
+            return list.ToArray();
+        }
+    }
+}
