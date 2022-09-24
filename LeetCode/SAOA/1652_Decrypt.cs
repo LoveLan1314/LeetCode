6dc791fd06f7957654cc398dc96e2a19a16335c2
XuhuiChu <736265181@qq.com>
2022-09-24 16:21:23 +0800

    1652. 拆炸弹

---
+++ LeetCode/SAOA/1652_Decrypt.cs
@@ -0,0 +1,34 @@
+﻿namespace LeetCode.SAOA
+{
+    internal sealed class DecryptSolution
+    {
+        public int[] Decrypt(int[] code, int k)
+        {
+            int n = code.Length;
+            int[] result = new int[n];
+            if (k == 0)
+            {
+                return result;
+            }
+            for (int i = 0; i < n; i++)
+            {
+                if (k > 0)
+                {
+                    for (int j = i + 1; j < i + k + 1; j++)
+                    {
+                        result[i] += code[j % n];
+                    }
+                }
+                else
+                {
+                    //此处k为负数
+                    for (int j = i + k; j < i; j++)
+                    {
+                        result[i] += code[(j + n) % n];
+                    }
+                }
+            }
+            return result;
+        }
+    }
+}
