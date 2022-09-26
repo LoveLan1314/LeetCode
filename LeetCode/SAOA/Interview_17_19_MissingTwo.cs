0df8e66921883906837cbd2044354aa53a01e911
褚许辉 <736265181@qq.com>
2022-09-26 20:44:39 +0800

    面试题 17.19. 消失的两个数字

---
+++ LeetCode/SAOA/Interview_17_19_MissingTwo.cs
@@ -0,0 +1,45 @@
+﻿namespace LeetCode.SAOA
+{
+    internal sealed class MissingTwoSolution
+    {
+        public int[] MissingTwo(int[] nums)
+        {
+            int xorsum = 0;
+            int n = nums.Length + 2;
+            foreach (int num in nums)
+            {
+                xorsum ^= num;
+            }
+            for (int i = 1; i <= n; i++)
+            {
+                xorsum ^= i;
+            }
+            // 防止溢出
+            int lsb = (xorsum == int.MinValue ? xorsum : xorsum & (-xorsum));
+            int type1 = 0, type2 = 0;
+            foreach (int num in nums)
+            {
+                if ((num & lsb) != 0)
+                {
+                    type1 ^= num;
+                }
+                else
+                {
+                    type2 ^= num;
+                }
+            }
+            for (int i = 1; i <= n; i++)
+            {
+                if ((i & lsb) != 0)
+                {
+                    type1 ^= i;
+                }
+                else
+                {
+                    type2 ^= i;
+                }
+            }
+            return new int[] { type1, type2 };
+        }
+    }
+}
