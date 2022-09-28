6a5650a6e983f156133435c925333dddaec8097a
褚许辉 <736265181@qq.com>
2022-09-28 20:22:31 +0800

    面试题 17.09. 第 k 个数

---
+++ LeetCode/SAOA/Interview_17_09_GetKthMagicNumber.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace LeetCode.SAOA
+{
+    internal sealed class GetKthMagicNumberSolution
+    {
+        public int GetKthMagicNumber(int k)
+        {
+            int[] dp = new int[k + 1];
+            dp[1] = 1;
+            int p3 = 1, p5 = 1, p7 = 1;
+            for (int i = 2; i <= k; i++)
+            {
+                int num3 = dp[p3] * 3, num5 = dp[p5] * 5, num7 = dp[p7] * 7;
+                dp[i] = Math.Min(Math.Min(num3, num5), num7);
+                if (dp[i] == num3)
+                {
+                    p3++;
+                }
+                if (dp[i] == num5)
+                {
+                    p5++;
+                }
+                if (dp[i] == num7)
+                {
+                    p7++;
+                }
+            }
+            return dp[k];
+        }
+    }
+}
