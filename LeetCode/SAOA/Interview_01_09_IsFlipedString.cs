ea1b466047b221a86b087eb5655ca3db9f2ee7e7
褚许辉 <736265181@qq.com>
2022-09-29 19:17:10 +0800

    面试题 01.09. 字符串轮转

---
+++ LeetCode/SAOA/Interview_01_09_IsFlipedString.cs
@@ -0,0 +1,15 @@
+﻿namespace LeetCode.SAOA
+{
+    internal sealed class IsFlipedStringSolution
+    {
+        public bool IsFlipedString(string s1, string s2)
+        {
+            if (s1.Length != s2.Length)
+            {
+                return false;
+            }
+            var s = s2 + s2;
+            return s.Contains(s1);
+        }
+    }
+}
