406e7d3fac717544d07249b73f55e5f2da61772b
褚许辉 <736265181@qq.com>
2022-10-03 19:15:46 +0800

    1784. 检查二进制字符串字段

---
+++ LeetCode/SAOA/1784_CheckOnesSegment.cs
@@ -0,0 +1,33 @@
+﻿namespace LeetCode.SAOA
+{
+    internal sealed class CheckOnesSegmentSolution
+    {
+        public bool CheckOnesSegment(string s)
+        {
+            var existOne = false;
+            var zeroAfterOne = false;
+            foreach (var item in s)
+            {
+                if (item == '1')
+                {
+                    if (existOne && zeroAfterOne)
+                    {
+                        return false;
+                    }
+                    else
+                    {
+                        existOne = true;
+                    }
+                }
+                else
+                {
+                    if (existOne)
+                    {
+                        zeroAfterOne = true;
+                    }
+                }
+            }
+            return true;
+        }
+    }
+}
