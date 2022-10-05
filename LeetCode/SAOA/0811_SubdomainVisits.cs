fba4231fac9b572d090c52f5c98b415c1038ce98
褚许辉 <736265181@qq.com>
2022-10-05 13:57:47 +0800

    811. 子域名访问计数

---
+++ LeetCode/SAOA/0811_SubdomainVisits.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace LeetCode.SAOA
+{
+    internal sealed class SubdomainVisitsSolution
+    {
+        public IList<string> SubdomainVisits(string[] cpdomains)
+        {
+            var pairs = new Dictionary<string, int>();
+            foreach (var item in cpdomains)
+            {
+                var arr = item.Split(" ");
+                var count = int.Parse(arr[0]);
+                var address = arr[1];
+                var finish = false;
+                do
+                {
+                    if (pairs.TryGetValue(address, out var c))
+                    {
+                        pairs[address] = count + c;
+                    }
+                    else
+                    {
+                        pairs.Add(address, count);
+                    }
+                    var index = address.IndexOf('.');
+                    if (index == -1)
+                    {
+                        finish = true;
+                    }
+                    else
+                    {
+                        address = address[(index + 1)..];
+                    }
+                } while (!finish);
+            }
+            return pairs.Select(i => $"{i.Value.ToString()} {i.Key}").ToList();
+        }
+    }
+}
