a0a6696bc6e716a2db5dbade1c513ecc4ada20d9
褚许辉 <736265181@qq.com>
2022-10-08 20:14:46 +0800

    1800. 最大升序子数组和

---
+++ LeetCode/SAOA/1800_MaxAscendingSum.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace LeetCode.SAOA
+{
+    internal sealed class MaxAscendingSumSolution
+    {
+        public int MaxAscendingSum(int[] nums)
+        {
+            var max = int.MinValue;
+            var current = nums[0];
+            for (int i = 1; i < nums.Length; i++)
+            {
+                if (nums[i] > nums[i - 1])
+                {
+                    current += nums[i];
+                }
+                else
+                {
+                    max = Math.Max(max, current);
+                    current = nums[i];
+                }
+            }
+            max = Math.Max(max, current);
+            return max;
+        }
+    }
+}
