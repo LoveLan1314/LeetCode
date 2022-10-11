bb15d1263d20ebfb4be993b340bd4da7b184e370
褚许辉 <736265181@qq.com>
2022-10-11 20:46:33 +0800

    1790. 仅执行一次字符串交换能否使两个字符串相等

---
+++ LeetCode/SAOA/1790_AreAlmostEqual.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode.SAOA
+{
+    internal sealed class AreAlmostEqualSolution
+    {
+        public bool AreAlmostEqual(string s1, string s2)
+        {
+            var difference = new List<int>();
+            for (int i = 0; i < s1.Length; i++)
+            {
+                if (s1[i] != s2[i])
+                {
+                    if (difference.Count == 2)
+                    {
+                        return false;
+                    }
+                    else
+                    {
+                        difference.Add(i);
+                    }
+                }
+            }
+            if (difference.Count == 0)
+            {
+                return true;
+            }
+            //如果为1返回false
+            if (difference.Count != 2)
+            {
+                return false;
+            }
+            return s1[difference[0]] == s2[difference[1]] && s1[difference[1]] == s2[difference[0]];
+        }
+    }
+}
