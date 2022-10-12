e322e06ab3bab96e5fbf7126d64db425d1b0be14
褚许辉 <736265181@qq.com>
2022-10-12 20:48:13 +0800

    817. 链表组件

---
+++ LeetCode/SAOA/0817_NumComponents.cs
@@ -0,0 +1,31 @@
+﻿using System.Linq;
+
+namespace LeetCode.SAOA
+{
+    internal sealed class NumComponentsSolution
+    {
+        public int NumComponents(ListNode head, int[] nums)
+        {
+            var set = nums.ToHashSet();
+            var count = 0;
+            var prevExists = false;
+            while (head != null)
+            {
+                if (set.Contains(head.val))
+                {
+                    if (!prevExists)
+                    {
+                        count++;
+                        prevExists = true;
+                    }
+                }
+                else
+                {
+                    prevExists = false;
+                }
+                head = head.next;
+            }
+            return count;
+        }
+    }
+}
