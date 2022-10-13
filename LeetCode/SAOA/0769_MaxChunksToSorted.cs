93abb0b5d109d37100a656e5ae9f43bba6632fcb
褚许辉 <736265181@qq.com>
2022-10-13 23:16:59 +0800

    769. 最多能完成排序的块

---
+++ LeetCode/SAOA/0769_MaxChunksToSorted.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace LeetCode.SAOA
+{
+    internal sealed class MaxChunksToSortedSolution1
+    {
+        public int MaxChunksToSorted(int[] arr)
+        {
+            int m = 0, res = 0;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                m = Math.Max(m, arr[i]);
+                if (m == i)
+                {
+                    res++;
+                }
+            }
+            return res;
+        }
+    }
+}
