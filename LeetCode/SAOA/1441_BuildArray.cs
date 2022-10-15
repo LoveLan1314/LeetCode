d627f085d8b06af672bdf528fac5ce1044d4a24a
褚许辉 <736265181@qq.com>
2022-10-15 10:52:48 +0800

    1441. 用栈操作构建数组

---
+++ LeetCode/SAOA/1441_BuildArray.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode.SAOA
+{
+    internal sealed class BuildArraySolution
+    {
+        public IList<string> BuildArray(int[] target, int n)
+        {
+            var result = new List<string>();
+            for (int i = 0, j = 0; i < target.Length && j < n; j++)
+            {
+                result.Add("Push");
+                if (target[i] != j + 1)
+                {
+                    result.Add("Pop");
+                }
+                else
+                {
+                    i++;
+                }
+            }
+            return result;
+        }
+    }
+}
