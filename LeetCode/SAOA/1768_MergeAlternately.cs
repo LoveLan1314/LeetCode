2450d1a01d2391df2ae428caa55b488960b572b8
褚许辉 <736265181@qq.com>
2022-10-23 09:40:13 +0800

    1768. 交替合并字符串

---
+++ LeetCode/SAOA/1768_MergeAlternately.cs
@@ -0,0 +1,29 @@
+﻿using System.Text;
+
+namespace LeetCode.SAOA
+{
+    internal sealed class MergeAlternatelySolution
+    {
+        public string MergeAlternately(string word1, string word2)
+        {
+            var m = word1.Length;
+            var n = word2.Length;
+            int i = 0, j = 0;
+            var sb = new StringBuilder();
+            while (i < m || j < n)
+            {
+                if (i < m)
+                {
+                    sb.Append(word1[i]);
+                    i++;
+                }
+                if (j < n)
+                {
+                    sb.Append(word2[j]);
+                    j++;
+                }
+            }
+            return sb.ToString();
+        }
+    }
+}
