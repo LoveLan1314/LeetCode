b8400ca8d97bc319ea54e12c4d11ac19990b5843
褚许辉 <736265181@qq.com>
2022-10-27 20:43:31 +0800

    1822. 数组元素积的符号

---
+++ LeetCode/SAOA/1822_ArraySign.cs
@@ -0,0 +1,22 @@
+﻿namespace LeetCode.SAOA
+{
+    internal sealed class ArraySignSolution
+    {
+        public int ArraySign(int[] nums)
+        {
+            var count = 0;
+            foreach (var item in nums)
+            {
+                if (item == 0)
+                {
+                    return 0;
+                }
+                else if (item < 0)
+                {
+                    count++;
+                }
+            }
+            return count % 2 == 0 ? 1 : -1;
+        }
+    }
+}
