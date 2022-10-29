f581e484deee8a0e3649e06b73358a74eb07416a
褚许辉 <736265181@qq.com>
2022-10-29 10:16:18 +0800

    1773. 统计匹配检索规则的物品数量

---
+++ LeetCode/SAOA/1773_CountMatches.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode.SAOA
+{
+    internal sealed class CountMatchesSolution
+    {
+        public int CountMatches(IList<IList<string>> items, string ruleKey, string ruleValue)
+        {
+            var index = ruleKey switch
+            {
+                "type" => 0,
+                "color" => 1,
+                "name" => 2,
+                _ => -1,
+            };
+            var count = 0;
+            foreach (var item in items)
+            {
+                if (item[index] == ruleValue)
+                {
+                    count++;
+                }
+            }
+            return count;
+        }
+    }
+}
