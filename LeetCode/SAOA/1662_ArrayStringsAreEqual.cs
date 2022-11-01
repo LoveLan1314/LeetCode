d5e28c26687bfafd2ef20ba4202b1757c51bc09a
褚许辉 <736265181@qq.com>
2022-11-01 22:09:59 +0800

    1662. 检查两个字符串数组是否相等

---
+++ LeetCode/SAOA/1662_ArrayStringsAreEqual.cs
@@ -0,0 +1,22 @@
+﻿using System.Text;
+
+namespace LeetCode.SAOA
+{
+    internal sealed class ArrayStringsAreEqualSolution
+    {
+        public bool ArrayStringsAreEqual(string[] word1, string[] word2)
+        {
+            return Join(word1).Equals(Join(word2));
+        }
+
+        private string Join(string[] words)
+        {
+            StringBuilder ret = new StringBuilder();
+            foreach (string s in words)
+            {
+                ret.Append(s);
+            }
+            return ret.ToString();
+        }
+    }
+}
