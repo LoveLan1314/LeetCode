79c78dbcdb08537360e89bc5ee54eeaffbf4ff96
褚许辉 <736265181@qq.com>
2022-11-06 21:40:05 +0800

    1678. 设计 Goal 解析器

---
+++ LeetCode/SAOA/1678_Interpret.cs
@@ -0,0 +1,10 @@
+﻿namespace LeetCode.SAOA
+{
+    internal sealed class InterpretSolution
+    {
+        public string Interpret(string command)
+        {
+            return command.Replace("()", "o").Replace("(al)", "al");
+        }
+    }
+}
