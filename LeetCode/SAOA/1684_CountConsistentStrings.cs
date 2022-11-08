97b1528dcab6808722dd38d9ed5dd7bbad596896
褚许辉 <736265181@qq.com>
2022-11-08 20:49:51 +0800

    1684. 统计一致字符串的数目

---
+++ LeetCode/SAOA/1684_CountConsistentStrings.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace LeetCode.SAOA
+{
+    internal sealed class CountConsistentStringsSolution
+    {
+        public int CountConsistentStrings(string allowed, string[] words)
+        {
+            var set = new HashSet<char>(allowed.ToCharArray().Distinct());
+            var count = 0;
+            foreach (var item in words)
+            {
+                var match = true;
+                foreach (var c in item)
+                {
+                    if (!set.Contains(c))
+                    {
+                        match = false;
+                        break;
+                    }
+                }
+                if (match)
+                {
+                    count++;
+                }
+            }
+            return count;
+        }
+    }
+}
