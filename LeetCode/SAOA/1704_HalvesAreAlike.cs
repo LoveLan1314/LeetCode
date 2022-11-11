b3efdbcc0c4fca4c86b7ac1e33b604784c345b5b
褚许辉 <736265181@qq.com>
2022-11-11 22:17:44 +0800

    1704. 判断字符串的两半是否相似

---
+++ LeetCode/SAOA/1704_HalvesAreAlike.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode.SAOA
+{
+    internal sealed class HalvesAreAlikeSolution
+    {
+        public bool HalvesAreAlike(string s)
+        {
+            var count = s.Length;
+            var mid = count / 2;
+            var leftCount = 0;
+            var rightCount = 0;
+            var set = new HashSet<char>() { 'a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U' };
+            for (int i = 0; i < mid; i++)
+            {
+                if (set.Contains(s[i]))
+                {
+                    leftCount++;
+                }
+            }
+            for (int i = mid; i < count; i++)
+            {
+                if (set.Contains(s[i]))
+                {
+                    rightCount++;
+                }
+            }
+            return leftCount == rightCount;
+        }
+    }
+}
