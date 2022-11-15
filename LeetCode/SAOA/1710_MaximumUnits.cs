d801121ccbbd90b1c603c98506365d34617f19eb
褚许辉 <736265181@qq.com>
2022-11-15 22:15:58 +0800

    1710. 卡车上的最大单元数

---
+++ LeetCode/SAOA/1710_MaximumUnits.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace LeetCode.SAOA
+{
+    internal sealed class MaximumUnitsSolution
+    {
+        public int MaximumUnits(int[][] boxTypes, int truckSize)
+        {
+            Array.Sort(boxTypes, (a, b) => b[1] - a[1]);
+            int result = 0;
+            foreach (var item in boxTypes)
+            {
+                int numberOfBoxes = item[0];
+                int numberOfUnitsPerBox = item[1];
+                if (numberOfBoxes < truckSize)
+                {
+                    result += numberOfBoxes * numberOfUnitsPerBox;
+                    truckSize -= numberOfBoxes;
+                }
+                else
+                {
+                    result += truckSize * numberOfUnitsPerBox;
+                    break;
+                }
+            }
+            return result;
+        }
+    }
+}
