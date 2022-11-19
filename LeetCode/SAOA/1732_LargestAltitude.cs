52613c5d214dbfd1ef45dd37044e68a9ff19f44b
褚许辉 <736265181@qq.com>
2022-11-19 15:13:08 +0800

    1732. 找到最高海拔

---
+++ LeetCode/SAOA/1732_LargestAltitude.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace LeetCode.SAOA
+{
+    internal sealed class LargestAltitudeSolution
+    {
+        public int LargestAltitude(int[] gain)
+        {
+            int max = 0;
+            int current = 0;
+            for (int i = 0; i < gain.Length; i++)
+            {
+                current += gain[i];
+                max = Math.Max(max, current);
+            }
+            return max;
+        }
+    }
+}
