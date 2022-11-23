97a25e039b72ff2d3d1624682e69f28742fa1258
褚许辉 <736265181@qq.com>
2022-11-23 17:47:53 +0800

    1742. 盒子中小球的最大数量

---
+++ LeetCode/SAOA/1742_CountBalls.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LeetCode.SAOA
+{
+    internal sealed class CountBallsSolution
+    {
+        public int CountBalls(int lowLimit, int highLimit)
+        {
+            var boxPairs = new Dictionary<int, int>();
+            int result = 0;
+            for (int i = lowLimit; i <= highLimit; i++)
+            {
+                int box = 0, x = i;
+                while (x != 0)
+                {
+                    box += x % 10;
+                    x /= 10;
+                }
+                if (boxPairs.TryGetValue(box, out int value))
+                {
+                    boxPairs[box] = value + 1;
+                }
+                else
+                {
+                    boxPairs[box] = 1;
+                }
+                result = Math.Max(result, boxPairs[box]);
+            }
+            return result;
+        }
+    }
+}
