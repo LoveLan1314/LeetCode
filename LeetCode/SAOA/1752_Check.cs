0077c27eccb7325b840ca72f8cf2005af7536b5c
褚许辉 <736265181@qq.com>
2022-11-27 15:56:43 +0800

    1752. 检查数组是否经排序和轮转得到

---
+++ LeetCode/SAOA/1752_Check.cs
@@ -0,0 +1,32 @@
+﻿namespace LeetCode.SAOA
+{
+    internal sealed class CheckSllution
+    {
+        public bool Check(int[] nums)
+        {
+            bool existDesc = false;
+            for (int i = 1; i < nums.Length; i++)
+            {
+                if (nums[i] < nums[i - 1])
+                {
+                    if (existDesc)
+                    {
+                        return false;
+                    }
+                    else
+                    {
+                        existDesc = true;
+                    }
+                }
+            }
+            if (existDesc)
+            {
+                return nums[0] >= nums[^1];
+            }
+            else
+            {
+                return true;
+            }
+        }
+    }
+}
