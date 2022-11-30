20a007ffb2d162ed953d02cf78bde2aa37c587c4
褚许辉 <736265181@qq.com>
2022-11-30 20:56:56 +0800

    895. 最大频率栈

---
+++ LeetCode/SAOA/0895_FreqStack.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LeetCode.SAOA
+{
+    internal sealed class FreqStack
+    {
+        private readonly IDictionary<int, int> _freq = new Dictionary<int, int>();
+        private readonly IDictionary<int, Stack<int>> _group = new Dictionary<int, Stack<int>>();
+        private int _maxFreq;
+
+        public FreqStack()
+        {
+
+        }
+
+        public void Push(int val)
+        {
+            if (!_freq.ContainsKey(val))
+            {
+                _freq.Add(val, 1);
+            }
+            else
+            {
+                _freq[val]++;
+            }
+            if (!_group.ContainsKey(_freq[val]))
+            {
+                _group.Add(_freq[val], new Stack<int>());
+            }
+            _group[_freq[val]].Push(val);
+            _maxFreq = Math.Max(_maxFreq, _freq[val]);
+
+        }
+
+        public int Pop()
+        {
+            int val = _group[_maxFreq].Peek();
+            _freq[val]--;
+            _group[_maxFreq].Pop();
+            if (_group[_maxFreq].Count == 0)
+            {
+                _maxFreq--;
+            }
+            return val;
+        }
+    }
+}
