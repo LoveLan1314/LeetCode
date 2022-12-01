b91550fb665216a169b49bb9f18c3ca3fe535329
褚许辉 <736265181@qq.com>
2022-12-01 21:31:43 +0800

    1779. 找到最近的有相同 X 或 Y 坐标的点

---
+++ LeetCode/SAOA/1779_NearestValidPoint.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace LeetCode.SAOA
+{
+    internal sealed class _1779_NearestValidPointSolution
+    {
+        public int NearestValidPoint(int x, int y, int[][] points)
+        {
+            int result = -1;
+            int minDistance = int.MaxValue;
+            for (int i = 0; i < points.Length; i++)
+            {
+                if (points[i][0] == x || points[i][1] == y)
+                {
+                    int distance = Math.Abs(points[i][0] - x) + Math.Abs(points[i][1] - y);
+                    if (distance < minDistance)
+                    {
+                        result = i;
+                        minDistance = distance;
+                    }
+                }
+            }
+            return result;
+        }
+    }
+}
