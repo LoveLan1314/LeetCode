6e228d34c83ad944e95df2583fe37153e0d983a1
褚许辉 <736265181@qq.com>
2022-12-02 21:42:01 +0800

    1769. 移动所有球到每个盒子所需的最小操作数

---
+++ LeetCode/SAOA/1769_MinOperations.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace LeetCode.SAOA
+{
+    internal sealed class _1769_MinOperationsSolution
+    {
+        public int[] MinOperations(string boxes)
+        {
+            int n = boxes.Length;
+            var result = new int[n];
+            for (int i = 0; i < n; i++)
+            {
+                int sum = 0;
+                for (int j = 0; j < n; j++)
+                {
+                    if (boxes[j] == '1')
+                    {
+                        sum += Math.Abs(i - j);
+                    }
+                }
+                result[i] = sum;
+            }
+            return result;
+        }
+    }
+}
