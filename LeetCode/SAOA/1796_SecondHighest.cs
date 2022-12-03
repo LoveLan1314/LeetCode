2e9301ca0f563f75dce11e00bd790a0a9948fef5
褚许辉 <736265181@qq.com>
2022-12-03 16:46:42 +0800

    1796. 字符串中第二大的数字

---
+++ LeetCode/SAOA/1796_SecondHighest.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace LeetCode.SAOA
+{
+    internal sealed class _1796_SecondHighestSolution
+    {
+        public int SecondHighest(string s)
+        {
+            var set = new HashSet<int>();
+            foreach (char item in s)
+            {
+                if (item >= 48 && item <= 57)
+                {
+                    int number = item - '0';
+                    if (!set.Contains(number))
+                    {
+                        set.Add(number);
+                    }
+                }
+            }
+            if (set.Count > 1)
+            {
+                return set.OrderByDescending(x => x).ToList()[1];
+            }
+            else
+            {
+                return -1;
+            }
+        }
+    }
+}
