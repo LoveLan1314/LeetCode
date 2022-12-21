faf84d5925dc6d05a1ef0e007858019acc64a21b
褚许辉 <736265181@qq.com>
2022-12-21 20:25:23 +0800

    1753. 移除石子的最大得分

---
+++ LeetCode/SAOA/1753_MaximumScore.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace LeetCode.SAOA
+{
+    internal sealed class _1753_MaximumScoreSolution
+    {
+        public int MaximumScore(int a, int b, int c)
+        {
+            int sum = a + b + c;
+            int maxVal = Math.Max(Math.Max(a, b), c);
+            return Math.Min(sum - maxVal, sum / 2);
+        }
+    }
+}
