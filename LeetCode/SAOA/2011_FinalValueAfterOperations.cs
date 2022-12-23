c2eac7fd1ab77fdba80c590b9b20efdd552a094f
褚许辉 <736265181@qq.com>
2022-12-23 20:08:14 +0800

    2011. 执行操作后的变量值

---
+++ LeetCode/SAOA/2011_FinalValueAfterOperations.cs
@@ -0,0 +1,22 @@
+﻿namespace LeetCode.SAOA
+{
+    internal sealed class _2011_FinalValueAfterOperationsSolution
+    {
+        public int FinalValueAfterOperations(string[] operations)
+        {
+            int result = 0;
+            foreach (string operation in operations)
+            {
+                if (operation[0] == '+' || operation[2] == '+')
+                {
+                    result++;
+                }
+                else if (operation[0] == '-' || operation[2] == '-')
+                {
+                    result--;
+                }
+            }
+            return result;
+        }
+    }
+}
