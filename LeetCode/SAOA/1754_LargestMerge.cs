6db29bed5c1fc5ed4222316c40140338d9ffea40
褚许辉 <736265181@qq.com>
2022-12-24 20:49:35 +0800

    1754. 构造字典序最大的合并字符串

---
+++ LeetCode/SAOA/1754_LargestMerge.cs
@@ -0,0 +1,27 @@
+﻿using System.Text;
+
+namespace LeetCode.SAOA
+{
+    internal sealed class _1754_LargestMergeSolution
+    {
+        public string LargestMerge(string word1, string word2)
+        {
+            var sb = new StringBuilder();
+            int i = 0, j = 0;
+            while (i < word1.Length || j < word2.Length)
+            {
+                if (i < word1.Length && word1[i..].CompareTo(word2[j..]) > 0)
+                {
+                    sb.Append(word1[i]);
+                    i++;
+                }
+                else
+                {
+                    sb.Append(word2[j]);
+                    j++;
+                }
+            }
+            return sb.ToString();
+        }
+    }
+}
