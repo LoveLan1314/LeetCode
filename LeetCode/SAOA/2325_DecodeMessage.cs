1583d3460c9d2f35b18a408f46e9d1f39399604b
褚许辉 <736265181@qq.com>
2023-02-01 20:40:24 +0800

    2325. 解密消息

---
+++ LeetCode/SAOA/2325_DecodeMessage.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode.SAOA
+{
+    internal sealed class _2325_DecodeMessage
+    {
+        public string DecodeMessage(string key, string message)
+        {
+            var pairs = new Dictionary<char, char>();
+            var index = 0;
+            foreach (char item in key)
+            {
+                if (item != ' ' && !pairs.ContainsKey(item))
+                {
+                    pairs.Add(item, (char)(97 + index));
+                    index++;
+                }
+            }
+            var sb = new StringBuilder();
+            foreach (char item in message)
+            {
+                if (item == ' ')
+                {
+                    sb.Append(item);
+                }
+                else
+                {
+                    sb.Append(pairs[item]);
+                }
+            }
+            return sb.ToString();
+        }
+    }
+}
