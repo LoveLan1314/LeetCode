e1a9b068678efffa385e6d54dec9a909ce618cf5
褚许辉 <736265181@qq.com>
2023-02-06 21:28:07 +0800

    2331. 计算布尔二叉树的值

---
+++ LeetCode/SAOA/2331_EvaluateTree.cs
@@ -0,0 +1,21 @@
+﻿namespace LeetCode.SAOA
+{
+    internal sealed class _2331_EvaluateTreeSolution
+    {
+        public bool EvaluateTree(TreeNode root)
+        {
+            if (root.left == null && root.right == null)
+            {
+                return root.val == 1;
+            }
+            if (root.val == 2)
+            {
+                return EvaluateTree(root.left) || EvaluateTree(root.right);
+            }
+            else
+            {
+                return EvaluateTree(root.left) && EvaluateTree(root.right);
+            }
+        }
+    }
+}
