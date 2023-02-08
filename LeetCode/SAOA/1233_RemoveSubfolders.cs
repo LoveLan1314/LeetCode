53cf08998f7b547cfe881cda244eed53f2334ceb
褚许辉 <736265181@qq.com>
2023-02-08 19:25:27 +0800

    1233. 删除子文件夹

---
+++ LeetCode/SAOA/1233_RemoveSubfolders.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LeetCode.SAOA
+{
+    internal sealed class _1233_RemoveSubfoldersSolution
+    {
+        public IList<string> RemoveSubfolders(string[] folder)
+        {
+            Array.Sort(folder);
+            var result = new List<string>() { folder[0] };
+            int n = folder.Length;
+            for (int i = 1; i < n; i++)
+            {
+                string prevValue = result[^1];
+                int pre = prevValue.Length;
+                if (!(pre < folder[i].Length && prevValue.Equals(folder[i][..pre]) && folder[i][pre] == '/'))
+                {
+                    result.Add(folder[i]);
+                }
+            }
+            return result;
+        }
+    }
+}
