7df9bec7fe6d38a04f2549c910203d5d5aa1af13
褚许辉 <736265181@qq.com>
2023-02-15 21:24:37 +0800

    1250. 检查「好数组」

---
+++ LeetCode/SAOA/1250_IsGoodArray.cs
@@ -0,0 +1,30 @@
+﻿namespace LeetCode.SAOA
+{
+    internal sealed class _1250_IsGoodArraySolution
+    {
+        public bool IsGoodArray(int[] nums)
+        {
+            int divisor = nums[0];
+            foreach (int num in nums)
+            {
+                divisor = GCD(divisor, num);
+                if (divisor == 1)
+                {
+                    break;
+                }
+            }
+            return divisor == 1;
+        }
+
+        private int GCD(int num1, int num2)
+        {
+            while (num2 != 0)
+            {
+                int temp = num1;
+                num1 = num2;
+                num2 = temp % num2;
+            }
+            return num1;
+        }
+    }
+}
