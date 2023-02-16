07db3263728bce33d5fa66f8f67a12cd84e1903e
褚许辉 <736265181@qq.com>
2023-02-16 20:26:46 +0800

    2341. 数组能形成多少数对

---
+++ LeetCode/SAOA/2341_NumberOfPairs.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode.SAOA
+{
+    internal sealed class _2341_NumberOfPairsSolution
+    {
+        public int[] NumberOfPairs(int[] nums)
+        {
+            int matchCount = 0;
+            HashSet<int> result = new HashSet<int>();
+            foreach (int i in nums)
+            {
+                if (result.Contains(i))
+                {
+                    matchCount++;
+                    result.Remove(i);
+                }
+                else
+                {
+                    result.Add(i);
+                }
+            }
+            return new int[] { matchCount, nums.Length - (matchCount * 2) };
+        }
+    }
+}
