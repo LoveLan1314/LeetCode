ebeaf3c340ebdd46ab4b429517d5a55b0ade07fb
褚许辉 <736265181@qq.com>
2023-02-18 19:47:32 +0800

    1237. 找出给定方程的正整数解

---
+++ LeetCode/SAOA/1237_FindSolution.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode.SAOA
+{
+    internal sealed class _1237_FindSolution
+    {
+        public IList<IList<int>> FindSolution(CustomFunction customfunction, int z)
+        {
+            var result = new List<IList<int>>();
+            for (int x = 1; x <= 1000; x++)
+            {
+                for (int y = 1; y <= 1000; y++)
+                {
+                    if (customfunction.f(x, y) == z)
+                    {
+                        result.Add(new List<int>() { x, y });
+                    }
+                }
+            }
+            return result;
+        }
+
+        // This is the custom function interface.
+        // You should not implement it, or speculate about its implementation
+        public sealed class CustomFunction
+        {
+            // Returns f(x, y) for any given positive integers x and y.
+            // Note that f(x, y) is increasing with respect to both x and y.
+            // i.e. f(x, y) < f(x + 1, y), f(x, y) < f(x, y + 1)
+            public int f(int x, int y)
+            {
+                return x;
+            }
+        };
+
+    }
+}
