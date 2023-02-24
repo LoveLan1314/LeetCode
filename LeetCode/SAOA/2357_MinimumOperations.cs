78831dba8051ac3a41737a3a736a0c8f8e3e8146
褚许辉 <736265181@qq.com>
2023-02-24 19:59:06 +0800

    2357. 使数组中所有元素都等于零

---
+++ LeetCode/SAOA/2357_MinimumOperations.cs
@@ -0,0 +1,12 @@
+﻿using System.Linq;
+
+namespace LeetCode.SAOA
+{
+    internal sealed class _2357_MinimumOperationsSolution
+    {
+        public int MinimumOperations(int[] nums)
+        {
+            return nums.Where(i => i > 0).Distinct().Count();
+        }
+    }
+}
