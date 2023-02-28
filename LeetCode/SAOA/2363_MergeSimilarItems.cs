50e589bbf338c3440472ee7c5d52439797613bb1
褚许辉 <736265181@qq.com>
2023-02-28 20:42:35 +0800

    2363. 合并相似的物品

---
+++ LeetCode/SAOA/2363_MergeSimilarItems.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace LeetCode.SAOA
+{
+    internal sealed class _2363_MergeSimilarItemsSolution
+    {
+        public IList<IList<int>> MergeSimilarItems(int[][] items1, int[][] items2)
+        {
+            var pairs = new Dictionary<int, int>();
+            foreach (var item in items1)
+            {
+                pairs.Add(item[0], item[1]);
+            }
+            foreach (var item in items2)
+            {
+                if (!pairs.ContainsKey(item[0]))
+                {
+                    pairs.Add(item[0], item[1]);
+                }
+                else
+                {
+                    pairs[item[0]] += item[1];
+                }
+            }
+            return pairs.OrderBy(i => i.Key).Select(i => new List<int>() { i.Key, i.Value } as IList<int>).ToList();
+        }
+    }
+}
