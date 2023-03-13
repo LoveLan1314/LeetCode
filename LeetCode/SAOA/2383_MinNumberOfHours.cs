64d0b9ff9c9be0f5279cc4e4f01102da5418be33
褚许辉 <736265181@qq.com>
2023-03-13 19:27:55 +0800

    2383. 赢得比赛需要的最少训练时长

---
+++ LeetCode/SAOA/2383_MinNumberOfHours.cs
@@ -0,0 +1,28 @@
+﻿namespace LeetCode.SAOA
+{
+    internal sealed class _2383_MinNumberOfHoursSolution
+    {
+        public int MinNumberOfHours(int initialEnergy, int initialExperience, int[] energy, int[] experience)
+        {
+            int sum = 0;
+            foreach (int i in energy)
+            {
+                sum += i;
+            }
+            int trainingHours = initialEnergy > sum ? 0 : sum + 1 - initialEnergy;
+            foreach (int i in experience)
+            {
+                if (initialExperience <= i)
+                {
+                    trainingHours += 1 + (i - initialExperience);
+                    initialExperience = 2 * i + 1;
+                }
+                else
+                {
+                    initialExperience += i;
+                }
+            }
+            return trainingHours;
+        }
+    }
+}
