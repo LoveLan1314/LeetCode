f35b4a551d5893bca18f88c9ce3a3e2c9e020875
褚许辉 <736265181@qq.com>
2023-03-21 21:34:03 +0800

    2469. 温度转换

---
+++ LeetCode/SAOA/2469_ConvertTemperature.cs
@@ -0,0 +1,10 @@
+﻿namespace LeetCode.SAOA
+{
+    internal sealed class _2469_ConvertTemperatureSolution
+    {
+        public double[] ConvertTemperature(double celsius)
+        {
+            return new double[] { celsius + 273.15, celsius * 1.80 + 32 };
+        }
+    }
+}
