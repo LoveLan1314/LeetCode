bd1d415a74a8d070aabf15bdcc2b5971eac13434
褚许辉 <736265181@qq.com>
2023-03-26 12:44:44 +0800

    2395. 和相等的子数组

---
+++ LeetCode/SAOA/2395_FindSubarrays.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode.SAOA
+{
+    internal sealed class _2395_FindSubarraysSolution
+    {
+        public bool FindSubarrays(int[] nums)
+        {
+            HashSet<int> set = new HashSet<int>();
+            for (int i = 1; i < nums.Length; i++)
+            {
+                int sum = nums[i] + nums[i - 1];
+                if (set.Contains(sum))
+                {
+                    return true;
+                }
+                else
+                {
+                    set.Add(sum);
+                }
+            }
+            return false;
+        }
+    }
+}
