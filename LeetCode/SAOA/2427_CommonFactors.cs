c27f212b5b09c818ce39eb502d6d16d75fd90445
褚许辉 <736265181@qq.com>
2023-04-05 13:38:33 +0800

    2427. 公因子的数目

---
+++ LeetCode/SAOA/2427_CommonFactors.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace LeetCode.SAOA
+{
+    internal sealed class _2427_CommonFactorsSolution
+    {
+        public int CommonFactors(int a, int b)
+        {
+            int result = 0;
+            int min = Math.Min(a, b);
+            for (int i = 1; i <= min; i++)
+            {
+                if (a % i == 0 && b % i == 0)
+                {
+                    result++;
+                }
+            }
+            return result;
+        }
+    }
+}
