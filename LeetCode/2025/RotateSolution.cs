aaf99de8e5dab424d39951963b70ff7b7574a2c4
chuxuhui <chuxuhui@nmy.cn>
2025-07-24 18:52:53 +0800

    189. 轮转数组

---
+++ LeetCode/2025/RotateSolution.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode._2025
+{
+    internal sealed class RotateSolution
+    {
+        public void Rotate(int[] nums, int k)
+        {
+            k %= nums.Length;
+            Reverse(nums, 0, nums.Length - 1);
+            Reverse(nums, 0, k - 1);
+            Reverse(nums, k, nums.Length - 1);
+        }
+
+        private void Reverse(int[] nums, int start, int end)
+        {
+            while (start < end)
+            {
+                (nums[end], nums[start]) = (nums[start], nums[end]);
+                start += 1;
+                end -= 1;
+            }
+        }
+    }
+}
