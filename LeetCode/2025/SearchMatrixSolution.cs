6401eb47584e7a8206a64e4fe06fcccdcb6253d2
chuxuhui <chuxuhui@nmy.cn>
2025-08-06 19:42:34 +0800

    240. 搜索二维矩阵 II

---
+++ LeetCode/2025/SearchMatrixSolution.cs
@@ -0,0 +1,20 @@
+﻿namespace LeetCode._2025
+{
+    internal sealed class SearchMatrixSolution
+    {
+        public bool SearchMatrix(int[][] matrix, int target)
+        {
+            foreach (var row in matrix)
+            {
+                foreach (var item in row)
+                {
+                    if (item == target)
+                    {
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+    }
+}
